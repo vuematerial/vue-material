8f6f39276533b8f08003e4a3a71c511817880952
Marcos Moura <marcosvmmoura@gmail.com>
2016-07-14 18:16:02 -0300

    Improve ripple css to match with color theme

--- src/docs.vue
+++ src/docs.vue
@@ -1,57 +1,73 @@
 <template>
   <div class="container">
-    <md-button class="md-primary" @click="clickAction">Button</md-button>
-    <md-link-button class="md-primary" :disabled="disabled">Primary</md-link-button>
-    <md-link-button class="md-raised">Raised</md-link-button>
-    <md-link-button class="md-raised" :disabled="!disabled">Disabled</md-link-button>
-    <md-link-button class="md-raised md-dense">Dense</md-link-button>
-    <md-link-button class="md-icon-button">
-      <md-icon>menu</md-icon>
-    </md-link-button>
+    <section>
+      <h2 class="title">Buttons</h2>
 
-    <div class="md-button-group">
+      <md-button class="md-primary" @click="clickAction">Button</md-button>
+      <md-link-button class="md-primary" :disabled="disabled">Primary</md-link-button>
+      <md-link-button class="md-raised">Raised</md-link-button>
+      <md-link-button class="md-raised" :disabled="!disabled">Disabled</md-link-button>
+      <md-link-button class="md-raised md-dense">Dense</md-link-button>
       <md-link-button class="md-icon-button">
-        <md-icon>format_align_left</md-icon>
+        <md-icon>menu</md-icon>
       </md-link-button>
+    </section>
 
-      <md-link-button class="md-icon-button">
-        <md-icon>format_align_center</md-icon>
-      </md-link-button>
+    <section>
+      <h2 class="title">Button Group</h2>
 
-      <md-link-button class="md-icon-button">
-        <md-icon>format_align_right</md-icon>
-      </md-link-button>
+      <div class="md-button-group">
+        <md-link-button class="md-icon-button">
+          <md-icon>format_align_left</md-icon>
+        </md-link-button>
 
-      <md-link-button class="md-icon-button">
-        <md-icon>format_align_justify</md-icon>
-      </md-link-button>
-    </div>
+        <md-link-button class="md-icon-button">
+          <md-icon>format_align_center</md-icon>
+        </md-link-button>
 
-    <div style="padding: 16px; position: relative" v-md-ink-ripple>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
+        <md-link-button class="md-icon-button">
+          <md-icon>format_align_right</md-icon>
+        </md-link-button>
 
-    <div style="width: 720px; max-width: 100%; height: 480px;" v-md-theme>
-      <md-bottom-bar>
-        <md-bottom-bar-item md-icon="ondemand_video">Movies</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="music_note" class="md-active">Music</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="books">Books</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="shop">Shop</md-bottom-bar-item>
-      </md-bottom-bar>
+        <md-link-button class="md-icon-button">
+          <md-icon>format_align_justify</md-icon>
+        </md-link-button>
+      </div>
+    </section>
 
-      <md-bottom-bar md-shift style="margin-top: 24px">
-        <md-bottom-bar-item md-icon="ondemand_video">Movies</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="music_note">Music</md-bottom-bar-item>
-        <md-bottom-bar-item class="md-active" md-icon="books">Books</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="photo">Pictures</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="shop">Shop</md-bottom-bar-item>
-      </md-bottom-bar>
-    </div>
+    <section>
+      <h2 class="title">Ripple</h2>
+
+      <div style="padding: 16px; position: relative" v-md-ink-ripple>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </section>
+
+    <section>
+      <h2 class="title">Bottom Bar</h2>
+
+      <div style="width: 720px; max-width: 100%; height: 480px;" v-md-theme>
+        <md-bottom-bar>
+          <md-bottom-bar-item md-icon="ondemand_video">Movies</md-bottom-bar-item>
+          <md-bottom-bar-item md-icon="music_note" class="md-active">Music</md-bottom-bar-item>
+          <md-bottom-bar-item md-icon="books">Books</md-bottom-bar-item>
+          <md-bottom-bar-item md-icon="shop">Shop</md-bottom-bar-item>
+        </md-bottom-bar>
+
+        <md-bottom-bar md-shift style="margin-top: 24px">
+          <md-bottom-bar-item md-icon="ondemand_video">Movies</md-bottom-bar-item>
+          <md-bottom-bar-item md-icon="music_note">Music</md-bottom-bar-item>
+          <md-bottom-bar-item class="md-active" md-icon="books">Books</md-bottom-bar-item>
+          <md-bottom-bar-item md-icon="photo">Pictures</md-bottom-bar-item>
+          <md-bottom-bar-item md-icon="shop">Shop</md-bottom-bar-item>
+        </md-bottom-bar>
+      </div>
+    </section>
   </div>
 </template>
 
