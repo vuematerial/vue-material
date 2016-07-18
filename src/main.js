196dd107e6cb8fe6227a685a6f72618764c00a46
Marcos Moura <marcosvmmoura@gmail.com>
2016-07-18 13:00:51 -0300

    Create button toggles

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import Docs from './docs';
 import './core/core';
 import MdInkRipple from './components/mdInkRipple';
 import MdButton from './components/mdButton';
+import MdButtonToggle from './components/mdButtonToggle';
 import MdIcon from './components/mdIcon';
 import MdBottomBar from './components/mdBottomBar';
 import MdTheme from './components/mdTheme';
 
 Vue.use(MdInkRipple);
 Vue.use(MdButton);
+Vue.use(MdButtonToggle);
 Vue.use(MdIcon);
 Vue.use(MdBottomBar);
 Vue.use(MdTheme, {
