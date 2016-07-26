fc51366254c1d460c56e6905c3f60625d8fd991a
Marcos Moura <marcosvmmoura@gmail.com>
2016-07-26 02:48:31 -0300

    Create dividers on lists

---
+++ src/docs/pages/Divider.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <md-divider></md-divider>
+
+    <md-divider class="md-inset"></md-divider>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  section {
+    margin: 24px;
+  }
+
+  .md-divider {
+    margin-top: 24px;
+  }
+</style>
