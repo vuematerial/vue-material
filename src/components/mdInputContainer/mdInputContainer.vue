783b21a9aef2770bbecad91fd927c891eb3494fc
Marcos Moura <marcosvmmoura@gmail.com>
2016-07-27 19:17:22 -0300

    Create button to toggle passwords

--- src/components/mdInputContainer/mdInputContainer.vue
+++ src/components/mdInputContainer/mdInputContainer.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="md-input-container" :class="classes">
     <slot></slot>
+
+    <md-button class="md-icon-button md-toggle-password" @click="togglePasswordType" v-if="mdHasPassword">
+      <md-icon>{{ showPassword ? 'visibility_off' : 'visibility' }}</md-icon>
+    </md-button>
   </div>
 </template>
 
@@ -11,6 +15,7 @@
   let inlineClass = 'md-input-inline';
   let disabledClass = 'md-input-disabled';
   let hasValueClass = 'md-has-value';
+  let hasPasswordClass = 'md-has-password';
 
   let focusBindFunction;
   let blurBindFunction;
@@ -35,7 +40,8 @@
   export default {
     props: {
       mdInline: Boolean,
-      mdDisabled: Boolean
+      mdDisabled: Boolean,
+      mdHasPassword: Boolean
     },
     computed: {
       classes() {
@@ -43,6 +49,7 @@
 
         this.mdInline && cssClasses.push(inlineClass);
         this.mdDisabled && cssClasses.push(disabledClass);
+        this.mdHasPassword && cssClasses.push(hasPasswordClass);
 
         return cssClasses.join(' ');
       }
@@ -54,13 +61,31 @@
     },
     data() {
       return {
-        input: false
+        input: false,
+        inputType: false,
+        showPassword: false
       };
+    },
+    methods: {
+      togglePasswordType() {
+        if (this.input.tagName.toLowerCase() === 'input') {
+          let type = this.input.type;
+
+          if (type === 'password') {
+            this.input.type = 'text';
+            this.showPassword = true;
+          } else {
+            this.input.type = 'password';
+            this.showPassword = false;
+          }
+        }
+      }
     },
     ready() {
       let container = this.$el;
 
-      this.input = container.querySelector('input');
+      this.input = container.querySelector('input') || container.querySelector('textarea');
+      this.inputType = this.input.type;
 
       focusBindFunction = function() {
         container.classList.add(focusedClass);
