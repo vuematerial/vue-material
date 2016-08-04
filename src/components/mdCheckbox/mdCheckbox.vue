1f8d2b1a98d7d481ea6076ccd055129b2a3a76fa
Marcos Moura <mrufino@ciandt.com>
2016-08-04 20:59:42 -0300

    Create initial state of checkbox

--- src/components/mdCheckbox/mdCheckbox.vue
+++ src/components/mdCheckbox/mdCheckbox.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="md-checkbox md-checked">
-    <div class="md-checkbox-container" v-md-ink-ripple>
-      <input type="checkbox" id="temp">
+  <div class="md-checkbox" :class="classes">
+    <div class="md-checkbox-container" @click="toggleCheck" v-md-ink-ripple>
+      <input type="checkbox" v-model="model" :name="name" :id="id" :disabled="disabled" :value="value">
+
       <md-icon>check</md-icon>
     </div>
 
-    <label class="md-checkbox-label" for="temp">
+    <label :for="id || name" class="md-checkbox-label">
       <slot></slot>
     </label>
   </div>
 </template>
 
 <style lang="scss" src="./mdCheckbox.scss"></style>
+
+<script>
+  export default {
+    props: {
+      model: {
+        type: Boolean,
+        required: true,
+        twoWay: true
+      },
+      name: String,
+      id: String,
+      disabled: Boolean
+    },
+    computed: {
+      classes() {
+        return {
+          'md-checked': Boolean(this.model)
+        };
+      }
+    },
+    methods: {
+      toggleCheck() {
+        this.model = !this.model;
+      }
+    },
+    ready() {
+
+    }
+  };
+</script>
