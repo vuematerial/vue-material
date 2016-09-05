f9064128a5e01595394743418400e0699d6a2355
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-05 02:47:08 -0300

    Convert checkbox to vue 2

--- src/docs/pages/Checkbox.vue
+++ src/docs/pages/Checkbox.vue
@@ -3,27 +3,27 @@
     <h2 class="title">Checkbox</h2>
 
     <div>
-      <md-checkbox :model.sync="checked0" id="my-test0" name="my-test0">My beautiful checkbox</md-checkbox>
+      <md-checkbox id="my-test0" name="my-test0" checked @change="onChange">My beautiful checkbox</md-checkbox>
     </div>
 
     <div v-md-theme="'indigo'">
-      <md-checkbox :model.sync="checked1" id="my-test1" name="my-test1">Themed</md-checkbox>
+      <md-checkbox id="my-test1" name="my-test1" checked @change="onChange">Themed</md-checkbox>
     </div>
 
     <div v-md-theme="'blue'">
-      <md-checkbox :model.sync="checked2" id="my-test2" name="my-test2">Accent Color</md-checkbox>
+      <md-checkbox id="my-test2" name="my-test2" checked @change="onChange">Accent Color</md-checkbox>
     </div>
 
     <div v-md-theme="'orange'">
-      <md-checkbox :model.sync="checked3" id="my-test3" name="my-test3">Fancy colors</md-checkbox>
+      <md-checkbox id="my-test3" name="my-test3" checked @change="onChange">Fancy colors</md-checkbox>
     </div>
 
-    <div v-md-theme="'bottom-bar'">
-      <md-checkbox :model.sync="checked4" id="my-test4" name="my-test4">Really simple</md-checkbox>
+    <div v-md-theme="'teal'">
+      <md-checkbox id="my-test4" name="my-test4" checked @change="onChange">Really simple</md-checkbox>
     </div>
 
-    <div v-md-theme="'bottom-bar'">
-      <md-checkbox :model.sync="checked5" id="my-test5" name="my-test5" disabled>Disabled</md-checkbox>
+    <div v-md-theme="'teal'">
+      <md-checkbox id="my-test5" name="my-test5" checked @change="onChange" disabled>Disabled</md-checkbox>
     </div>
   </section>
 </template>
@@ -36,15 +36,10 @@
 
 <script>
   export default {
-    data() {
-      return {
-        checked0: true,
-        checked1: true,
-        checked2: true,
-        checked3: true,
-        checked4: true,
-        checked5: true
-      };
+    methods: {
+      onChange(change) {
+        console.log('Change', change);
+      }
     }
   };
 </script>
