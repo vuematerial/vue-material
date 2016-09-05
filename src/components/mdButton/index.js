6b3540bb786db56df3e810095f8e4b3a495a15f7
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-05 02:15:28 -0300

    Convert component template to render function

--- src/components/mdButton/index.js
+++ src/components/mdButton/index.js
@@ -1,10 +1,8 @@
 import MdButton from './mdButton.vue';
-import MdLinkButton from './mdLinkButton.vue';
 import MdButtonTheme from './mdButton.theme';
 
 export default function install(Vue) {
   Vue.component('md-button', Vue.extend(MdButton));
-  Vue.component('md-link-button', Vue.extend(MdLinkButton));
 
   window.VueMaterial.styles.push(MdButtonTheme);
 }
