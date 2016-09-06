0d12b4ca2714fc719b44c5c0902c7d51af170c74
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-06 01:29:07 -0300

    Remove legacy documentation

--- src/docs-temp/core/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import codeExample from './components/code-example';
-import codeSection from './components/code-section';
-import pageContainer from './components/page-container';
-
-Vue.component('code-example', codeExample);
-Vue.component('code-section', codeSection);
-Vue.component('page-container', pageContainer);
