831fe95ba7e316cfe666b77710fd95076b12c8e0
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-08 01:32:30 -0300

    Upgrade switch to vue 2.0 without drag events

--- src/docs/pages/Switch.vue
+++ src/docs/pages/Switch.vue
@@ -3,27 +3,27 @@
     <h2 class="title">Switch</h2>
 
     <div>
-      <md-switch :model.sync="checked0" id="my-test0" name="my-test0"></md-switch>
+      <md-switch v-model="checked0" id="my-test0" name="my-test0"></md-switch>
     </div>
 
     <div v-md-theme="'indigo'">
-      <md-switch :model.sync="checked1" id="my-test1" name="my-test1"></md-switch>
+      <md-switch v-model="checked1" id="my-test1" name="my-test1">With label</md-switch>
     </div>
 
     <div v-md-theme="'blue'">
-      <md-switch :model.sync="checked2" id="my-test2" name="my-test2"></md-switch>
+      <md-switch v-model="checked2" id="my-test2" name="my-test2">Themed</md-switch>
     </div>
 
     <div v-md-theme="'orange'">
-      <md-switch :model.sync="checked3" id="my-test3" name="my-test3"></md-switch>
+      <md-switch v-model="checked3" id="my-test3" name="my-test3">Accent Color</md-switch>
     </div>
 
-    <div v-md-theme="'bottom-bar'">
-      <md-switch :model.sync="checked4" id="my-test4" name="my-test4"></md-switch>
+    <div v-md-theme="'teal'">
+      <md-switch v-model="checked4" id="my-test4" name="my-test4">Fancy colors</md-switch>
     </div>
 
-    <div v-md-theme="'bottom-bar'">
-      <md-switch :model.sync="checked5" id="my-test5" name="my-test5" disabled></md-switch>
+    <div v-md-theme="'teal'">
+      <md-switch v-model="checked5" id="my-test5" name="my-test5" disabled>Disabled</md-switch>
     </div>
   </section>
 </template>
