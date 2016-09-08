b18c5657ceb22149ef1c6593d5c2c5d4cb1be130
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-08 02:14:39 -0300

    Add support to router-link

--- src/components/mdList/mdListItem.vue
+++ src/components/mdList/mdListItem.vue
@@ -1,11 +1,3 @@
-<template>
-  <li class="md-list-item" @click="$emit('click')">
-    <md-button :title="title" :target="target" :href="href" class="md-list-item-container">
-      <slot></slot>
-    </md-button>
-  </li>
-</template>
-
 <script>
   export default {
     props: {
@@ -13,6 +5,39 @@
       vLink: [String, Object],
       title: String,
       target: String
+    },
+    render(createElement) {
+      let containerClass = 'md-button md-list-item-container';
+      let listItem = {
+        staticClass: 'md-list-item',
+        on: {
+          click: () => {
+            this.$emit('click');
+          }
+        }
+      };
+      let slot = this.$slots.default;
+      let componentOptions = slot[0].componentOptions;
+
+      if (componentOptions && componentOptions.tag === 'router-link') {
+        slot[0].data.staticClass = containerClass;
+        slot[0].data.directives = [{
+          name: 'md-ink-ripple'
+        }];
+
+        return createElement('li', listItem, slot);
+      }
+
+      let button = createElement('md-button', {
+        staticClass: containerClass,
+        attrs: {
+          title: this.title,
+          target: this.target,
+          href: this.href
+        }
+      }, slot);
+
+      return createElement('li', listItem, [button]);
     }
   };
 </script>
