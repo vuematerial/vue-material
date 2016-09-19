8c8e99edba94aae18fa889bf69b1bb3cbc71a829
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-19 00:46:56 -0300

    Rename home page

--- src/docs/pages/Home.vue
+++ src/docs/pages/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="page-content">
-    <h1>Home</h1>
+    <h1>Introduction</h1>
   </div>
 </template>
+
+<script>
+  export default {
+    mounted() {
+      this.$root.pageTitle = 'Introduction';
+    }
+  };
+</script>
