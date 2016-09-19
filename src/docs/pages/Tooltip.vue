bb5a7be84e98f4f6d91b8c0dd825b05e6502618b
Marcos Moura <mrufino@ciandt.com>
2016-09-19 20:35:30 -0300

    Update tooltip examples and fix tooltip z-index

--- src/docs/pages/Tooltip.vue
+++ src/docs/pages/Tooltip.vue
@@ -1,71 +1,80 @@
 <template>
-  <div>
-    <section>
-      <h2 class="title">Tooltip</h2>
+  <demo-page>
+    <div slot="examples">
+      <demo-example label="Default">
+        <div>
+          <md-button class="md-icon-button md-raised md-primary">
+            <md-icon>folder</md-icon>
+            <md-tooltip md-direction="top">My tooltip</md-tooltip>
+          </md-button>
 
-      <div>
-        <md-button class="md-icon-button md-raised md-primary">
-          <md-icon>folder</md-icon>
-          <md-tooltip md-direction="top">My tooltip</md-tooltip>
-        </md-button>
+          <md-avatar>
+            <img src="https://placeimg.com/40/40/people/1" alt="People">
+            <md-tooltip md-direction="bottom">My tooltip</md-tooltip>
+          </md-avatar>
+        </div>
 
-        <md-avatar>
-          <img src="https://placeimg.com/40/40/people/1" alt="People">
-          <md-tooltip md-direction="bottom">My tooltip</md-tooltip>
-        </md-avatar>
-      </div>
+        <div>
+          <md-button class="md-icon-button md-raised md-warn">
+            <md-icon>home</md-icon>
+            <md-tooltip md-direction="left">My tooltip</md-tooltip>
+          </md-button>
 
-      <div>
-        <md-button class="md-icon-button md-raised md-warn">
-          <md-icon>home</md-icon>
-          <md-tooltip md-direction="left">My tooltip</md-tooltip>
-        </md-button>
+          <md-avatar class="md-avatar-icon">
+            <md-icon>person</md-icon>
+            <md-tooltip md-direction="right">My tooltip</md-tooltip>
+          </md-avatar>
+        </div>
+      </demo-example>
 
-        <md-avatar class="md-avatar-icon">
-          <md-icon>person</md-icon>
-          <md-tooltip md-direction="right">My tooltip</md-tooltip>
-        </md-avatar>
-      </div>
-    </section>
+      <demo-example label="Delay - 400ms">
+        <div>
+          <md-button class="md-icon-button md-raised md-primary">
+            <md-icon>folder</md-icon>
+            <md-tooltip md-delay="400" md-direction="top">My tooltip</md-tooltip>
+          </md-button>
 
-    <section>
-      <h2 class="title">Tooltip - Delay</h2>
+          <md-avatar>
+            <img src="https://placeimg.com/40/40/people/1" alt="People">
+            <md-tooltip md-delay="400" md-direction="bottom">My tooltip</md-tooltip>
+          </md-avatar>
+        </div>
 
-      <div>
-        <md-button class="md-icon-button md-raised md-primary">
-          <md-icon>folder</md-icon>
-          <md-tooltip md-delay="500" md-direction="top">My tooltip</md-tooltip>
-        </md-button>
+        <div>
+          <md-button class="md-icon-button md-raised md-warn">
+            <md-icon>home</md-icon>
+            <md-tooltip md-delay="400" md-direction="left">My tooltip</md-tooltip>
+          </md-button>
 
-        <md-avatar>
-          <img src="https://placeimg.com/40/40/people/1" alt="People">
-          <md-tooltip md-delay="500" md-direction="bottom">My tooltip</md-tooltip>
-        </md-avatar>
-      </div>
+          <md-avatar class="md-avatar-icon">
+            <md-icon>person</md-icon>
+            <md-tooltip md-delay="400" md-direction="right">My tooltip</md-tooltip>
+          </md-avatar>
+        </div>
+      </demo-example>
+    </div>
 
-      <div>
-        <md-button class="md-icon-button md-raised md-warn">
-          <md-icon>home</md-icon>
-          <md-tooltip md-delay="500" md-direction="left">My tooltip</md-tooltip>
-        </md-button>
+    <div slot="code">
+      <h2>Code</h2>
+    </div>
 
-        <md-avatar class="md-avatar-icon">
-          <md-icon>person</md-icon>
-          <md-tooltip md-delay="500" md-direction="right">My tooltip</md-tooltip>
-        </md-avatar>
-      </div>
-    </section>
-  </div>
+    <div slot="api">
+
+    </div>
+  </demo-page>
 </template>
 
 <style lang="scss" scoped>
-  section {
-    margin-left: 100px;
-    margin-bottom: 32px;
-  }
-
   .md-button,
   .md-avatar {
     margin: 24px;
   }
 </style>
+
+<script>
+  export default {
+    mounted() {
+      this.$root.pageTitle = 'Tooltip';
+    }
+  };
+</script>
