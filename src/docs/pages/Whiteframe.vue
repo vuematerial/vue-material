7e5171ad492d9e5920271c917745c873f1d81aff
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-19 21:14:47 -0300

    Update whiteframe examples

--- src/docs/pages/Whiteframe.vue
+++ src/docs/pages/Whiteframe.vue
@@ -1,37 +1,63 @@
 <template>
-  <section>
-    <md-whiteframe md-elevation="1"></md-whiteframe>
-    <md-whiteframe md-elevation="2"></md-whiteframe>
-    <md-whiteframe md-elevation="3"></md-whiteframe>
-    <md-whiteframe md-elevation="4"></md-whiteframe>
-    <md-whiteframe md-elevation="5"></md-whiteframe>
-    <md-whiteframe md-elevation="6"></md-whiteframe>
-    <md-whiteframe md-elevation="7"></md-whiteframe>
-    <md-whiteframe md-elevation="8"></md-whiteframe>
-    <md-whiteframe md-elevation="9"></md-whiteframe>
-    <md-whiteframe md-elevation="10"></md-whiteframe>
-    <md-whiteframe md-elevation="11"></md-whiteframe>
-    <md-whiteframe md-elevation="12"></md-whiteframe>
-    <md-whiteframe md-elevation="13"></md-whiteframe>
-    <md-whiteframe md-elevation="14"></md-whiteframe>
-    <md-whiteframe md-elevation="15"></md-whiteframe>
-    <md-whiteframe md-elevation="16"></md-whiteframe>
-    <md-whiteframe md-elevation="17"></md-whiteframe>
-    <md-whiteframe md-elevation="18"></md-whiteframe>
-    <md-whiteframe md-elevation="19"></md-whiteframe>
-    <md-whiteframe md-elevation="20"></md-whiteframe>
-    <md-whiteframe md-elevation="21"></md-whiteframe>
-    <md-whiteframe md-elevation="22"></md-whiteframe>
-    <md-whiteframe md-elevation="23"></md-whiteframe>
-    <md-whiteframe md-elevation="24"></md-whiteframe>
-  </section>
+  <demo-page>
+    <div slot="examples" class="whiteframe-list">
+      <demo-example label="Default">
+        <md-whiteframe md-elevation="1">1dp</md-whiteframe>
+        <md-whiteframe md-elevation="2">2dp</md-whiteframe>
+        <md-whiteframe md-elevation="3">3dp</md-whiteframe>
+        <md-whiteframe md-elevation="4">4dp</md-whiteframe>
+        <md-whiteframe md-elevation="5">5dp</md-whiteframe>
+        <md-whiteframe md-elevation="6">6dp</md-whiteframe>
+        <md-whiteframe md-elevation="7">7dp</md-whiteframe>
+        <md-whiteframe md-elevation="8">8dp</md-whiteframe>
+        <md-whiteframe md-elevation="9">9dp</md-whiteframe>
+        <md-whiteframe md-elevation="10">10dp</md-whiteframe>
+        <md-whiteframe md-elevation="11">11dp</md-whiteframe>
+        <md-whiteframe md-elevation="12">12dp</md-whiteframe>
+        <md-whiteframe md-elevation="13">13dp</md-whiteframe>
+        <md-whiteframe md-elevation="14">14dp</md-whiteframe>
+        <md-whiteframe md-elevation="15">15dp</md-whiteframe>
+        <md-whiteframe md-elevation="16">16dp</md-whiteframe>
+        <md-whiteframe md-elevation="17">17dp</md-whiteframe>
+        <md-whiteframe md-elevation="18">18dp</md-whiteframe>
+        <md-whiteframe md-elevation="19">19dp</md-whiteframe>
+        <md-whiteframe md-elevation="20">20dp</md-whiteframe>
+        <md-whiteframe md-elevation="21">21dp</md-whiteframe>
+        <md-whiteframe md-elevation="22">22dp</md-whiteframe>
+        <md-whiteframe md-elevation="23">23dp</md-whiteframe>
+        <md-whiteframe md-elevation="24">24dp</md-whiteframe>
+      </demo-example>
+    </div>
+
+    <div slot="code">
+      <h2>Code</h2>
+    </div>
+
+    <div slot="api">
+
+    </div>
+  </demo-page>
 </template>
 
-<style lang="scss" scoped>
-  .md-whiteframe {
-    width: 100px;
-    height: 100px;
-    margin: 24px 12px 12px 24px;
-    display: inline-block;
+<style lang="scss">
+  .whiteframe-list .demo-example .demo-example-body {
+    padding-bottom: 48px;
+
+    .md-whiteframe {
+      width: 100px;
+      height: 100px;
+      margin: 24px 12px 12px 24px;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
 </style>
+
+<script>
+  export default {
+    mounted() {
+      this.$root.pageTitle = 'Whiteframe';
+    }
+  };
+</script>
