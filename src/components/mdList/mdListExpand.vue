5d2d655ec2c55c844a7862fc94f17cc682475632
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-25 00:15:53 -0300

    Create expansible lists

---
+++ src/components/mdList/mdListExpand.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="md-list-expand" :style="{ 'margin-bottom': height }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        height: null
+      };
+    },
+    watch: {
+      '$el.offsetHeight': function() {
+        console.log(this);
+      }
+    },
+    mounted() {
+      this.height = -this.$el.offsetHeight + 'px';
+    }
+  };
+</script>
