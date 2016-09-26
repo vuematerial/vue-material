6748f56f56fe5861f6895def27d0b44beb14783a
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-26 12:57:41 -0300

    Start documentation of introduction

--- src/docs/pages/Introduction.vue
+++ src/docs/pages/Introduction.vue
@@ -1,5 +1,8 @@
 <template>
   <single-page class="single-page-home" label="Introduction">
-    <single-page-banner label="Introduction"></single-page-banner>
+    <single-page-banner label="Vue Material"></single-page-banner>
+
+    <p class="introduction">Vue Material is a lightweight implementation <a href="http://material.google.com" target="_blank" rel="noopener">Google Material Design</a> specifications.</p>
+    <p>This project provides reusable components and a series of UI Elements to build crafted Material Design applications. It aims to support the latest version of Vue and to have the same <a href="https://saucelabs.com/u/vuejs" target="_blank" rel="noopener">compatibility</a> with Web Browsers.</p>
   </single-page>
 </template>
