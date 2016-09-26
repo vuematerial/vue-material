5f966430b3a7cb9ae09c5c7f2713eb83c75ad31b
Marcos Moura <marcosvmmoura@gmail.com>
2016-09-26 00:30:43 -0300

    Change subheader tag to li inside lists

--- src/components/mdSubheader/mdSubheader.vue
+++ src/components/mdSubheader/mdSubheader.vue
@@ -1,7 +1,18 @@
-<template>
-  <div class="md-subheader">
-    <slot></slot>
-  </div>
-</template>
-
 <style lang="scss" src="./mdSubheader.scss"></style>
+
+<script>
+  export default {
+    render(createElement) {
+      let tag = 'div';
+      let options = {
+        staticClass: 'md-subheader'
+      };
+
+      if (this.$parent.$options._componentTag === 'md-list') {
+        tag = 'li';
+      }
+
+      return createElement(tag, options, this.$slots.default);
+    }
+  };
+</script>
