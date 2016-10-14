da30a66965c6c620ca6fa749a340842f8a6f5fed
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-14 13:10:58 -0300

    Add theme configuration

--- docs/src/pages/GettingStarted.vue
+++ docs/src/pages/GettingStarted.vue
@@ -40,10 +40,38 @@ Vue.use(VueMaterial.MdIcon);
 Vue.use(VueMaterial.MdSidenav);
 Vue.use(VueMaterial.MdToolbar);
       </code-block>
+    </single-page-section>
 
-      <h3 class="md-subheading">Caveats</h3>
+    <single-page-section label="Apply theme">
+      <p>Right now, to get Vue Material working properly, you'll need configure and apply a default theme.</p>
+      <code-block lang="javascript">
+Vue.material.theme.register('default', {
+  primary: 'cyan',
+  accent: 'pink'
+});
+      </code-block>
 
-      <p>Right now, to get Vue Material working properly, you'll need to use MdTheme and MdInkRipple. The MdTheme will apply the default colors (or the specified ones) and the MdInkRipple will enable ripple effect to Buttons and some other elements.</p>
+      <p>Or you can register multiple themes at once.</p>
+
+      <code-block lang="javascript">
+Vue.material.theme.registerAll({
+  default: {
+    primary: 'cyan',
+    accent: 'pink'
+  },
+  phone: {
+    primary: 'indigo',
+    accent: 'pink'
+  }
+});
+      </code-block>
+
+      <p>Apply your theme using <code>v-md-theme</code> directive:</p>
+
+      <code-block lang="xml">
+&lt;div v-md-theme=&quot;&#039;default&#039;&quot;&gt;&lt;/div&gt;
+&lt;div v-md-theme=&quot;&#039;phone&#039;&quot;&gt;&lt;/div&gt;
+      </code-block>
     </single-page-section>
   </single-page>
 </template>
