2f7c4bd68c8b7c631779523059d13ca4efdd611a
Marcos Moura <mrufino@ciandt.com>
2016-10-14 19:12:48 -0300

    Finish installation docs

--- docs/src/pages/GettingStarted.vue
+++ docs/src/pages/GettingStarted.vue
@@ -20,7 +20,7 @@ var VueMaterial = require(&#039;vue-material&#039;);
       </code-block>
       <small>* Others package managers like JSPM and Bower are not supported yet.</small>
 
-      <p>Or <a href="https://github.com/marcosmoura/vue-material/archive/master.zip" target="_blank" rel="noopener">download</a> from Github and reference the script and the stylesheet in yout HTML:</p>
+      <p>Or <a href="https://github.com/marcosmoura/vue-material/archive/master.zip" target="_blank" rel="noopener">download</a> from Github and reference the script and the stylesheet in your HTML:</p>
       <code-block lang="xml">
 &lt;link rel=&quot;stylesheet&quot; href=&quot;path/to/vue-material.css&quot;&gt;
 &lt;script src=&quot;path/to/vue-material.js&quot;&gt;&lt;/script&gt;
@@ -28,13 +28,13 @@ var VueMaterial = require(&#039;vue-material&#039;);
     </single-page-section>
 
     <single-page-section label="Usage">
-      <p>Enable Vue Material in your application using enableAll method. If you want to enable only some components you can enable them using <code>Vue.use()</code>:</p>
+      <p>Enable Vue Material in your application using <code>Vue.use()</code>. You can always enable individual components:</p>
       <code-block lang="javascript">
 Vue.use(VueMaterial);
 
 // OR
 
-Vue.use(VueMaterial.MdCore); //Required to make all components work
+Vue.use(VueMaterial.MdCore); //Required to boot vue material
 Vue.use(VueMaterial.MdButton);
 Vue.use(VueMaterial.MdIcon);
 Vue.use(VueMaterial.MdSidenav);
@@ -43,7 +43,7 @@ Vue.use(VueMaterial.MdToolbar);
     </single-page-section>
 
     <single-page-section label="Apply theme">
-      <p>Right now, to get Vue Material working properly, you'll need configure and apply a default theme.</p>
+      <p>To get Vue Material working properly, you'll need to configure and apply a default theme.</p>
       <code-block lang="javascript">
 Vue.material.theme.register('default', {
   primary: 'cyan',
