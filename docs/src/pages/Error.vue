cdd0d08d149694207d9ecfc1a009e12c5cd49efa
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-17 16:14:13 -0200

    Finish error page

--- docs/src/pages/Error.vue
+++ docs/src/pages/Error.vue
@@ -1,5 +1,9 @@
 <template>
   <single-page class="single-page-home" label="Error">
     <single-page-banner label="Error 404"></single-page-banner>
+
+    <h2 class="md-headline">Hmmm. Too bad</h2>
+    <p>Are you searching for a new component? Or maybe you have an question? Get in touch!</p>
+    <md-button href="https://github.com/marcosmoura/vue-material/issues/new" target="_blank" rel="noopener">New Issue</md-button>
   </single-page>
 </template>
