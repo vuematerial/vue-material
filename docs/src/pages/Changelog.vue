07ddab37d766ac9c077c546ec6645c9c85fb3bf6
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-17 00:34:55 -0200

    Add initial changelog

--- docs/src/pages/Changelog.vue
+++ docs/src/pages/Changelog.vue
@@ -1,5 +1,38 @@
 <template>
   <single-page class="single-page-home" label="Changelog">
     <single-page-banner label="Changelog"></single-page-banner>
+
+    <section>
+      <h2 class="md-headline">v0.1.0 - Initial Release</h2>
+      <h3 class="md-title">New Components:</h3>
+      <ul>
+        <li>Avatar</li>
+        <li>Bottom Bar</li>
+        <li>Button</li>
+        <li>Button Toggle</li>
+        <li>Checkbox</li>
+        <li>Divider</li>
+        <li>Icon</li>
+        <li>Input</li>
+        <li>List</li>
+        <li>Radio</li>
+        <li>Select</li>
+        <li>Sidenav</li>
+        <li>Subheader</li>
+        <li>Switch</li>
+        <li>Tabs</li>
+        <li>Toolbar</li>
+        <li>Tooltip</li>
+        <li>Whiteframe</li>
+      </ul>
+
+      <h3 class="md-title">UI Elements:</h3>
+      <ul>
+        <li>Tipography</li>
+        <li>Fluid Media</li>
+        <li>Custom Scrollbar</li>
+        <li>Selection Styles</li>
+      </ul>
+    </section>
   </single-page>
 </template>
