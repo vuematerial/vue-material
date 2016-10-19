7fc01a5602a583e27e71aea5940100d232c7575a
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-19 00:43:37 -0200

    Add expansion card and media examples

--- src/components/mdCard/mdCardMedia.vue
+++ src/components/mdCard/mdCardMedia.vue
@@ -7,15 +7,26 @@
 <script>
   export default {
     props: {
-      mdRatio: String
+      mdRatio: String,
+      mdMedium: Boolean,
+      mdBig: Boolean
     },
     computed: {
       classes() {
-        return {
+        let classes = {
           'md-16-9': this.mdRatio === '16:9' || this.mdRatio === '16/9',
           'md-4-3': this.mdRatio === '4:3' || this.mdRatio === '4/3',
           'md-1-1': this.mdRatio === '1:1' || this.mdRatio === '1/1'
         };
+
+        if (this.mdMedium || this.mdBig) {
+          classes = {
+            'md-medium': this.mdMedium,
+            'md-big': this.mdBig
+          };
+        }
+
+        return classes;
       }
     }
   };
---
+++ src/components/mdCard/mdCardMediaActions.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="md-card-media-actions">
+    <slot></slot>
+  </div>
+</template>
