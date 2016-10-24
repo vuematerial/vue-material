d4c22451666b4c527d8e0d6418fb8839a17cbf6b
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-24 00:51:53 -0200

    0.2.0

--- docs/src/pages/Changelog.vue
+++ docs/src/pages/Changelog.vue
@@ -8,10 +8,18 @@
       <ul>
         <li>Cards</li>
       </ul>
+
+      <h3 class="md-title">Changes:</h3>
+      <ul>
+        <li>Add Roboto font and google icons to the docs <strong><a href="https://github.com/marcosmoura/vue-material/commit/421ce7bb21a1489c8f98ee12d0d525eec6b0ee12" target="_blank" rel="noopener">421ce7b</a></strong></li>
+        <li>Explains better how to install and configure vue-material <strong><a href="https://github.com/marcosmoura/vue-material/commit/cc43985fc1d30403a7c95bfeb2dc38bbcc1acb9e" target="_blank" rel="noopener">cc43985</a></strong></li>
+        <li>Emit change &amp; input events from mdTextarea <strong><a href="https://github.com/marcosmoura/vue-material/commit/b474af9fd8d5f4563914e5aae47d14b7c8c62fb2" target="_blank" rel="noopener">b474af9</a></strong> (Thanks to <strong><a href="https://github.com/jvanbrug" target="_blank" rel="noopener">@jvanbrug</a></strong>)</li>
+      </ul>
     </section>
 
     <section>
       <h2 class="md-headline">v0.1.2 - Minor Fixes</h2>
+      <h3 class="md-title">Changes:</h3>
       <ul>
         <li>Fixed tooltips on Firefox</li>
         <li>Misaligned icons inside buttons on Firefox</li>
@@ -21,6 +29,7 @@
 
     <section>
       <h2 class="md-headline">v0.1.1 - Minor Fixes</h2>
+      <h3 class="md-title">Changes:</h3>
       <ul>
         <li>Minor fixes</li>
         <li>Firefox support</li>
@@ -63,3 +72,9 @@
     </section>
   </single-page>
 </template>
+
+<style lang="scss" scoped>
+  section + section {
+    margin-top: 56px;
+  }
+</style>
