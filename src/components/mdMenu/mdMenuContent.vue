9893850a7b3ba5979e55ad8345c996d26e20f0bb
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-24 21:26:26 -0200

    Close content on esc

--- src/components/mdMenu/mdMenuContent.vue
+++ src/components/mdMenu/mdMenuContent.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="md-menu-content">
+  <div class="md-menu-content" @keydown.esc="close" tabindex="-1">
     <slot></slot>
   </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      close() {
+        this.$parent.close();
+      }
+    },
+    mounted() {
+      if (!this.$parent.$el.classList.contains('md-menu')) {
+        this.$destroy();
+
+        throw new Error('You must wrap the md-menu-content in a md-menu');
+      }
+    }
+  };
+</script>
