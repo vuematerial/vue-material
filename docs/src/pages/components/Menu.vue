b564e48c0a2aa6cf4977fb62fb13d688305b6f31
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-24 19:43:55 -0200

    Keep menu content inside viewport

--- docs/src/pages/components/Menu.vue
+++ docs/src/pages/components/Menu.vue
@@ -49,16 +49,6 @@
             <md-menu-item>My Item 3</md-menu-item>
           </md-menu-content>
         </md-menu>
-
-        <md-menu>
-          <md-button md-menu-trigger>Options</md-button>
-
-          <md-menu-content>
-            <md-menu-item>My Item 1</md-menu-item>
-            <md-menu-item>My Item 2</md-menu-item>
-            <md-menu-item>My Item 3</md-menu-item>
-          </md-menu-content>
-        </md-menu>
       </demo-example>
     </div>
 
