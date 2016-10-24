955b1ed40a1ba6a0f24df410054a524ade79939b
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-24 21:26:26 -0200

    Close content on esc

--- src/components/mdMenu/mdMenuItem.vue
+++ src/components/mdMenu/mdMenuItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md-menu-item" v-md-ink-ripple @click="$emit('click')">
+  <div class="md-menu-item" @click="close">
     <div class="md-menu-item-content">
       <slot></slot>
     </div>
@@ -8,6 +8,18 @@
 
 <script>
   export default {
+    methods: {
+      close() {
+        this.$emit('click');
+        this.$parent.close();
+      }
+    },
+    mounted() {
+      if (!this.$parent.$el.classList.contains('md-menu-content')) {
+        this.$destroy();
 
+        throw new Error('You must wrap the md-menu-item in a md-menu-content');
+      }
+    }
   };
 </script>
