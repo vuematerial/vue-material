5aec1b189da7cb4f396b54b8803b9368e3105c4f
Marcos Moura <marcosvmmoura@gmail.com>
2016-10-25 21:10:04 -0200

    Start table component

--- docs/src/pages/components/Table.vue
+++ docs/src/pages/components/Table.vue
@@ -2,7 +2,25 @@
   <demo-page label="Components - Table">
     <div slot="examples">
       <demo-example label="Default">
-        <md-table></md-table>
+        <md-table>
+          <thead>
+            <tr>
+              <th>Head 1</th>
+              <th>Head 2</th>
+              <th>Head 3</th>
+              <th>Head 4</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Cell 1</td>
+              <td>Cell 2</td>
+              <td>Cell 3</td>
+              <td>Cell 4</td>
+            </tr>
+          </tbody>
+        </md-table>
       </demo-example>
     </div>
 
