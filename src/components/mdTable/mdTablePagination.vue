16bc1dbda7ea6ab0e1a45d7ab4cc6e19993d6ecf
Marcos Moura <marcosvmmoura@gmail.com>
2016-11-03 01:36:30 -0200

    Create pagination HTML

--- src/components/mdTable/mdTablePagination.vue
+++ src/components/mdTable/mdTablePagination.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="md-table-pagination">
-    <slot></slot>
+    <span class="md-table-pagination-label">Rows per page:</span>
+
+    <md-select v-model="currentSize" @change="$emit('size')">
+      <md-option value="10">10</md-option>
+      <md-option value="25">25</md-option>
+      <md-option value="50">50</md-option>
+      <md-option value="100">100</md-option>
+    </md-select>
+
+    <span>1-10 of {{ mdTotal }}</span>
+
+    <md-button class="md-icon-button md-table-pagination-previous">
+      <md-icon>keyboard_arrow_left</md-icon>
+    </md-button>
+
+    <md-button class="md-icon-button md-table-pagination-next">
+      <md-icon>keyboard_arrow_right</md-icon>
+    </md-button>
   </div>
 </template>
 
 <script>
   export default {
     props: {
-
+      mdSize: {
+        type: Number,
+        default: 10
+      },
+      mdPage: {
+        type: Number,
+        default: 1
+      },
+      mdTotal: {
+        type: [Number, String],
+        default: 'Many'
+      }
+    },
+    data() {
+      return {
+        currentSize: this.mdSize,
+        currentPage: this.mdPage
+      };
     }
   };
 </script>
