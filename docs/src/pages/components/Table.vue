b1f8f46e47c3e8f009bbfdbcdd64bad181618757
Marcos Moura <marcosvmmoura@gmail.com>
2016-11-14 18:56:33 -0200

    Add table examples

--- docs/src/pages/components/Table.vue
+++ docs/src/pages/components/Table.vue
@@ -165,7 +165,227 @@
     </div>
 
     <div slot="code">
+      <demo-example label="Default">
+<code-block lang="xml">
+&lt;md-table md-sort=&quot;calories&quot;&gt;
+  &lt;md-table-header&gt;
+    &lt;md-table-row&gt;
+      &lt;md-table-head md-sort-by=&quot;dessert&quot;&gt;Dessert (100g serving)&lt;/md-table-head&gt;
+      &lt;md-table-head md-sort-by=&quot;calories&quot; md-numeric md-tooltip=&quot;The total amount of food energy and the given serving size&quot;&gt;Calories (g)&lt;/md-table-head&gt;
+      &lt;md-table-head md-sort-by=&quot;fat&quot; md-numeric&gt;Fat (g)&lt;/md-table-head&gt;
+      &lt;md-table-head md-sort-by=&quot;carbs&quot; md-numeric&gt;Carbs (g)&lt;/md-table-head&gt;
+      &lt;md-table-head md-sort-by=&quot;protein&quot; md-numeric&gt;Protein (g)&lt;/md-table-head&gt;
+      &lt;md-table-head md-sort-by=&quot;sodium&quot; md-numeric&gt;Sodium (mg)&lt;/md-table-head&gt;
+      &lt;md-table-head md-sort-by=&quot;calcium&quot; md-numeric&gt;Calcium (%)&lt;/md-table-head&gt;
+      &lt;md-table-head md-sort-by=&quot;iron&quot; md-numeric&gt;Iron (%)&lt;/md-table-head&gt;
+    &lt;/md-table-row&gt;
+  &lt;/md-table-header&gt;
 
+  &lt;md-table-body&gt;
+    &lt;md-table-row v-for=&quot;(row, index) in 5&quot; :key=&quot;index&quot;&gt;
+      &lt;md-table-cell&gt;Dessert Name&lt;/md-table-cell&gt;
+      &lt;md-table-cell v-for=&quot;(col, index) in 7&quot; :key=&quot;index&quot; md-numeric&gt;10&lt;/md-table-cell&gt;
+    &lt;/md-table-row&gt;
+  &lt;/md-table-body&gt;
+&lt;/md-table&gt;
+</code-block>
+      </demo-example>
+
+      <demo-example label="Within cards with Pagination and Inline Edit">
+<code-block lang="xml">
+&lt;md-table-card&gt;
+  &lt;md-toolbar&gt;
+    &lt;h1 class=&quot;md-title&quot;&gt;Nutrition&lt;/h1&gt;
+    &lt;md-button class=&quot;md-icon-button&quot;&gt;
+      &lt;md-icon&gt;filter_list&lt;/md-icon&gt;
+    &lt;/md-button&gt;
+
+    &lt;md-button class=&quot;md-icon-button&quot;&gt;
+      &lt;md-icon&gt;search&lt;/md-icon&gt;
+    &lt;/md-button&gt;
+  &lt;/md-toolbar&gt;
+
+  &lt;md-table md-sort=&quot;dessert&quot; md-sort-type=&quot;desc&quot; @select=&quot;onSelect&quot; @sort=&quot;onSort&quot;&gt;
+    &lt;md-table-header&gt;
+      &lt;md-table-row&gt;
+        &lt;md-table-head md-sort-by=&quot;dessert&quot;&gt;Dessert (100g serving)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;calories&quot; md-numeric md-tooltip=&quot;The total amount of food energy and the given serving size&quot;&gt;Calories (g)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;fat&quot; md-numeric&gt;Fat (g)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;carbs&quot; md-numeric&gt;Carbs (g)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;protein&quot; md-numeric&gt;Protein (g)&lt;/md-table-head&gt;
+        &lt;md-table-head&gt;
+          &lt;md-icon&gt;message&lt;/md-icon&gt;
+          &lt;span&gt;Comments&lt;/span&gt;
+        &lt;/md-table-head&gt;
+      &lt;/md-table-row&gt;
+    &lt;/md-table-header&gt;
+
+    &lt;md-table-body&gt;
+      &lt;md-table-row v-for=&quot;(row, rowIndex) in nutrition&quot; :key=&quot;rowIndex&quot; :md-item=&quot;row&quot; md-auto-select md-selection&gt;
+        &lt;md-table-cell v-for=&quot;(column, columnIndex) in row&quot; :key=&quot;columnIndex&quot; :md-numeric=&quot;columnIndex !== &#039;dessert&#039; &amp;&amp; columnIndex !== &#039;comment&#039;&quot; v-if=&quot;columnIndex !== &#039;type&#039;&quot;&gt;
+          &lt;md-table-edit
+            :md-name=&quot;&#039;comment&#039; + columnIndex&quot;
+            :md-id=&quot;&#039;comment&#039; + columnIndex&quot;
+            md-placeholder=&quot;Add a comment&quot;
+            md-maxlength=&quot;120&quot;
+            v-model=&quot;nutrition[rowIndex].comment&quot;
+            v-if=&quot;columnIndex === &#039;comment&#039;&quot;&gt;&lt;/md-table-edit&gt;
+
+          &lt;span v-if=&quot;columnIndex !== &#039;comment&#039;&quot;&gt;{{ column }}&lt;/span&gt;
+        &lt;/md-table-cell&gt;
+      &lt;/md-table-row&gt;
+    &lt;/md-table-body&gt;
+  &lt;/md-table&gt;
+
+  &lt;md-table-pagination
+    md-size=&quot;10&quot;
+    md-total=&quot;5&quot;
+    md-page=&quot;1&quot;
+    md-label=&quot;Rows&quot;
+    md-separator=&quot;of&quot;
+    :md-page-options=&quot;[5, 10, 25, 50]&quot;
+    @pagination=&quot;onPagination&quot;&gt;&lt;/md-table-pagination&gt;
+&lt;/md-table-card&gt;
+</code-block>
+
+<code-block lang="javscript">
+export default {
+  data: () => ({
+    nutrition: [
+      {
+        dessert: 'Frozen yogurt',
+        type: 'ice_cream',
+        calories: '159',
+        fat: '6.0',
+        carbs: '24',
+        protein: '4.0',
+        comment: 'Icy'
+      },
+      {
+        dessert: 'Ice cream sandwich',
+        type: 'ice_cream',
+        calories: '237',
+        fat: '9.0',
+        carbs: '37',
+        protein: '4.3',
+        comment: 'Super Tasty'
+      },
+      {
+        dessert: 'Eclair',
+        type: 'pastry',
+        calories: '262',
+        fat: '16.0',
+        carbs: '24',
+        protein: '6.0',
+        comment: ''
+      },
+      {
+        dessert: 'Cupcake',
+        type: 'pastry',
+        calories: '305',
+        fat: '3.7',
+        carbs: '67',
+        protein: '4.3',
+        comment: ''
+      },
+      {
+        dessert: 'Gingerbread',
+        type: 'other',
+        calories: '356',
+        fat: '16.0',
+        carbs: '49',
+        protein: '3.9',
+        comment: ''
+      }
+    ],
+    selectedData: [],
+    sort: {},
+    page: {}
+  }),
+  methods: {
+    onSelect(data) {
+      this.selectedData = data;
+      this.$forceUpdate();
+    },
+    onSort(sort) {
+      this.sort = sort;
+    },
+    onPagination(page) {
+      this.page = page;
+    }
+  }
+};
+</code-block>
+      </demo-example>
+
+      <demo-example label="Inline Menus and Edit Icon">
+<code-block lang="xml">
+&lt;md-table-card&gt;
+  &lt;md-toolbar&gt;
+    &lt;h1 class=&quot;md-title&quot;&gt;Nutrition&lt;/h1&gt;
+    &lt;md-button class=&quot;md-icon-button&quot;&gt;
+      &lt;md-icon&gt;filter_list&lt;/md-icon&gt;
+    &lt;/md-button&gt;
+
+    &lt;md-button class=&quot;md-icon-button&quot;&gt;
+      &lt;md-icon&gt;search&lt;/md-icon&gt;
+    &lt;/md-button&gt;
+  &lt;/md-toolbar&gt;
+
+  &lt;md-table-alternate-header md-selected-label=&quot;selected&quot;&gt;
+    &lt;md-button class=&quot;md-icon-button&quot;&gt;
+      &lt;md-icon&gt;delete&lt;/md-icon&gt;
+    &lt;/md-button&gt;
+
+    &lt;md-button class=&quot;md-icon-button&quot;&gt;
+      &lt;md-icon&gt;more_vert&lt;/md-icon&gt;
+    &lt;/md-button&gt;
+  &lt;/md-table-alternate-header&gt;
+
+  &lt;md-table md-sort=&quot;calories&quot;&gt;
+    &lt;md-table-header&gt;
+      &lt;md-table-row&gt;
+        &lt;md-table-head md-sort-by=&quot;dessert&quot;&gt;Dessert (100g serving)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;type&quot; width=&quot;100px&quot;&gt;Type&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;calories&quot; md-numeric md-tooltip=&quot;The total amount of food energy and the given serving size&quot;&gt;Calories (g)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;fat&quot; md-numeric&gt;Fat (g)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;carbs&quot; md-numeric&gt;Carbs (g)&lt;/md-table-head&gt;
+        &lt;md-table-head md-sort-by=&quot;protein&quot; md-numeric&gt;Protein (g)&lt;/md-table-head&gt;
+        &lt;md-table-head&gt;
+          &lt;md-icon&gt;message&lt;/md-icon&gt;
+          &lt;span&gt;Comments&lt;/span&gt;
+        &lt;/md-table-head&gt;
+      &lt;/md-table-row&gt;
+    &lt;/md-table-header&gt;
+
+    &lt;md-table-body&gt;
+      &lt;md-table-row v-for=&quot;(row, rowIndex) in nutrition&quot; :key=&quot;rowIndex&quot; :md-item=&quot;row&quot; md-selection&gt;
+        &lt;md-table-cell v-for=&quot;(column, columnIndex) in row&quot; :key=&quot;columnIndex&quot; :md-numeric=&quot;columnIndex !== &#039;dessert&#039; &amp;&amp; columnIndex !== &#039;comment&#039; &amp;&amp; columnIndex !== &#039;type&#039;&quot;&gt;
+          &lt;span v-if=&quot;columnIndex === &#039;comment&#039;&quot;&gt;{{ column }}&lt;/span&gt;
+
+          &lt;md-button class=&quot;md-icon-button&quot; v-if=&quot;columnIndex === &#039;comment&#039;&quot;&gt;
+            &lt;md-icon&gt;edit&lt;/md-icon&gt;
+          &lt;/md-button&gt;
+
+          &lt;md-select
+            placeholder=&quot;Type&quot;
+            :name=&quot;&#039;type&#039; + columnIndex&quot;
+            :id=&quot;&#039;type&#039; + columnIndex&quot;
+            v-model=&quot;nutrition[rowIndex].type&quot;
+            v-if=&quot;columnIndex === &#039;type&#039;&quot;&gt;
+            &lt;md-option value=&quot;ice_cream&quot;&gt;Ice Cream&lt;/md-option&gt;
+            &lt;md-option value=&quot;pastry&quot;&gt;Pastry&lt;/md-option&gt;
+            &lt;md-option value=&quot;other&quot;&gt;Other&lt;/md-option&gt;
+          &lt;/md-select&gt;
+
+          &lt;span v-if=&quot;columnIndex !== &#039;type&#039; &amp;&amp; columnIndex !== &#039;comment&#039;&quot;&gt;{{ column }}&lt;/span&gt;
+        &lt;/md-table-cell&gt;
+      &lt;/md-table-row&gt;
+    &lt;/md-table-body&gt;
+  &lt;/md-table&gt;
+&lt;/md-table-card&gt;
+</code-block>
+      </demo-example>
     </div>
 
     <div slot="api">
