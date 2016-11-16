1b3525ff03c117215351aa6ad6a30332af44ec00
Marcos Moura <marcosvmmoura@gmail.com>
2016-11-16 17:56:26 -0200

    Init dialog

---
+++ docs/src/pages/components/Dialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <demo-page label="Components - Dialog">
+    <div slot="examples">
+      <demo-example label="Default" size="2">
+        <md-dialog ref="dialog">
+          <div class="md-dialog-content">
+            <form>
+              <md-input-container>
+                <label>Test</label>
+                <md-input></md-input>
+              </md-input-container>
+            </form>
+          </div>
+
+          <md-button class="md-primary md-raised" @click="closeDialog">Close</md-button>
+        </md-dialog>
+
+        <md-button class="md-primary md-raised" @click="openDialog">Open</md-button>
+      </demo-example>
+    </div>
+
+    <div slot="code">
+      <demo-example label="Default">
+        <code-block lang="xml">
+        </code-block>
+      </demo-example>
+    </div>
+
+    <div slot="api">
+
+    </div>
+  </demo-page>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+  export default {
+    methods: {
+      openDialog() {
+        this.$refs.dialog.open();
+      },
+      closeDialog() {
+        this.$refs.dialog.close();
+      }
+    }
+  };
+</script>
