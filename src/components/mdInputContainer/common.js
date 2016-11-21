5efddcca95dc2d986597cea14ab35a8056837fbe
Marcos Moura <marcosvmmoura@gmail.com>
2016-11-21 00:04:31 -0200

    Fix input getting wrong value after manual update #100 #96

--- src/components/mdInputContainer/common.js
+++ src/components/mdInputContainer/common.js
@@ -29,7 +29,7 @@ export default {
       this.parentContainer.counterLength = this.maxlength;
     },
     setParentValue() {
-      this.parentContainer.setValue(this.$el.value);
+      this.parentContainer.setValue(this.value);
     },
     setParentDisabled() {
       this.parentContainer.isDisabled = this.disabled;
@@ -49,9 +49,9 @@ export default {
     },
     onInput() {
       this.setParentValue();
-      this.parentContainer.inputLength = this.$el.value.length;
-      this.$emit('change', this.$el.value);
-      this.$emit('input', this.$el.value);
+      this.parentContainer.inputLength = this.value.length;
+      this.$emit('change', this.value);
+      this.$emit('input', this.value);
     }
   }
 };
