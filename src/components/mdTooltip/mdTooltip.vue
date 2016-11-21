8331d9e2ac905cf39379ed677d0fd307a9c74400
Marcos Moura <marcosvmmoura@gmail.com>
2016-11-21 04:01:49 -0200

    Improve tooltip styling

--- src/components/mdTooltip/mdTooltip.vue
+++ src/components/mdTooltip/mdTooltip.vue
@@ -23,13 +23,14 @@
     },
     data: () => ({
       active: false,
+      parentClass: null,
       transitionOff: false,
       topPosition: false,
       leftPosition: false
     }),
     computed: {
       classes() {
-        return {
+        const cssClasses = {
           'md-active': this.active,
           'md-transition-off': this.transitionOff,
           'md-tooltip-top': this.mdDirection === 'top',
@@ -37,6 +38,12 @@
           'md-tooltip-bottom': this.mdDirection === 'bottom',
           'md-tooltip-left': this.mdDirection === 'left'
         };
+
+        if (this.parentClass) {
+          cssClasses[this.parentClass] = true;
+        }
+
+        return cssClasses;
       },
       style() {
         return {
@@ -52,6 +59,13 @@
       }
     },
     methods: {
+      removeTooltips() {
+        const tooltips = [...document.querySelectorAll('.md-tooltip')];
+
+        tooltips.forEach((tooltip) => {
+          tooltip.parentNode.removeChild(tooltip);
+        });
+      },
       calculateTooltipPosition() {
         let position = this.parentElement.getBoundingClientRect();
         let cssPosition = {};
@@ -90,18 +104,23 @@
         this.topPosition = cssPosition.top;
         this.leftPosition = cssPosition.left;
       },
-      removeTooltips() {
-        const tooltips = [...document.querySelectorAll('.md-tooltip')];
+      generateTooltipClasses() {
+        let classes = [];
 
-        tooltips.forEach((tooltip) => {
-          tooltip.parentNode.removeChild(tooltip);
+        this.parentElement.classList.forEach((cssClass) => {
+          if (cssClass.indexOf('md-') >= 0 && cssClass !== 'md-active') {
+            classes.push(cssClass + '-tooltip');
+          }
         });
+
+        this.parentClass = classes.join(' ');
       },
       open() {
         this.removeTooltips();
         this.rootElement.appendChild(this.tooltipElement);
         getComputedStyle(this.tooltipElement).top;
         this.transitionOff = true;
+        this.generateTooltipClasses();
         this.calculateTooltipPosition();
 
         window.setTimeout(() => {
