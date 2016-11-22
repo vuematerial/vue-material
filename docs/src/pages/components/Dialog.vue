3ec365c1ccd2d27b31a378e4f40b8570bf2980d1
Marcos Moura <marcosvmmoura@gmail.com>
2016-11-21 22:30:32 -0200

    Create styles for actions and content

--- docs/src/pages/components/Dialog.vue
+++ docs/src/pages/components/Dialog.vue
@@ -3,34 +3,39 @@
     <div slot="examples">
       <demo-example label="Default" height="500">
         <md-dialog ref="dialog1">
-          <div class="md-dialog-content">
-            <form>
-              <md-input-container>
-                <label>Test</label>
-                <md-input></md-input>
-              </md-input-container>
-            </form>
-          </div>
+          <md-dialog-content>
+            <h2 class="md-title">Use Google's location service?</h2>
 
-          <md-button class="md-primary" @click="closeDialog('dialog1')">Close</md-button>
+            <p>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</p>
+          </md-dialog-content>
+
+          <md-dialog-actions>
+            <md-button class="md-primary" @click="closeDialog('dialog1')">Disagree</md-button>
+            <md-button class="md-primary" @click="closeDialog('dialog1')">Agree</md-button>
+          </md-dialog-actions>
         </md-dialog>
 
         <md-dialog md-open-from="#trigger" md-close-to="#trigger" ref="dialog2">
-          <div class="md-dialog-content">
+          <md-dialog-content>
+            <h2 class="md-title">Create new note</h2>
+
             <form>
               <md-input-container>
-                <label>Test</label>
-                <md-input></md-input>
+                <label>Note</label>
+                <md-textarea></md-textarea>
               </md-input-container>
             </form>
-          </div>
+          </md-dialog-content>
 
-          <md-button class="md-primary" @click="closeDialog('dialog2')">Close</md-button>
+          <md-dialog-actions>
+            <md-button class="md-primary" @click="closeDialog('dialog2')">Cancel</md-button>
+            <md-button class="md-primary" @click="closeDialog('dialog2')">Create</md-button>
+          </md-dialog-actions>
         </md-dialog>
 
         <md-button class="md-primary md-raised" @click="openDialog('dialog1')">Simple</md-button>
         <md-button class="md-fab md-fab-bottom-right" id="trigger" @click="openDialog('dialog2')">
-          <md-icon>add<md-icon>
+          <md-icon>add</md-icon>
         </md-button>
       </demo-example>
     </div>
--- src/components/mdDialog/index.js
+++ src/components/mdDialog/index.js
@@ -1,8 +1,12 @@
 import mdDialog from './mdDialog.vue';
+import mdDialogContent from './mdDialogContent.vue';
+import mdDialogActions from './mdDialogActions.vue';
 import mdDialogTheme from './mdDialog.theme';
 
 export default function install(Vue) {
   Vue.component('md-dialog', Vue.extend(mdDialog));
+  Vue.component('md-dialog-content', Vue.extend(mdDialogContent));
+  Vue.component('md-dialog-actions', Vue.extend(mdDialogActions));
 
   Vue.material.styles.push(mdDialogTheme);
 }
