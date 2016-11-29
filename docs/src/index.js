6dc7efb10b7334dd76cdde3620f69c2e6fa5f958
Marcos Moura <marcosvmmoura@gmail.com>
2016-11-29 02:35:11 -0200

    remove unused code and rename components

--- docs/src/index.js
+++ docs/src/index.js
@@ -11,47 +11,38 @@ import PageContent from './components/PageContent';
 import DocsComponent from './components/DocsComponent';
 import ExampleBox from './components/ExampleBox';
 import ApiTable from './components/ApiTable';
-
-import SinglePage from './components/single-page';
-import SinglePageBanner from './components/single-page-banner';
-import SinglePageSection from './components/single-page-section';
-import DemoPage from './components/demo-page';
-import DemoExample from './components/demo-example';
-import CodeBlock from './components/code-block';
+import CodeBlock from './components/CodeBlock';
+import DemoPage from './components/DemoPage';
+import DemoExample from './components/DemoExample';
 
 
 Vue.component('page-content', PageContent);
 Vue.component('docs-component', DocsComponent);
 Vue.component('example-box', ExampleBox);
 Vue.component('api-table', ApiTable);
-
-Vue.component('single-page', SinglePage);
-Vue.component('single-page-banner', SinglePageBanner);
-Vue.component('single-page-section', SinglePageSection);
+Vue.component('code-block', CodeBlock);
 Vue.component('demo-page', DemoPage);
 Vue.component('demo-example', DemoExample);
-Vue.component('code-block', CodeBlock);
 
 Vue.use(VueRouter);
 
 let router = new VueRouter({
-  base: '/vue-material/',
   routes
 });
 
 let Docs = Vue.component('app', App);
 let handleSectionTheme = (currentRoute) => {
-  if (currentRoute.name.indexOf('getting') >= 0) {
+  if (currentRoute.name === 'getting-started') {
     Docs.theme = 'indigo';
   } else if (currentRoute.name.indexOf('themes') >= 0) {
     Docs.theme = 'teal';
   } else if (currentRoute.name.indexOf('ui-elements') >= 0) {
     Docs.theme = 'blue-grey';
-  } else if (currentRoute.name.indexOf('changelog') >= 0) {
+  } else if (currentRoute.name === 'changelog') {
     Docs.theme = 'orange';
-  } else if (currentRoute.name.indexOf('about') >= 0) {
+  } else if (currentRoute.name === 'about') {
     Docs.theme = 'green';
-  } else if (currentRoute.name.indexOf('error') >= 0) {
+  } else if (currentRoute.name === 'error') {
     Docs.theme = 'red';
   } else {
     Docs.theme = 'default';
