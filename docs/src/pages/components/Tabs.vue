d59d3f58e0c4b5e16ce409da8a00c9ea905a70b4
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-03 17:45:59 -0200

    recreate tabs

--- docs/src/pages/components/Tabs.vue
+++ docs/src/pages/components/Tabs.vue
@@ -1,6 +1,6 @@
 <template>
   <page-content page-title="Components - Tabs">
-    <!-- <docs-component>
+    <docs-component>
       <div slot="description">
         <p>Tabs enable content organization at a high level, such as switching between views, data sets, or functional aspects of an app.</p>
         <p>The following classes can be applied to change the color palette:</p>
@@ -123,7 +123,7 @@
       <div slot="example">
         <example-box card-title="Default">
           <div slot="demo">
-            <md-tabs>
+            <md-tabs :md-dynamic-height="false">
               <md-tab id="movies" md-label="Movies">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
               </md-tab>
@@ -167,19 +167,23 @@
           </div>
         </example-box>
 
-        <example-box card-title="Fixed with only icons">
+        <example-box card-title="Fixed">
           <div slot="demo">
-            <md-tabs md-fixed>
-              <md-tab md-icon="phone">
+            <md-tabs :md-dynamic-height="false" md-fixed>
+              <md-tab id="movies" md-label="Movies">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
               </md-tab>
 
-              <md-tab md-icon="favorite">
+              <md-tab id="music" md-label="Music">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
               </md-tab>
 
-              <md-tab md-icon="near_me">
+              <md-tab id="books" md-label="Books">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+              </md-tab>
+
+              <md-tab id="pictures" md-label="Pictures">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
               </md-tab>
             </md-tabs>
@@ -188,16 +192,20 @@
           <div slot="code">
             <code-block lang="xml">
               &lt;md-tabs md-fixed&gt;
-                &lt;md-tab md-icon=&quot;phone&quot;&gt;
+                &lt;md-tab id=&quot;movies&quot; md-label=&quot;Movies&quot;&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.&lt;/p&gt;
                 &lt;/md-tab&gt;
 
-                &lt;md-tab md-icon=&quot;favorite&quot;&gt;
+                &lt;md-tab id=&quot;music&quot; md-label=&quot;Music&quot;&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.&lt;/p&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.&lt;/p&gt;
                 &lt;/md-tab&gt;
 
-                &lt;md-tab md-icon=&quot;near_me&quot;&gt;
+                &lt;md-tab id=&quot;books&quot; md-label=&quot;Books&quot;&gt;
+                  &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.&lt;/p&gt;
+                &lt;/md-tab&gt;
+
+                &lt;md-tab id=&quot;pictures&quot; md-label=&quot;Pictures&quot;&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.&lt;/p&gt;
                 &lt;/md-tab&gt;
               &lt;/md-tabs&gt;
@@ -207,7 +215,7 @@
 
         <example-box card-title="Centered with Text and Icon">
           <div slot="demo">
-            <md-tabs md-centered>
+            <md-tabs :md-dynamic-height="false" md-centered>
               <md-tab md-label="Movies" md-icon="ondemand_video">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
               </md-tab>
@@ -251,23 +259,19 @@
           </div>
         </example-box>
 
-        <example-box card-title="Aligned to the right">
+        <example-box card-title="Aligned to the right with only icons">
           <div slot="demo">
-            <md-tabs md-right>
-              <md-tab id="movies" md-label="Movies">
+            <md-tabs :md-dynamic-height="false" md-right>
+              <md-tab md-icon="phone">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
               </md-tab>
 
-              <md-tab id="music" md-label="Music">
+              <md-tab md-icon="favorite">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
               </md-tab>
 
-              <md-tab id="books" md-label="Books">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
-              </md-tab>
-
-              <md-tab id="pictures" md-label="Pictures">
+              <md-tab md-icon="near_me">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
               </md-tab>
             </md-tabs>
@@ -276,233 +280,23 @@
           <div slot="code">
             <code-block lang="xml">
               &lt;md-tabs md-right&gt;
-                &lt;md-tab id=&quot;movies&quot; md-label=&quot;Movies&quot;&gt;
+                &lt;md-tab md-icon=&quot;phone&quot;&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.&lt;/p&gt;
                 &lt;/md-tab&gt;
 
-                &lt;md-tab id=&quot;music&quot; md-label=&quot;Music&quot;&gt;
+                &lt;md-tab md-icon=&quot;favorite&quot;&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.&lt;/p&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.&lt;/p&gt;
                 &lt;/md-tab&gt;
 
-                &lt;md-tab id=&quot;books&quot; md-label=&quot;Books&quot;&gt;
-                  &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.&lt;/p&gt;
-                &lt;/md-tab&gt;
-
-                &lt;md-tab id=&quot;pictures&quot; md-label=&quot;Pictures&quot;&gt;
+                &lt;md-tab md-icon=&quot;near_me&quot;&gt;
                   &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.&lt;/p&gt;
                 &lt;/md-tab&gt;
               &lt;/md-tabs&gt;
-            </code-block>
-          </div>
-        </example-box>
-
-        <example-box card-title="Playground">
-          <div slot="demo">
-            <div class="playground">
-              <md-subheader>Tabs Attributes</md-subheader>
-              <md-checkbox id="fixed" v-model="playground.fixed">Fixed</md-checkbox>
-              <md-checkbox id="centered" v-model="playground.centered">Centered</md-checkbox>
-              <md-input-container>
-                <label for="shadow">Shadow</label>
-                <md-input type="number" id="shadow" v-model="playground.shadow" min="0" max="24"></md-input>
-              </md-input-container>
-
-              <md-subheader>Theme</md-subheader>
-              <md-radio v-model="playground.theme" id="theme1" name="theme" md-value="default">Default</md-radio>
-              <md-radio v-model="playground.theme" id="theme2" name="theme" md-value="green">Green</md-radio>
-              <md-radio v-model="playground.theme" id="theme3" name="theme" md-value="cyan">Cyan</md-radio>
-              <md-radio v-model="playground.theme" id="theme4" name="theme" md-value="brown">Brown</md-radio>
-
-              <md-subheader>Colors</md-subheader>
-              <md-radio v-model="playground.color" id="color1" name="color" md-value="0">Default</md-radio>
-              <md-radio v-model="playground.color" id="color2" name="color" md-value="1">Accent</md-radio>
-              <md-radio v-model="playground.color" id="color3" name="color" md-value="2">Warn</md-radio>
-              <md-radio v-model="playground.color" id="color4" name="color" md-value="3">Transparent</md-radio>
-
-              <md-subheader>Second Tab</md-subheader>
-              <md-checkbox id="disabled" v-model="playground.tabs[1].disabled">Disabled</md-checkbox>
-              <md-checkbox id="active" v-model="playground.tabs[1].active">Active</md-checkbox>
-            </div>
-
-            <md-tabs
-              :class="{
-                'md-accent': playground.color === '1',
-                'md-warn': playground.color === '2',
-                'md-transparent': playground.color === '3'
-              }"
-              :md-fixed="playground.fixed"
-              :md-centered="playground.centered"
-              :md-elevation="playground.shadow"
-              v-md-theme="playground.theme">
-              <md-tab v-for="(tab, index) in playground.tabs" :md-label="tab.label" :md-icon="tab.icon" :md-disabled="tab.disabled" :md-active="tab.active">
-                <md-input-container>
-                  <label :for="'label' + index">Label</label>
-                  <md-input type="text" :id="'label' + index" v-model="tab.label"></md-input>
-                </md-input-container>
-
-                <md-input-container>
-                  <label :for="'icon' + index">Icon</label>
-                  <md-input type="text" :id="'icon' + index" v-model="tab.icon"></md-input>
-                </md-input-container>
-              </md-tab>
-            </md-tabs>
-          </div>
-
-          <div slot="code">
-            <code-block lang="xml">
-              &lt;div class=&quot;playground&quot;&gt;
-                &lt;md-subheader&gt;Tabs Attributes&lt;/md-subheader&gt;
-                &lt;md-checkbox id=&quot;fixed&quot; v-model=&quot;playground.fixed&quot;&gt;Fixed&lt;/md-checkbox&gt;
-                &lt;md-checkbox id=&quot;centered&quot; v-model=&quot;playground.centered&quot;&gt;Centered&lt;/md-checkbox&gt;
-                &lt;md-input-container&gt;
-                  &lt;label for=&quot;shadow&quot;&gt;Shadow&lt;/label&gt;
-                  &lt;md-input type=&quot;number&quot; id=&quot;shadow&quot; v-model=&quot;playground.shadow&quot; min=&quot;0&quot; max=&quot;24&quot;&gt;&lt;/md-input&gt;
-                &lt;/md-input-container&gt;
-
-                &lt;md-subheader&gt;Theme&lt;/md-subheader&gt;
-                &lt;md-radio v-model=&quot;playground.theme&quot; id=&quot;theme1&quot; name=&quot;theme&quot; md-value=&quot;default&quot;&gt;Default&lt;/md-radio&gt;
-                &lt;md-radio v-model=&quot;playground.theme&quot; id=&quot;theme2&quot; name=&quot;theme&quot; md-value=&quot;green&quot;&gt;Green&lt;/md-radio&gt;
-                &lt;md-radio v-model=&quot;playground.theme&quot; id=&quot;theme3&quot; name=&quot;theme&quot; md-value=&quot;cyan&quot;&gt;Cyan&lt;/md-radio&gt;
-                &lt;md-radio v-model=&quot;playground.theme&quot; id=&quot;theme4&quot; name=&quot;theme&quot; md-value=&quot;brown&quot;&gt;Brown&lt;/md-radio&gt;
-
-                &lt;md-subheader&gt;Colors&lt;/md-subheader&gt;
-                &lt;md-radio v-model=&quot;playground.color&quot; id=&quot;color1&quot; name=&quot;color&quot; md-value=&quot;0&quot;&gt;Default&lt;/md-radio&gt;
-                &lt;md-radio v-model=&quot;playground.color&quot; id=&quot;color2&quot; name=&quot;color&quot; md-value=&quot;1&quot;&gt;Accent&lt;/md-radio&gt;
-                &lt;md-radio v-model=&quot;playground.color&quot; id=&quot;color3&quot; name=&quot;color&quot; md-value=&quot;2&quot;&gt;Warn&lt;/md-radio&gt;
-                &lt;md-radio v-model=&quot;playground.color&quot; id=&quot;color4&quot; name=&quot;color&quot; md-value=&quot;3&quot;&gt;Transparent&lt;/md-radio&gt;
-
-                &lt;md-subheader&gt;Second Tab&lt;/md-subheader&gt;
-                &lt;md-checkbox id=&quot;disabled&quot; v-model=&quot;playground.tabs[1].disabled&quot;&gt;Disabled&lt;/md-checkbox&gt;
-                &lt;md-checkbox id=&quot;active&quot; v-model=&quot;playground.tabs[1].active&quot;&gt;Active&lt;/md-checkbox&gt;
-              &lt;/div&gt;
-
-              &lt;md-tabs
-                :class=&quot;{
-                  &#039;md-accent&#039;: playground.color === &#039;1&#039;,
-                  &#039;md-warn&#039;: playground.color === &#039;2&#039;,
-                  &#039;md-transparent&#039;: playground.color === &#039;3&#039;
-                }&quot;
-                :md-fixed=&quot;playground.fixed&quot;
-                :md-centered=&quot;playground.centered&quot;
-                :md-elevation=&quot;playground.shadow&quot;
-                v-md-theme=&quot;playground.theme&quot;&gt;
-                &lt;md-tab v-for=&quot;(tab, index) in playground.tabs&quot; :md-label=&quot;tab.label&quot; :md-icon=&quot;tab.icon&quot; :md-disabled=&quot;tab.disabled&quot; :md-active=&quot;tab.active&quot;&gt;
-                  &lt;md-input-container&gt;
-                    &lt;label :for=&quot;&#039;label&#039; + index&quot;&gt;Label&lt;/label&gt;
-                    &lt;md-input type=&quot;text&quot; :id=&quot;&#039;label&#039; + index&quot; v-model=&quot;tab.label&quot;&gt;&lt;/md-input&gt;
-                  &lt;/md-input-container&gt;
-
-                  &lt;md-input-container&gt;
-                    &lt;label :for=&quot;&#039;icon&#039; + index&quot;&gt;Icon&lt;/label&gt;
-                    &lt;md-input type=&quot;text&quot; :id=&quot;&#039;icon&#039; + index&quot; v-model=&quot;tab.icon&quot;&gt;&lt;/md-input&gt;
-                  &lt;/md-input-container&gt;
-                &lt;/md-tab&gt;
-              &lt;/md-tabs&gt;
-            </code-block>
-
-            <code-block lang="javascript">
-              export default {
-                data() {
-                  return {
-                    playground: {
-                      color: '3',
-                      fixed: true,
-                      centered: false,
-                      shadow: 0,
-                      theme: 'default',
-                      tabs: [
-                        {
-                          label: 'Phone',
-                          icon: 'phone',
-                          active: false,
-                          disabled: false
-                        },
-                        {
-                          label: 'Favorites',
-                          icon: 'favorite',
-                          active: false,
-                          disabled: false
-                        },
-                        {
-                          label: 'Near me',
-                          icon: 'near_me',
-                          active: true,
-                          disabled: false
-                        }
-                      ]
-                    }
-                  };
-                }
-              };
             </code-block>
           </div>
         </example-box>
       </div>
-    </docs-component> -->
-
-    <md-tabs>
-      <md-tab id="tab-1" md-label="Test 1">
-        <p>Optio, assumenda placeat laboriosam incidunt obcaecati nisi, ipsa earum rem cumque nesciunt iste animi recusandae! Nostrum vero quod consequuntur enim quo cum.</p>
-      </md-tab>
-
-      <md-tab id="tab-2" md-label="Test 2">
-        <p>Animi architecto blanditiis nihil aliquam nemo aperiam quo asperiores quam suscipit quae labore in, qui odit beatae assumenda, accusamus, ex sapiente fugit?</p>
-      </md-tab>
-
-      <md-tab id="tab-3" md-label="Test 3">
-        <p>Excepturi, blanditiis in a non ipsa. Praesentium atque maxime officia amet porro veritatis, vitae quisquam ex magni nisi sed. Asperiores, vel aut.</p>
-      </md-tab>
-    </md-tabs>
+    </docs-component>
   </page-content>
 </template>
-
-<style lang="scss" scoped>
-  .playground {
-    padding: 0 16px 16px;
-
-    .md-subheader {
-      padding: 16px 0 0;
-    }
-  }
-
-  div.examples .demo-example .demo-example-body {
-    padding: 0;
-  }
-</style>
-
-<script>
-  export default {
-    data() {
-      return {
-        playground: {
-          color: '3',
-          fixed: true,
-          centered: false,
-          shadow: 0,
-          theme: 'default',
-          tabs: [
-            {
-              label: 'Phone',
-              icon: 'phone',
-              active: false,
-              disabled: false
-            },
-            {
-              label: 'Favorites',
-              icon: 'favorite',
-              active: false,
-              disabled: false
-            },
-            {
-              label: 'Near me',
-              icon: 'near_me',
-              active: true,
-              disabled: false
-            }
-          ]
-        }
-      };
-    }
-  };
-</script>
