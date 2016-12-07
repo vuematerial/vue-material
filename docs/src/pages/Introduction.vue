9df9f27a4a8bf5307ee83112cab0bc27d6e02424
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-07 19:18:49 -0200

    create custom tags in whiteframe component (#170)
    * create custom tags in whiteframe
    
    * add custom tag in api. fix #169
    
    * add custom tag in examples. fix #169

--- docs/src/pages/Introduction.vue
+++ docs/src/pages/Introduction.vue
@@ -1,31 +1,33 @@
 <template>
   <page-content page-title="Introduction">
     <div class="main-content">
-      <div class="button-actions">
-        <div class="example">
-          <img src="assets/vue-material-example.png" alt="Material Design">
+      <div class="introduction">
+        <div class="button-actions">
+          <div class="example">
+            <img src="assets/vue-material-example.png" alt="Material Design">
+          </div>
+
+          <div class="introduction">Build well-crafted apps with Material Design and Vue 2</div>
+
+          <md-button class="md-primary md-raised" href="#/getting-started">Getting Started</md-button>
+          <md-button class="md-primary md-raised" href="https://github.com/marcosmoura/vue-material" target="_blank" rel="noopener">Github</md-button>
         </div>
 
-        <div class="introduction">Build well-crafted apps with Material Design and Vue 2</div>
+        <div class="row">
+          <div class="column">
+            <h2 class="md-headline">Material Design</h2>
+            <p>Vue Material is lightweight framework built exactly according to the <a href="http://material.google.com" target="_blank" rel="noopener">Material Design</a> specs. Build powerful and well-designed web apps that can can fit on every screen.</p>
+          </div>
 
-        <md-button class="md-primary md-raised" href="#/getting-started">Getting Started</md-button>
-        <md-button class="md-primary md-raised" href="https://github.com/marcosmoura/vue-material" target="_blank" rel="noopener">Github</md-button>
-      </div>
+          <div class="column">
+            <h2 class="md-headline">Full-featured</h2>
+            <p>You can generate and use themes dynamically, use components on demand, take advantage of UI Elements and Components with an ease-to-use API and more...</p>
+          </div>
 
-      <div class="row">
-        <div class="column">
-          <h2 class="md-headline">Material Design</h2>
-          <p>Vue Material is lightweight framework built exactly according to the <a href="http://material.google.com" target="_blank" rel="noopener">Material Design</a> specs. Build powerful and well-designed web apps that can can fit on every screen.</p>
-        </div>
-
-        <div class="column">
-          <h2 class="md-headline">Full-featured</h2>
-          <p>You can generate and use themes dynamically, use components on demand, take advantage of UI Elements and Components with an ease-to-use API and more...</p>
-        </div>
-
-        <div class="column">
-          <h2 class="md-headline">Compatible</h2>
-          <p>It aims to deliver a collection of reusable components and a series of UI Elements to build applications with support to all modern Web Browsers through Vue 2.0.</p>
+          <div class="column">
+            <h2 class="md-headline">Compatible</h2>
+            <p>It aims to deliver a collection of reusable components and a series of UI Elements to build applications with support to all modern Web Browsers through Vue 2.0.</p>
+          </div>
         </div>
       </div>
     </div>
@@ -33,7 +35,7 @@
 </template>
 
 <style lang="scss" scoped>
-  .main-content {
+  .introduction {
     max-width: 960px;
     margin: 0 auto;
   }
