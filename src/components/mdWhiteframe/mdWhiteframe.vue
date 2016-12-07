6a552978832fc727255647480276da2759e3b0fe
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-07 19:18:49 -0200

    create custom tags in whiteframe component (#170)
    * create custom tags in whiteframe
    
    * add custom tag in api. fix #169
    
    * add custom tag in examples. fix #169

--- src/components/mdWhiteframe/mdWhiteframe.vue
+++ src/components/mdWhiteframe/mdWhiteframe.vue
@@ -1,40 +1,37 @@
-<template>
-  <div class="md-whiteframe" :class="classes">
-    <slot></slot>
-  </div>
-</template>
-
 <style lang="scss" src="./mdWhiteframe.scss"></style>
 
 <script>
   export default {
     props: {
-      mdElevation: [String, Number]
-    },
-    data() {
-      return {
-        elevation: this.mdElevation === 0 ? 0 : this.mdElevation || 1
-      };
-    },
-    watch: {
-      mdElevation() {
-        this.elevation = this.mdElevation;
+      mdElevation: {
+        type: [String, Number],
+        default: 1
+      },
+      mdTag: {
+        type: String,
+        default: 'div'
       }
     },
     computed: {
       classes() {
-        let numberedElevation = parseInt(this.elevation, 10);
+        let numberedElevation = parseInt(this.mdElevation, 10);
         let elevationClass = 'md-whiteframe-';
 
         if (!isNaN(numberedElevation) && typeof numberedElevation === 'number') {
           elevationClass += numberedElevation;
           elevationClass += 'dp';
-        } else if (this.elevation.indexOf('dp') > -1) {
-          elevationClass += this.elevation;
+        } else if (this.mdElevation.indexOf('dp') > -1) {
+          elevationClass += this.mdElevation;
         }
 
         return elevationClass;
       }
+    },
+    render(createElement) {
+      return createElement(this.mdTag, {
+        staticClass: 'md-whiteframe',
+        class: this.classes
+      }, this.$slots.default);
     }
   };
 </script>
