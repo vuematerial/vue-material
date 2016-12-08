51c17e6e1804337181747658a60f78824b33201b
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-08 10:48:37 -0200

    [build] 0.4.1

--- dist/components/mdSidenav/index.debug.js
+++ dist/components/mdSidenav/index.debug.js
@@ -114,7 +114,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	if (typeof __vue_options__ === "function") {
 	  __vue_options__ = __vue_options__.options
 	}
-	__vue_options__.__file = "/Users/marcosmoura/Projects/github/vue-material/src/components/mdSidenav/mdSidenav.vue"
+	__vue_options__.__file = "/Users/mrufino/Projects/personal/github/vue-material/src/components/mdSidenav/mdSidenav.vue"
 	__vue_options__.render = __vue_template__.render
 	__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
 	
@@ -125,9 +125,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	  if (!hotAPI.compatible) return
 	  module.hot.accept()
 	  if (!module.hot.data) {
-	    hotAPI.createRecord("data-v-4904390e", __vue_options__)
+	    hotAPI.createRecord("data-v-0821376f", __vue_options__)
 	  } else {
-	    hotAPI.reload("data-v-4904390e", __vue_options__)
+	    hotAPI.reload("data-v-0821376f", __vue_options__)
 	  }
 	})()}
 	if (__vue_options__.functional) {console.error("[vue-loader] mdSidenav.vue: functional components are not supported and should be defined in plain js files using render functions.")}
@@ -230,7 +230,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	if (false) {
 	  module.hot.accept()
 	  if (module.hot.data) {
-	     require("vue-hot-reload-api").rerender("data-v-4904390e", module.exports)
+	     require("vue-hot-reload-api").rerender("data-v-0821376f", module.exports)
 	  }
 	}
 
