8dda3a70bc932dcb6d1b56e3b3ccedafe574ab1e
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-12 05:26:21 -0200

    create layout base

--- docs/src/pages/ui-elements/Layout.vue
+++ docs/src/pages/ui-elements/Layout.vue
@@ -6,10 +6,111 @@
       </div>
 
       <div slot="example">
-        <example-box card-title="Default">
-          <div class="demo" slot="demo">
-            <md-layout>
+        <example-box card-title="Gutters">
+          <div class="layout-demo grid" slot="demo">
+            <md-layout class="no-gutter color">
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
 
+            <md-layout :md-gutter="8" class="color">
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+
+            <md-layout :md-gutter="16" class="color">
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+
+            <md-layout :md-gutter="24" class="color">
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+
+            <md-layout :md-gutter="40" class="color">
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+          </div>
+
+          <div slot="code">
+            <code-block lang="xml">
+
+            </code-block>
+          </div>
+        </example-box>
+
+        <example-box card-title="Sizes">
+          <div class="layout-demo grid" slot="demo">
+            <md-layout :md-gutter="16" class="color">
+              <md-layout :md-flex="50"></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+
+            <md-layout :md-gutter="16" class="color">
+              <md-layout :md-flex="33"></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+
+            <md-layout :md-gutter="16" class="color">
+              <md-layout></md-layout>
+              <md-layout :md-flex="66"></md-layout>
+            </md-layout>
+
+            <md-layout :md-gutter="16" class="color">
+              <md-layout :md-flex="75"></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+
+            <md-layout :md-gutter="16" class="color">
+              <md-layout :md-flex="50"></md-layout>
+              <md-layout></md-layout>
+              <md-layout></md-layout>
+            </md-layout>
+          </div>
+
+          <div slot="code">
+            <code-block lang="xml">
+
+            </code-block>
+          </div>
+        </example-box>
+
+        <example-box card-title="Direction">
+          <div class="layout-demo grid" slot="demo">
+            <md-layout class="column-size" :md-gutter="16">
+              <md-layout md-column :md-gutter="16" class="color">
+                <md-layout :md-flex="20"></md-layout>
+                <md-layout></md-layout>
+              </md-layout>
+
+              <md-layout md-column :md-gutter="16" class="color">
+                <md-layout :md-flex="50"></md-layout>
+                <md-layout></md-layout>
+                <md-layout></md-layout>
+              </md-layout>
+            </md-layout>
+
+            <md-layout class="column-size" :md-gutter="24">
+              <md-layout md-column :md-gutter="24" class="color">
+                <md-layout :md-flex="25"></md-layout>
+                <md-layout></md-layout>
+              </md-layout>
+
+              <md-layout md-column :md-gutter="24" class="color">
+                <md-layout :md-flex="25"></md-layout>
+                <md-layout :md-flex="50"></md-layout>
+                <md-layout></md-layout>
+              </md-layout>
             </md-layout>
           </div>
 
@@ -25,5 +126,61 @@
 </template>
 
 <style lang="scss" scoped>
+  .layout-demo {
+    min-height: 200px;
+  }
 
+  .no-gutter > .md-layout:last-child:after {
+    background-color: rgba(#000, .2) !important;
+  }
+
+  .grid {
+    .md-layout:not(.md-column) {
+      height: 40px;
+    }
+
+    > .md-layout:not(.md-column) + .md-layout {
+      margin-top: 16px;
+    }
+  }
+
+  .color {
+    $sizes: (8, 16, 24, 40);
+
+    @each $item in $sizes {
+      $size: $item / 2 + px;
+
+      &:not(.md-column).md-gutter-#{$item} > .md-layout:after {
+        right: $size;
+        left: $size;
+      }
+
+      &.md-column.md-gutter-#{$item} > .md-layout:after {
+        top: $size;
+        bottom: $size;
+      }
+    }
+
+    > .md-layout {
+      position: relative;
+
+      &:after {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background-color: rgba(#000, .12);
+        content: " ";
+      }
+    }
+  }
+
+  .column-size {
+    height: 250px !important;
+
+    + .column-size {
+      margin-top: 48px !important;
+    }
+  }
 </style>
--- src/components/mdLayout/mdLayout.vue
+++ src/components/mdLayout/mdLayout.vue
@@ -6,6 +6,56 @@
       mdTag: {
         type: String,
         default: 'div'
+      },
+      mdRow: Boolean,
+      mdRowXsmall: Boolean,
+      mdRowSmall: Boolean,
+      mdRowMedium: Boolean,
+      mdRowLarge: Boolean,
+      mdRowXlarge: Boolean,
+      mdColumn: Boolean,
+      mdColumnXsmall: Boolean,
+      mdColumnSmall: Boolean,
+      mdColumnMedium: Boolean,
+      mdColumnLarge: Boolean,
+      mdColumnXlarge: Boolean,
+      mdGutter: [Number, Boolean],
+      mdFlex: [Number, Boolean]
+    },
+    computed: {
+      classes() {
+        let classes = {
+          'md-row': this.mdRow,
+          'md-row-xsmall': this.mdRowXsmall,
+          'md-row-small': this.mdRowSmall,
+          'md-row-medium': this.mdRowMedium,
+          'md-row-large': this.mdRowLarge,
+          'md-row-xlarge': this.mdRowXlarge,
+          'md-column': this.mdColumn,
+          'md-column-xsmall': this.mdColumnXsmall,
+          'md-column-small': this.mdColumnSmall,
+          'md-column-medium': this.mdColumnMedium,
+          'md-column-large': this.mdColumnLarge,
+          'md-column-xlarge': this.mdColumnXlarge
+        };
+
+        if (this.mdGutter) {
+          if (typeof this.mdGutter === 'boolean') {
+            classes['md-gutter-8'] = true;
+          } else {
+            classes['md-gutter-' + this.mdGutter] = true;
+          }
+        }
+
+        if (this.mdFlex) {
+          if (typeof this.mdFlex === 'boolean') {
+            classes['md-flex'] = true;
+          } else {
+            classes['md-flex-' + this.mdFlex] = true;
+          }
+        }
+
+        return classes;
       }
     },
     render(createElement) {
