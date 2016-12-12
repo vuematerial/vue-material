8a72eddf3cf816c9d7aa1c837b85323baddc1024
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-12 00:15:27 -0200

    rewrite snackbar

--- src/components/mdSnackbar/mdSnackbar.vue
+++ src/components/mdSnackbar/mdSnackbar.vue
@@ -9,8 +9,6 @@
 <style lang="scss" src="./mdSnackbar.scss"></style>
 
 <script>
-  import transitionEndEventName from '../../core/utils/transitionEndEventName';
-
   export default {
     props: {
       mdPosition: {
@@ -25,8 +23,8 @@
     data() {
       return {
         active: false,
-        closeTimeout: null,
-        positionClass: this.mdPosition.replace(/ /g, '-')
+        rootElement: {},
+        snackbarElement: {}
       };
     },
     computed: {
@@ -35,56 +33,29 @@
           'md-active': this.active
         };
 
-        cssClasses['md-position-' + this.positionClass] = true;
+        cssClasses['md-position-' + this.mdPosition.replace(/ /g, '-')] = true;
 
         return cssClasses;
       }
     },
     methods: {
-      removeSnackbar() {
-        const snackbars = [...document.querySelectorAll('.md-snackbar')];
-        const activeRipple = this.snackbarElement.querySelector('.md-ripple.md-active');
-
-        snackbars.forEach((snackbar) => {
-          if (snackbar.parentNode) {
-            snackbar.parentNode.removeChild(snackbar);
-          }
-        });
-
-        if (activeRipple) {
-          activeRipple.classList.remove('md-active');
-        }
-      },
       open() {
-        this.removeSnackbar();
-        this.rootElement.appendChild(this.snackbarElement);
-        this.rootElement.classList.add('md-has-toast-' + this.positionClass);
-
-        window.getComputedStyle(this.snackbarElement).transition;
-        this.closeTimeout = window.setTimeout(this.close, this.mdDuration);
         this.active = true;
         this.$emit('open');
       },
       close() {
-        if (this.rootElement.contains(this.snackbarElement)) {
-          window.clearTimeout(this.closeTimeout);
-          this.active = false;
-          this.rootElement.classList.remove('md-has-toast-' + this.positionClass);
-          this.snackbarElement.removeEventListener(transitionEndEventName, this.removeSnackbar);
-          this.snackbarElement.addEventListener(transitionEndEventName, this.removeSnackbar);
-          this.$emit('close');
-        }
+        this.active = false;
+        this.$emit('close');
       }
     },
     mounted() {
       this.$nextTick(() => {
         this.rootElement = this.$root.$el;
         this.snackbarElement = this.$el;
-        this.removeSnackbar();
       });
     },
     beforeDestroy() {
-      this.removeSnackbar();
+
     }
   };
 </script>
