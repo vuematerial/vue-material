503fd4543ba2efd6edff033f8d039adb0aa7a925
Hoa Nguyen Van <vanhoa@vhn.vn>
2016-12-16 09:37:10 +0700

    lazy update ink's size

--- src/core/components/mdInkRipple/index.js
+++ src/core/components/mdInkRipple/index.js
@@ -97,6 +97,32 @@ export default function install(Vue) {
 
         rippleParent.appendChild(rippleElement);
         holder.appendChild(rippleParent);
+
+        // sometime the element's size hasnt been ready yet
+        if (elementSize === '0px' && document.readyState !== 'complete') {
+          let giveOfTime = false;
+          let lazySizeUpdate = () => {
+            let finalElementSize = Math.round(Math.max(holder.offsetWidth, holder.offsetHeight)) + 'px';
+
+            if (finalElementSize === '0px') {
+              if (document.readyState !== 'complete') {
+                // try again later
+              } else if (!giveOfTime) {
+                //keep retrying for 3s
+                giveOfTime = new Date(new Date().getTime() + 3000);
+              } else if (new Date() >= giveOfTime) {
+                //failed...
+                return;
+              }
+              setTimeout(lazySizeUpdate, 100);
+            }
+
+            rippleElement.style.width = finalElementSize;
+            rippleElement.style.height = finalElementSize;
+          };
+
+          setTimeout(lazySizeUpdate, 100);
+        }
       }
 
       if (holder !== element || !ripple) {
