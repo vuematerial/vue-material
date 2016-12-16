2d139d8af37ac64160b05c8cdb85401980af9ad3
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-16 13:47:14 -0200

    recalculate the expansion element if a child is added

--- src/components/mdList/mdListExpand.vue
+++ src/components/mdList/mdListExpand.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md-list-expand" :style="{ 'margin-bottom': height }">
+  <div class="md-list-expand" :style="{ 'margin-bottom': height }" :class="classes" ref="expand">
     <slot></slot>
   </div>
 </template>
@@ -8,16 +8,47 @@
   export default {
     data() {
       return {
-        height: 0
+        height: 0,
+        contentObserver: null,
+        transitionOff: true
       };
+    },
+    computed: {
+      classes() {
+        return {
+          'md-transition-off': this.transitionOff
+        };
+      }
     },
     methods: {
       calculatePadding() {
         this.height = -this.$el.offsetHeight + 'px';
+
+        window.setTimeout(() => {
+          this.transitionOff = false;
+        });
+      },
+      recalculateAfterChildChange() {
+        this.transitionOff = true;
+        this.calculatePadding();
+      },
+      observeChildChanges() {
+        this.contentObserver = new MutationObserver(this.recalculateAfterChildChange);
+        this.contentObserver.observe(this.$refs.expand, {
+          childList: true,
+          characterData: true,
+          subtree: true
+        });
       }
     },
     mounted() {
       this.calculatePadding();
+      this.observeChildChanges();
+    },
+    beforeDestroy() {
+      if (this.contentObserver) {
+        this.contentObserver.disconnect();
+      }
     }
   };
 </script>
