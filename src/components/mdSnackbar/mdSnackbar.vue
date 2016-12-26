6959b995d98794abdb549185bbfc96ab76986c62
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-26 17:01:22 -0200

    add open/close methods

--- src/components/mdSnackbar/mdSnackbar.vue
+++ src/components/mdSnackbar/mdSnackbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md-snackbar" :class="classes">
+  <div class="md-snackbar" :class="classes" :id="snackbarId">
     <div class="md-snackbar-content">
       <slot></slot>
     </div>
@@ -9,8 +9,13 @@
 <style lang="scss" src="./mdSnackbar.scss"></style>
 
 <script>
+  import uniqueId from '../../core/utils/uniqueId';
+  import transitionEndEventName from '../../core/utils/transitionEndEventName';
+  import manager from './manager';
+
   export default {
     props: {
+      id: [String, Number],
       mdPosition: {
         type: String,
         default: 'bottom center'
@@ -22,9 +27,11 @@
     },
     data() {
       return {
+        snackbarId: this.id || 'snackbar-' + uniqueId(),
         active: false,
         rootElement: {},
-        snackbarElement: {}
+        snackbarElement: {},
+        closeTimeout: null
       };
     },
     computed: {
@@ -40,22 +47,43 @@
     },
     methods: {
       open() {
+        if (manager.current) {
+          manager.current.close();
+        }
+
+        manager.current = this;
+        this.rootElement.appendChild(this.snackbarElement);
+        window.getComputedStyle(this.snackbarElement).backgroundColor;
         this.active = true;
         this.$emit('open');
+        //this.closeTimeout = window.setTimeout(this.close, this.mdDuration);
       },
       close() {
+        const removeElement = () => {
+          this.snackbarElement.removeEventListener(transitionEndEventName, removeElement);
+
+          if (this.rootElement.contains(this.snackbarElement)) {
+            this.rootElement.removeChild(this.snackbarElement);
+          }
+        };
+
+        manager.current = null;
         this.active = false;
         this.$emit('close');
+        this.snackbarElement.removeEventListener(transitionEndEventName, removeElement);
+        this.snackbarElement.addEventListener(transitionEndEventName, removeElement);
+        window.clearTimeout(this.closeTimeout);
       }
     },
     mounted() {
       this.$nextTick(() => {
         this.rootElement = this.$root.$el;
         this.snackbarElement = this.$el;
+        this.snackbarElement.parentNode.removeChild(this.snackbarElement);
       });
     },
     beforeDestroy() {
-
+      this.close();
     }
   };
 </script>
