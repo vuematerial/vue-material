cf304f14eb5455ae0e9ca0fdc16868d4bf7712a0
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-29 02:42:00 -0200

    create base mdProgress component files

---
+++ src/components/mdProgress/mdProgress.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="md-progress" appear>
+    <div class="md-progress" :class="[themeClass, classes]":style="styles">
+    </div>
+  </transition>
+</template>
+
+<style lang="scss" src="./mdProgress.scss"></style>
+
+<script>
+  import theme from '../../core/components/mdTheme/mixin';
+
+  export default {
+    props: {
+
+    },
+    mixins: [theme],
+    computed: {
+      classes() {
+        return {
+
+        };
+      },
+      styles() {
+        return {
+
+        };
+      }
+    },
+    data: () => ({
+
+    }),
+    methods: {
+
+    }
+  };
+</script>
