524bd9fe5b6c33106a4fc77b8cc1bb2bf1a4940a
Marcos Moura <marcosvmmoura@gmail.com>
2016-12-29 01:55:32 -0200

    create file input #259

--- docs/src/pages/components/File.vue
+++ docs/src/pages/components/File.vue
@@ -2,7 +2,7 @@
   <page-content page-title="Components - File">
     <docs-component>
       <div slot="description">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam debitis consequatur placeat! Velit, esse alias voluptatem dolor facere cumque placeat at similique consequatur, minima assumenda eaque quis laborum. Quia, deleniti.</p>
+        <p>The file picker aim to select files like images, videos and other formats. They can have multiselection and use the devide file system to pick the file.</p>
       </div>
 
       <div slot="api">
@@ -18,9 +18,45 @@
 
             <md-table-body>
               <md-table-row>
-                <md-table-cell>empty</md-table-cell>
-                <md-table-cell><code>Type</code></md-table-cell>
-                <md-table-cell>Description</md-table-cell>
+                <md-table-cell>v-model</md-table-cell>
+                <md-table-cell><code>String</code></md-table-cell>
+                <md-table-cell>A required model object to bind the value.</md-table-cell>
+              </md-table-row>
+
+              <md-table-row>
+                <md-table-cell>id</md-table-cell>
+                <md-table-cell><code>String</code></md-table-cell>
+                <md-table-cell>Sets the input id.</md-table-cell>
+              </md-table-row>
+
+              <md-table-row>
+                <md-table-cell>name</md-table-cell>
+                <md-table-cell><code>String</code></md-table-cell>
+                <md-table-cell>Sets the input name.</md-table-cell>
+              </md-table-row>
+
+              <md-table-row>
+                <md-table-cell>disabled</md-table-cell>
+                <md-table-cell><code>Boolean</code></md-table-cell>
+                <md-table-cell>Disable the input and prevent his actions. Default <code>false</code></md-table-cell>
+              </md-table-row>
+
+              <md-table-row>
+                <md-table-cell>required</md-table-cell>
+                <md-table-cell><code>Boolean</code></md-table-cell>
+                <md-table-cell>Apply the required rule to style the label with an "*". Default <code>false</code></md-table-cell>
+              </md-table-row>
+
+              <md-table-row>
+                <md-table-cell>accept</md-table-cell>
+                <md-table-cell><code>String</code></md-table-cell>
+                <md-table-cell>Filter files that can be selected by mimetype pattern.</md-table-cell>
+              </md-table-row>
+
+              <md-table-row>
+                <md-table-cell>multiple</md-table-cell>
+                <md-table-cell><code>Boolean</code></md-table-cell>
+                <md-table-cell>Enable multiple selection.</md-table-cell>
               </md-table-row>
             </md-table-body>
           </md-table>
@@ -28,10 +64,34 @@
       </div>
 
       <div slot="example">
-        <example-box card-title="Default">
+        <example-box card-title="Single File">
           <div slot="demo">
             <md-input-container>
-              <md-file></md-file>
+              <label>Single</label>
+              <md-file v-model="single"></md-file>
+            </md-input-container>
+
+            <md-input-container>
+              <md-file v-model="placeholder" placeholder="A nice input placeholder"></md-file>
+            </md-input-container>
+
+            <md-input-container>
+              <md-file placeholder="Disabled" disabled></md-file>
+            </md-input-container>
+
+            <md-input-container>
+              <label>Initial Value</label>
+              <md-file v-model="initialValue"></md-file>
+            </md-input-container>
+
+            <md-input-container>
+              <label>Multiple</label>
+              <md-file v-model="multiple" multiple></md-file>
+            </md-input-container>
+
+            <md-input-container>
+              <label>Only Images</label>
+              <md-file v-model="onlyImages" accept="image/*"></md-file>
             </md-input-container>
           </div>
 
@@ -49,7 +109,11 @@
 <script>
   export default {
     data: () => ({
-
+      single: null,
+      placeholder: null,
+      initialValue: 'my-profile-picture.jpg',
+      multiple: null,
+      onlyImages: null
     }),
     methods: {
 
