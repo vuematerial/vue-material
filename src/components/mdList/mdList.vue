118cb72bc3fdbc5fdffaafe2b3ef5796c2c4544a
ivictbor <ivictbor@gmail.com>
2017-02-04 17:21:57 +0200

    merged left-expand-indicator

--- src/components/mdList/mdList.vue
+++ src/components/mdList/mdList.vue
@@ -10,6 +10,12 @@
   import theme from '../../core/components/mdTheme/mixin';
 
   export default {
-    mixins: [theme]
+    mixins: [theme],
+    props: {
+      leftExpandIndicator: {
+        type: Boolean,
+        default: false
+      }
+    }
   };
 </script>
