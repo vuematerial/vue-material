e64eae9a791dc75a7f10c7addfc325e587ca50db
Fergardi <fergardi@gmail.com>
2017-02-10 16:29:53 +0100

    Finished first scaffolding of a grid. Lacking responsiveness.

--- docs/src/pages/components/Grid.vue
+++ docs/src/pages/components/Grid.vue
@@ -30,30 +30,73 @@
       <div slot="example">
         <example-box card-title="Simple grid">
           <div slot="demo">
-            <md-grid>
-              <md-grid-media>
-                <md-grid-content class="double-line">
-                  <span>Image title</span>
-                  <span>Image title</span>
+            <md-grid md-gutter>
+              <md-grid-media class="md-primary">
+                <md-grid-content>
+                  <div class="double-line">
+                    <strong>Image title</strong>
+                    <span>Image subtitle</span>
+                  </div>
+                  <md-button class="md-icon-button"><md-icon>favorite</md-icon></md-button>
+                </md-grid-content>
+                <img src="https://placeimg.com/480/640/any"/>
+              </md-grid-media>
+              <md-grid-media class="md-accent">
+                <md-grid-content>
+                  <div class="single-line">
+                    <span>Image title</span>
+                  </div>
                   <md-button class="md-icon-button"><md-icon>star</md-icon></md-button>
                 </md-grid-content>
-                <img src="https://placeimg.com/640/480/any"/>
+                <img src="https://placeimg.com/480/640/any"/>
+                <md-grid-content>
+                  <div class="double-line">
+                    <strong>Image title</strong>
+                    <span>Image subtitle</span>
+                  </div>
+                </md-grid-content>
+              </md-grid-media>
+              <md-grid-media class="md-warn">
+                <img src="https://placeimg.com/480/640/any"/>
+                <md-grid-content>
+                  <md-button class="md-icon-button"><md-icon>home</md-icon></md-button>
+                  <div class="single-line">
+                    <span>Image title</span>
+                  </div>
+                </md-grid-content>
               </md-grid-media>
               <md-grid-media>
-                <md-grid-content class="single-line">
-                  <span>Image title</span>
+                <md-grid-content>
+                  <div class="double-line">
+                    <strong>Image title</strong>
+                    <span>Image subtitle</span>
+                  </div>
+                  <md-button class="md-icon-button"><md-icon>favorite</md-icon></md-button>
+                </md-grid-content>
+                <img src="https://placeimg.com/480/640/any"/>
+              </md-grid-media>
+              <md-grid-media>
+                <md-grid-content>
+                  <div class="single-line">
+                    <span>Image title</span>
+                  </div>
                   <md-button class="md-icon-button"><md-icon>star</md-icon></md-button>
                 </md-grid-content>
-                <img src="https://placeimg.com/640/480/any"/>
-                <md-grid-content class="single-line">
-                  <span>Image title</span>
+                <img src="https://placeimg.com/480/640/any"/>
+                <md-grid-content>
+                  <div class="double-line">
+                    <strong>Image title</strong>
+                    <span>Image subtitle</span>
+                  </div>
                 </md-grid-content>
               </md-grid-media>
               <md-grid-media>
-                <img src="https://placeimg.com/640/480/any"/>
-                <md-grid-content class="double-line">
-                  <span>Image title</span>
-                  <span>Image title</span>
+                <img src="https://placeimg.com/480/640/any"/>
+                <md-grid-content>
+                  <md-button class="md-icon-button"><md-icon>home</md-icon></md-button>
+                  <div class="single-line">
+                    <span>Image title</span>
+                  </div>
                 </md-grid-content>
               </md-grid-media>
             </md-grid>
