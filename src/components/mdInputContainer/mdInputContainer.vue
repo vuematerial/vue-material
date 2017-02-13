ec1f050d80539a01f497b754180e1ee3847312c2
d3radicated <john.ryan.camatog@gmail.com>
2017-02-13 11:07:25 +0800

    add md-clearable to md-input-container (#473)
    * Add md-clearable to md-input-container
    
    * Add docs for md-clearable
    
    * Fix deprecated click event for clear button

--- src/components/mdInputContainer/mdInputContainer.vue
+++ src/components/mdInputContainer/mdInputContainer.vue
@@ -7,6 +7,10 @@
     <md-button class="md-icon-button md-toggle-password" @click.native="togglePasswordType" v-if="mdHasPassword">
       <md-icon>{{ showPassword ? 'visibility_off' : 'visibility' }}</md-icon>
     </md-button>
+
+    <md-button class="md-icon-button md-clear-input" @click.native="clearInput" v-if="mdClearable && hasValue">
+      <md-icon>clear</md-icon>
+    </md-button>
   </div>
 </template>
 
@@ -20,7 +24,8 @@
     name: 'md-input-container',
     props: {
       mdInline: Boolean,
-      mdHasPassword: Boolean
+      mdHasPassword: Boolean,
+      mdClearable: Boolean
     },
     mixins: [theme],
     data() {
@@ -51,6 +56,7 @@
         return {
           'md-input-inline': this.mdInline,
           'md-has-password': this.mdHasPassword,
+          'md-clearable': this.mdClearable,
           'md-has-select': this.hasSelect,
           'md-has-file': this.hasFile,
           'md-has-value': this.hasValue,
@@ -76,6 +82,13 @@
           }
 
           this.input.focus();
+        }
+      },
+      clearInput() {
+        if (this.isInput()) {
+          this.input.value = '';
+          this.setValue(this.input.value);
+          this.$emit('input', this.input.value);
         }
       },
       setValue(value) {
