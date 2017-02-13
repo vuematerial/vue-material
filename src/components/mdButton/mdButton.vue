64fe1652972903afd780e5e1bee08a26e0d21e15
Marcos Moura <marcosvmmoura@gmail.com>
2017-02-13 18:43:20 -0200

    add support to router-link with md-button as tag #470

--- src/components/mdButton/mdButton.vue
+++ src/components/mdButton/mdButton.vue
@@ -1,13 +1,13 @@
 <template>
-  <button class="md-button" :class="[themeClass]" :type="type" :disabled="disabled" v-if="!href">
-    <md-ink-ripple :md-disabled="disabled"></md-ink-ripple>
-    <slot></slot>
-  </button>
-
-  <a class="md-button" :class="[themeClass]" :href="href" :disabled="disabled" :target="target" :rel="newRel" v-else>
+  <a class="md-button" :class="[themeClass]" :href="href" :disabled="disabled" :target="target" :rel="newRel" @click="$emit('click', $event)" v-if="href">
     <md-ink-ripple :md-disabled="disabled"></md-ink-ripple>
     <slot></slot>
   </a>
+
+  <button class="md-button" :class="[themeClass]" :type="type" :disabled="disabled" @click="$emit('click', $event)" v-else>
+    <md-ink-ripple :md-disabled="disabled"></md-ink-ripple>
+    <slot></slot>
+  </button>
 </template>
 
 <style lang="scss" src="./mdButton.scss"></style>
