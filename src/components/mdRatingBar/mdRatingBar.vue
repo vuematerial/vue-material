4b07c3c5ef3a909970a7efbdbb044cef8ceb9842
Laercio Santana <laerciosantana021@hotmail.com>
2017-03-18 02:43:07 +0000

    [rating-bar] initial commit *add rating bar component

---
+++ src/components/mdRatingBar/mdRatingBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="md-rating-bar" :class="[themeClass]" :disabled="disabled">
+    <div class="back-stars" :disabled="disabled">
+      <md-icon v-for="i in numStars">star</md-icon>
+    </div>
+    <div class="front-stars" :style="frontStarsStyle">
+      <md-icon v-for="i in numStars" class="md-accent">star</md-icon>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./mdRatingBar.scss"></style>
+
+<script>
+  import theme from '../../core/components/mdTheme/mixin';
+
+  let iconSize = 24;//size of each icon from rating bar in pixels
+
+  export default {
+    props: {
+      numStars: {
+        type: Number,
+        default: 5
+      },
+      rating: {
+        type: Number,
+        default: 0
+      },
+      disabled: Boolean
+    },
+    mixins: [theme],
+    computed: {
+      frontStarsStyle() {
+        return {
+          width: 100 * this.rating + '%',
+          'margin-left': -iconSize * this.numStars + 'px'
+        };
+      }
+    }
+  };
+</script>
