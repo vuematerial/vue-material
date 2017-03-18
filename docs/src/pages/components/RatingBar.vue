52f376069dc41ab37d6307a8a53b797b6edb635f
Laercio Santana <laerciosantana021@hotmail.com>
2017-03-18 17:54:40 -0300

    [rating-bar] add entry to documentation

---
+++ docs/src/pages/components/RatingBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <page-content page-title="Components - Radio">
+    <docs-component>
+      <div slot="description">
+        <p>Rating Bar can be used to show a rating to user or capture a rating from user.</p>
+        <p>The following classes can be applied to change the color palette:</p>
+        <ul class="md-body-2">
+          <li><code>md-primary</code></li>
+          <li><code>md-accent</code></li>
+          <li><code>md-warn</code></li>
+        </ul>
+      </div>
+
+      <div slot="api">
+
+      </div>
+
+      <div slot="example">
+
+      </div>
+    </docs-component>
+  </page-content>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  };
+</script>
