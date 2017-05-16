75a184560d934d13483230d692a7849954f34f90
marcosmoura <marcosvmmoura@gmail.com>
2017-05-16 05:10:18 -0300

    init theme generator with sass

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="container">
     <slot />
+
+    <div class="md-button md-theme-default" @click="accent = !accent" :class="{ 'md-accent': accent }"></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+  @import '~theme/index';
+  @import '~components/mdButton/theme';
 
+  @include md-apply-theme('default');
+  @include md-apply-theme('alt', (
+    primary: green
+  ));
 </style>
 
 <script>
   export default {
-    name: 'App'
+    name: 'app',
+    data: () => ({
+      accent: false
+    })
   }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './theme/index.scss'
+
 const plugin = (Vue, options = {}) => {
 
 }
