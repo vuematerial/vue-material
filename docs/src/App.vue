18e9bef58175dff5684ea9ff2c07812603fdf349
marcosmoura <marcosvmmoura@gmail.com>
2017-05-17 02:46:17 -0300

    create initial scaffold of mdButton component

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <slot />
 
-    <div class="md-button md-theme-default" @click="accent = !accent" :class="{ 'md-accent': accent }">Button</div>
-    <div class="md-button md-theme-alt" @click="accent = !accent" :class="{ 'md-accent': accent }">Button</div>
+    <md-button @click.native="accent = !accent" :class="{ 'md-accent': accent }" class="md-theme-default">Button</md-button>
+    <md-button @click.native="accent = !accent" :class="{ 'md-accent': accent }" class="md-theme-alt">Button</md-button>
   </div>
 </template>
 
-<style lang="scss" scoped>
-  @import '~theme/index';
+<style lang="scss">
+  @import '~theme';
 
   @include md-apply-theme('default');
   @include md-apply-theme('alt', (
@@ -16,12 +16,17 @@
     accent: #CDDC39
   ));
 
-  @import '~components/mdButton/mdButton';
+  @import '~components/mdButton/theme';
 </style>
 
 <script>
+  import MdButton from 'components/mdButton/mdButton'
+
   export default {
     name: 'app',
+    components: {
+      MdButton
+    },
     data: () => ({
       accent: false
     })
