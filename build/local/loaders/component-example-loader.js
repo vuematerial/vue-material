8ea34e4a03d42a8f9dad6162d2b7564bad9a457d
marcosmoura <marcosvmmoura@gmail.com>
2017-06-12 04:04:38 -0300

    make the example compiler faster

--- build/local/loaders/component-example-loader.js
+++ build/local/loaders/component-example-loader.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const compiler = require('vue-template-compiler')
 const transpile = require('vue-template-es2015-compiler')
-const pretty = require('pretty')
 
 function camelCaseToDash (str) {
   return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
@@ -12,28 +11,6 @@ module.exports = function (source) {
   const fileName = camelCaseToDash(path.basename(filePath).replace('.vue', ''))
   const parsedComponent = compiler.parseComponent(source)
   const parsedTags = {}
-  let output = ''
-
-  function parseTag (tag, schema) {
-    let attributes = ''
-    let content = schema.content
-
-    if (schema.attrs) {
-      Object.keys(schema.attrs).forEach(attr => {
-        attributes += ` ${attr}='${schema.attrs[attr]}'`
-      })
-    }
-
-    if (tag !== 'style') {
-      parsedTags[tag] = pretty(content)
-    } else {
-      parsedTags[tag] = content
-    }
-
-    return `<${tag}${attributes}>${parsedTags[tag]}</${tag}>
-
-    `
-  }
 
   Object.keys(parsedComponent).forEach(b => {
     const schema = parsedComponent[b]
@@ -42,10 +19,10 @@ module.exports = function (source) {
     if (schema && tag !== 'customBlocks') {
       if (Array.isArray(schema)) {
         schema.forEach(config => {
-          output += parseTag(tag, config)
+          parsedTags[tag] = config.content
         })
       } else {
-        output += parseTag(tag, schema)
+        parsedTags[tag] = schema.content
       }
     }
   })
@@ -67,7 +44,7 @@ module.exports = function (source) {
       c.options.examples = c.options.examples || {};
       c.options.examples['${fileName}'] = {
         name: '${fileName}',
-        source: ${JSON.stringify(pretty(output))},
+        source: ${JSON.stringify(source)},
         template: ${JSON.stringify(template)},
         script: ${JSON.stringify(script)},
         style: ${JSON.stringify(style)}
