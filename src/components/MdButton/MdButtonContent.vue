9b59a9f16834a023ba552da6c286ec8f15ab1ac0
marcosmoura <marcosvmmoura@gmail.com>
2017-06-25 21:51:43 -0300

    fix(MdButton): force MdRipple to be added when using MdButton alone

--- src/components/MdButton/MdButtonContent.vue
+++ src/components/MdButton/MdButtonContent.vue
@@ -7,11 +7,24 @@
 </template>
 
 <script>
+  import MdRipple from 'components/MdRipple/MdRipple'
+
   export default {
     name: 'MdButtonContent',
+    components: {
+      MdRipple
+    },
     props: {
       mdRipple: Boolean,
       disabled: Boolean
     }
   }
 </script>
+
+<style lang="scss">
+  .md-button-content {
+    position: relative;
+    z-index: 2;
+  }
+</style>
+
