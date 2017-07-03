d4064cf4b457a62c776affd4fe4c9e44896e58a3
marcosmoura <marcosvmmoura@gmail.com>
2017-07-03 01:17:27 -0300

    docs(MdInput): create switch to toggle errors

--- docs/app/pages/Components/Input/examples/ErrorsMessages.vue
+++ docs/app/pages/Components/Input/examples/ErrorsMessages.vue
@@ -2,25 +2,37 @@
   <div>
     <md-field>
       <label>Required Field</label>
-      <md-input required />
+      <md-input required></md-input>
     </md-field>
 
-    <md-field class="md-invalid">
+    <md-field :class="messageClass">
       <label>Error</label>
-      <md-input required />
+      <md-input required></md-input>
       <span class="md-error">There is an error</span>
     </md-field>
 
-    <md-field class="md-invalid">
+    <md-field :class="messageClass">
       <label>Textarea</label>
-      <md-textarea required />
+      <md-textarea required></md-textarea>
       <span class="md-error">There is an error</span>
     </md-field>
+
+    <md-switch v-model="hasMessages">{{ !hasMessages ? 'Show' : 'Hide' }} Errors</md-switch>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'ErrorsMessages'
+    name: 'ErrorsMessages',
+    data: () => ({
+      hasMessages: false
+    }),
+    computed: {
+      messageClass () {
+        return {
+          'md-invalid': this.hasMessages
+        }
+      }
+    }
   }
 </script>
