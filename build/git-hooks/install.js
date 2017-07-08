7197dc81f139cf1c322fc75419868fda783637fb
marcosmoura <marcosvmmoura@gmail.com>
2017-07-08 16:53:27 -0300

    chore: create a prepush hook to run all jest tests

--- build/git-hooks/install.js
+++ build/git-hooks/install.js
@@ -2,7 +2,9 @@ import { test, ln, chmod } from 'shelljs'
 
 if (test('-e', '.git/hooks')) {
   ln('-sf', '../../build/git-hooks/pre-commit', '.git/hooks/pre-commit')
+  ln('-sf', '../../build/git-hooks/pre-push', '.git/hooks/pre-push')
   ln('-sf', '../../build/git-hooks/commit-msg', '.git/hooks/commit-msg')
   chmod('+x', '.git/hooks/pre-commit')
+  chmod('+x', '.git/hooks/pre-push')
   chmod('+x', '.git/hooks/commit-msg')
 }
