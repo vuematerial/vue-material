1114e395e863eed73fdd8322e17375c2e88b676d
decksterr <arthurpicerna@gmail.com>
2017-08-06 00:49:30 -0500

    fix for `change` event triggering twice when clicking on a md-radio's label

--- src/components/mdRadio/mdRadio.vue
+++ src/components/mdRadio/mdRadio.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="md-radio" :class="[themeClass, classes]">
-    <div class="md-radio-container" @click.stop="toggleCheck">
+    <div class="md-radio-container" @click.self="toggleCheck">
       <input type="radio" :name="name" :id="id" :disabled="disabled" :value="value">
       <md-ink-ripple :md-disabled="disabled" />
     </div>
