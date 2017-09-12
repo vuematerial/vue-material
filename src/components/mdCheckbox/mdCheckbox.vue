db56d76068756537bf4631494c357f7f22f77616
Jonathan Spies <jonathan.spies@gmail.com>
2017-09-12 16:54:14 -0500

    allow Arrays as v-model to checkboxes

--- src/components/mdCheckbox/mdCheckbox.vue
+++ src/components/mdCheckbox/mdCheckbox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md-checkbox" :class="[themeClass, classes]">
     <div class="md-checkbox-container" @click.stop="toggleCheck" tabindex="0">
-      <input type="checkbox" :name="name" :id="id" :disabled="disabled" :value="value" :checked="checked" tabindex="-1">
+      <input type="checkbox" :name="name" :id="id" :disabled="disabled" :value="mdValue" :checked="checked" tabindex="-1">
       <md-ink-ripple :md-disabled="disabled" />
     </div>
 
@@ -20,9 +20,10 @@
     name: 'md-checkbox',
     props: {
       name: String,
-      value: [String, Boolean],
+      value: [String, Boolean, Array],
       id: String,
-      disabled: Boolean
+      disabled: Boolean,
+      mdValue: [String]
     },
     mixins: [theme],
     data() {
@@ -33,23 +34,44 @@
     computed: {
       classes() {
         return {
-          'md-checked': this.checked,
+          'md-checked': this.isArray() ? this.value.indexOf(this.mdValue) >= 0 : this.checked,
           'md-disabled': this.disabled
         };
       }
     },
     watch: {
       value() {
-        this.checked = !!this.value;
+        if (!this.isArray()) {
+          this.checked = !!this.value;
+        }
       }
     },
     methods: {
       toggleCheck($event) {
+
         if (!this.disabled) {
-          this.checked = !this.checked;
-          this.$emit('change', this.checked, $event);
-          this.$emit('input', this.checked, $event);
+          if (this.isArray()) {
+            let index = this.value.indexOf(this.mdValue);
+  
+            if (index >= 0) {
+              this.value.splice(index, 1);
+            } else {
+              this.value.push(this.mdValue);
+            }
+
+            this.$emit('change', this.value, $event);
+            this.$emit('input', this.value, $event);
+          } else {
+            this.checked = !this.checked;
+            this.$emit('change', this.checked, $event);
+            this.$emit('input', this.checked, $event);
+          }
         }
+      },
+
+      isArray() {
+        return Array.isArray(this.value);
+        //return typeof this.value === 'object';
       }
     }
   };
