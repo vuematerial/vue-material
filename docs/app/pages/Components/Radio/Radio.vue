62e00354f15c4b6766afdfb896ec71df828a628d
Marcos Moura <marcosvmmoura@gmail.com>
2017-10-11 11:38:40 -0300

    docs(MdRadio): add missing docs

--- docs/app/pages/Components/Radio/Radio.vue
+++ docs/app/pages/Components/Radio/Radio.vue
@@ -1,4 +1,5 @@
 <example src="./examples/RegularRadio.vue" />
+<example src="./examples/HueColors.vue" />
 
 <template>
   <page-container centered :title="$t('pages.radio.title')">
@@ -7,18 +8,63 @@
     </div>
 
     <div class="page-container-section">
-      <h2>Regular Radio</h2>
+      <h2>Radio</h2>
 
       <code-example title="Radio" :component="examples['regular-radio']" />
+      <code-example title="Hue Colors" :component="examples['hue-colors']" />
+
+      <api-item title="API - md-radio">
+        <p>The following options can be applied to all radios:</p>
+
+        <api-table :headings="props.headings" :props="props.props" slot="props" />
+        <api-table :headings="events.headings" :props="events.props" slot="events" />
+
+        <note-block tip>All other <code>&lt;input type=&quot;radio&quot;&gt;</code> attributes, such as <strong>name</strong> and <strong>required</strong>, can be used on <code>md-radio</code>.</note-block>
+      </api-item>
     </div>
   </page-container>
 </template>
 
 <script>
-import examples from 'docs-mixins/docsExample'
+  import examples from 'docs-mixins/docsExample'
 
-export default {
-  name: 'Radio',
-  mixins: [examples]
-}
+  export default {
+    name: 'Radio',
+    mixins: [examples],
+    data: () => ({
+      props: {
+        headings: ['Name', 'Description', 'Default'],
+        props: [
+          {
+            name: 'value',
+            type: 'String|Number|Boolean',
+            description: 'The value of the radio',
+            defaults: 'on'
+          },
+          {
+            name: 'v-model',
+            type: 'String|Number|Boolean',
+            description: 'The model variable to bind the selection value. If no value is assigned, then it will use the same behaviour of a regular input[type="radio"].',
+            defaults: 'null'
+          },
+          {
+            name: 'id',
+            type: 'String',
+            description: 'The radio unique id.',
+            defaults: 'a random string'
+          }
+        ]
+      },
+      events: {
+        headings: ['Name', 'Description', 'Value'],
+        props: [
+          {
+            name: 'change',
+            description: 'Triggered after a value is selected',
+            value: 'value'
+          }
+        ]
+      }
+    })
+  }
 </script>
---
+++ docs/app/pages/Components/Radio/examples/HueColors.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <md-radio v-model="radio" value="accent">Accent <small>(Default)</small></md-radio>
+    <md-radio v-model="radio" value="Primary" class="md-primary">Primary</md-radio>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HueColors',
+    data: () => ({
+      radio: 'accent'
+    })
+  }
+</script>
+
+<style lang="scss" scoped>
+  .md-radio {
+    display: flex;
+  }
+</style>
