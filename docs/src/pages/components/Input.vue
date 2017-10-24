814a8633c20b537d3d18a48d62b4bd7c569b99ea
Eric Tran <EricTran5791@users.noreply.github.com>
2017-10-24 11:54:14 -0400

    Add selectedColor to the component data (#1077)

--- docs/src/pages/components/Input.vue
+++ docs/src/pages/components/Input.vue
@@ -807,6 +807,7 @@
       return {
         autocompleteValue: '',
         colorValue: '',
+        selectedColor: '',
         initialValue: 'My initial value',
         listAutocomplete: [
           {name: 'oi'},
