c3dc836952722a8ea1ac118b7666cf832ad9bcec
Marcos Moura <marcosvmmoura@gmail.com>
2017-10-25 02:54:34 -0200

    chore: fix build script and upgrade libraries

---
+++ build/git-hooks/install.sh
@@ -0,0 +1,6 @@
+ln -sf ../../build/git-hooks/pre-commit .git/hooks/pre-commit
+ln -sf ../../build/git-hooks/pre-push .git/hooks/pre-push
+ln -sf ../../build/git-hooks/commit-msg .git/hooks/commit-msg
+chmod +x .git/hooks/pre-commit
+chmod +x .git/hooks/pre-push
+chmod +x .git/hooks/commit-msg
