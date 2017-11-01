3232cbbc44469b40672c016a9b2ed57572ec166e
Marcos Moura <marcos.rufino@hotmart.com.br>
2017-11-01 21:02:33 -0200

    fix(MdFile): add required v-model

--- docs/app/pages/Components/File/examples/FileField.vue
+++ docs/app/pages/Components/File/examples/FileField.vue
@@ -2,32 +2,32 @@
   <div>
     <md-field>
       <label>Single</label>
-      <md-file/>
+      <md-file v-model="single" />
     </md-field>
 
     <md-field>
       <label>Upload files</label>
-      <md-file placeholder="A nice input placeholder"/>
+      <md-file v-model="placeholder" placeholder="A nice input placeholder" />
     </md-field>
 
     <md-field>
       <label>Disabled</label>
-      <md-file disabled/>
+      <md-file v-model="disabled" disabled/>
     </md-field>
 
     <md-field>
       <label>Initial value</label>
-      <md-file v-model="initial"/>
+      <md-file v-model="initial" />
     </md-field>
 
     <md-field>
       <label>Multiple</label>
-      <md-file multiple v-model="multiple"/>
+      <md-file v-model="multiple" multiple />
     </md-field>
 
     <md-field>
       <label>Only images</label>
-      <md-file accept="image/*" v-model="single"/>
+      <md-file v-model="single" accept="image/*" />
     </md-field>
   </div>
 </template>
@@ -38,6 +38,8 @@ export default {
   data: () => ({
     initial: 'vue-material-is-awesome.jpg',
     single: null,
+    placeholder: null,
+    disabled: null,
     multiple: null
   })
 }
--- src/components/MdField/MdFile/MdFile.test.js
+++ src/components/MdField/MdFile/MdFile.test.js
@@ -14,28 +14,3 @@ test('should render the field', async () => {
   expect(wrapper.hasClass('md-field')).toBe(true)
   expect(wrapper.text().trim()).toBe('Lorem ipsum')
 })
-
-test('should render the file field', async () => {
-  const wrapperWithFile = mount(MdField, {
-    slots: {
-      default: [MdFile]
-    }
-  })
-
-  const mdFile = wrapperWithFile.first(MdFile)
-
-  expect(mdFile.hasClass('md-file')).toBe(true)
-})
-
-test('should render the input field inside the mdFile', async () => {
-  const wrapperWithFile = mount(MdField, {
-    slots: {
-      default: [MdFile]
-    }
-  })
-
-  const mdFile = wrapperWithFile.first(MdFile)
-  const mdInput = mdFile.vm.$children[0]
-
-  expect(mdFile.hasClass('md-file') && mdInput instanceof Object).toBe(true)
-})
