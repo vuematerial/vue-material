f31b353eb16d3d869abee0b67d82725504799ab7
Marcos Moura <marcosvmmoura@gmail.com>
2017-11-01 01:14:36 -0200

    docs(MdFile): add a tip to tell that all input props can be passed to files

--- docs/app/pages/Components/File/File.vue
+++ docs/app/pages/Components/File/File.vue
@@ -4,6 +4,7 @@
   <page-container centered :title="$t('pages.file.title')">
     <div class="page-container-section">
       <p>File inputs allows the user to pick one or multiple files. It can also filter the file type just like a normal input file or select multiple files.</p>
+      <note-block tip><code>md-file</code> works just like a regular <code>md-input</code> component. This means that you can pass any <code>md-input</code> prop to <code>md-file</code>, like <strong>placeholder</strong>, <strong>required</strong> and <strong>disabled</strong>, for example.</note-block>
     </div>
 
     <div class="page-container-section">
