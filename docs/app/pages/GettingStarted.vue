bd968422ade45ed38d56a808fb5a8c5d8b0a2b3c
Marcos Moura <marcosvmmoura@gmail.com>
2017-11-14 06:39:50 -0200

    fix: add beta flag

--- docs/app/pages/GettingStarted.vue
+++ docs/app/pages/GettingStarted.vue
@@ -15,8 +15,8 @@
 
       <p>You can install Vue Material through NPM or Yarn:</p>
       <code-example :title="$t('pages.gettingStarted.npmOrYarn')" label="Shell" lang="shell">
-        $ npm install vue-material --save
-        $ yarn add vue-material
+        $ npm install vue-material@beta --save
+        $ yarn add vue-material@beta
       </code-example>
     </div>
 
