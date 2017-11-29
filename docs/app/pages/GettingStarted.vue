af464167a9a8030a9ec1ec8aad0138dfddb9a76f
Marcos Moura <marcosvmmoura@gmail.com>
2017-11-28 23:38:57 -0200

    docs: improve getting started

--- docs/app/pages/GettingStarted.vue
+++ docs/app/pages/GettingStarted.vue
@@ -18,13 +18,18 @@
         $ npm install vue-material@beta --save
         $ yarn add vue-material@beta
       </code-example>
+
+      <p>It's optional, but to have the best experience possible, use Roboto and Google Icons from Google CDN:</p>
+      <code-example title="Roboto Font and Icons" label="HTML" lang="html">
+        &lt;link rel=&quot;stylesheet&quot; href=&quot;//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons&quot;&gt;
+      </code-example>
+
     </div>
 
     <div class="page-container-section">
       <h2 class="md-headline">Usage</h2>
 
       <p>To use Vue Material in your application, you can import only the components that you're really using. This will allow you to make your build way more compact than installing a full bundle.</p>
-
       <code-example title="Individual components">
         import Vue from 'vue'
         import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
@@ -35,7 +40,7 @@
         Vue.use(MdTabs)
       </code-example>
 
-      <note-block alert>Although is not recommended you can use the full bundle of Vue Material. This will import <strong>ALL</strong> components and UI Elements:</note-block>
+      <note-block alert>Although is not recommended you can use the full bundle of Vue Material. This will import <strong>ALL</strong> components and UI Elements, and will hurt performance:</note-block>
       <code-example title="Full Bundle">
         import Vue from 'vue'
         import VueMaterial from 'vue-material'
@@ -44,29 +49,24 @@
         Vue.use(VueMaterial)
       </code-example>
 
-      <p>It's optional, but to have the best experience possible, use Roboto and Google Icons from Google CDN:</p>
-
-      <code-example title="Roboto Font and Icons" label="HTML" lang="html">
-        &lt;link rel=&quot;stylesheet&quot; href=&quot;//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons&quot;&gt;
-      </code-example>
-
-      <p>For fast prototyping without installing templates from <strong>vue-cli</strong> use CDNs:</p>
+      <p>Although is not the recommended approach, you can always use CDNs for fast prototyping:</p>
+      <note-block tip>For better integration with Vue Material, use <a href="https://github.com/vuejs-templates/webpack" target="_blank">vue-cli</a>.</note-block>
 
       <code-example title="CDN" label="HTML" lang="html">
         &#x3C;!doctype html&#x3E;
         &#x3C;html&#x3E;
           &#x3C;head&#x3E;
             &#x3C;meta charset=&#x22;utf-8&#x22;&#x3E;
-            &#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons&#x22;&#x3E;
-          &#x9;&#x3C;link type=&#x22;text/css&#x22; rel=&#x22;stylesheet&#x22; href=&#x22;https://unpkg.com/vue-material@beta/dist/vue-material.min.css&#x22;&#x3E;
             &#x3C;meta content=&#x22;width=device-width,initial-scale=1,minimal-ui&#x22; name=&#x22;viewport&#x22;&#x3E;
+            &#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons&#x22;&#x3E;
+          &#x9;&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://unpkg.com/vue-material@beta/dist/vue-material.min.css&#x22;&#x3E;
           &#x3C;/head&#x3E;
           &#x3C;body&#x3E;
             &#x3C;div id=&#x22;app&#x22;&#x3E;
               &#x3C;!-- Your code here --&#x3E;
             &#x3C;/div&#x3E;
-            &#x3C;script type=&#x22;text/javascript&#x22; src=&#x22;https://unpkg.com/vue&#x22;&#x3E;&#x3C;/script&#x3E;
-            &#x3C;script type=&#x22;text/javascript&#x22; src=&#x22;https://unpkg.com/vue-material@beta&#x22;&#x3E;&#x3C;/script&#x3E;
+            &#x3C;script src=&#x22;https://unpkg.com/vue&#x22;&#x3E;&#x3C;/script&#x3E;
+            &#x3C;script src=&#x22;https://unpkg.com/vue-material@beta&#x22;&#x3E;&#x3C;/script&#x3E;
             &#x3C;script&#x3E;
               Vue.use(VueMaterial.default);
 
