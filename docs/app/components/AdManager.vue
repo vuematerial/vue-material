0143f9dff40e2ab8404dd2280e49a5c292bb6a02
Marcos Moura <marcos.rufino@hotmart.com.br>
2017-12-01 14:59:18 -0200

    docs: remove code sponsor :(

--- docs/app/components/AdManager.vue
+++ docs/app/components/AdManager.vue
@@ -1,11 +1,12 @@
+<template>
+  <md-content id="ad-manager" md-theme="default"></md-content>
+</template>
 
 <script>
   export default {
     name: 'AdManager',
-    abstract: true,
     data: () => ({
-      carbonEl: null,
-      codeSponsorEl: null
+      carbonEl: null
     }),
     render () {
       return null
@@ -13,98 +14,86 @@
     methods: {
       saveAdsElements () {
         this.carbonEl = document.getElementById('carbonads')
-        this.codeSponsorEl = document.querySelector('.code-sponsor-widget')
-        if (!this.carbonEl || !this.codeSponsorEl) {
+
+        if (!this.carbonEl) {
           this.$destroy()
         }
       },
-      appendCarbonAds() {
+      appendCarbonAds () {
         const interval = window.setInterval(() => {
           const carbonAds = document.getElementById('carbonads')
           const container = document.querySelector('.container')
-          const mainContainer = document.querySelector('.main-container')
+          const pageContainer = document.querySelector('.page-container')
           const splashContainer = document.querySelector('.splash-container')
-          const carbonAdsContainer = document.querySelector('.code-sponsor-widget')
+          const adManager = this.$el
+
           if (carbonAds && container) {
-            if (splashContainer) {
-              carbonAdsContainer.appendChild(carbonAds)
-            } else if (mainContainer) {
-              mainContainer.insertBefore(carbonAds, mainContainer.firstChild)
+            if (pageContainer) {
+              pageContainer.insertBefore(carbonAds, pageContainer.firstChild)
+            } else if (splashContainer && adManager) {
+              adManager.appendChild(carbonAds)
             } else {
               container.insertBefore(carbonAds, container.firstChild)
             }
+
             window.clearInterval(interval)
           }
         }, 50)
       },
-      appendCodeSponsor() {
-        const interval = window.setInterval(() => {
-          const carbonAds = document.querySelector('.code-sponsor-widget')
-          const container = document.querySelector('.container')
-          const mainContainer = document.querySelector('.main-container')
-          if (carbonAds && mainContainer) {
-            if (mainContainer) {
-              mainContainer.appendChild(carbonAds)
-            } else {
-              container.appendChild(carbonAds)
-            }
-            window.clearInterval(interval)
-          }
-        }, 50)
-      },
-      moveCarbonAdsToBody() {
+      moveCarbonAdsToBody () {
         if (this.carbonEl) {
           document.body.appendChild(this.carbonEl)
-        }
-      },
-      moveCodeSponsorToBody() {
-        if (this.codeSponsorEl) {
-          document.body.appendChild(this.codeSponsorEl)
         }
       }
     },
     mounted () {
       this.saveAdsElements()
       this.appendCarbonAds()
-      this.appendCodeSponsor()
     },
-    beforeDestroy() {
+    beforeDestroy () {
       this.moveCarbonAdsToBody()
-      this.moveCodeSponsorToBody()
     }
   }
 </script>
 
 <style lang="scss">
   @import "~vue-material/theme/engine";
+
   $ad-responsive-big: 1670px;
   $ad-responsive-small: 600px;
+
   @mixin ad-theme ($hue, $color) {
-    #carbonads,
-    .code-sponsor-widget {
+    #carbonads {
       background: md-get-palette-color(grey, $hue);
+
+      .carbon-poweredby {
+        color: rgba($color, .54);
+
+        &:hover {
+          color: rgba($color, .87);
+        }
+      }
     }
+
     .carbon-wrap a {
       color: rgba($color, .7);
+
       &:hover {
         color: $color;
       }
     }
-    #carbonads .carbon-poweredby {
-      color: rgba($color, .54);
-      &:hover {
-        color: rgba($color, .87);
-      }
-    }
   }
+
   .main-container {
     @include ad-theme(200, #000);
   }
+
   .splash-container {
     @media (min-width: $ad-responsive-big) {
       @include ad-theme(800, #fff);
     }
   }
+
   #carbonads {
     max-width: 162px;
     margin: 1em 0 24px 16px;
@@ -112,17 +101,20 @@
     float: right;
     position: relative;
     z-index: 10;
+
     .splash-container & {
       @media (max-width: $ad-responsive-big) {
         background: #fff;
       }
     }
+
     @media (min-width: $ad-responsive-big) {
       margin: 0;
       position: fixed;
       right: 16px;
       bottom: 16px;
     }
+
     @media (max-width: $ad-responsive-small) {
       max-width: none;
       min-height: 100px;
@@ -130,6 +122,7 @@
       padding: 0;
       float: none;
     }
+
     .splash-container & {
       @media (max-width: $ad-responsive-big) {
         max-width: none;
@@ -139,23 +132,28 @@
         float: none;
       }
     }
+
     .carbon-wrap {
       display: flex;
       flex-direction: column;
       font-size: 12px;
+
       @media (max-width: $ad-responsive-small) {
         display: block;
       }
+
       .splash-container & {
         @media (max-width: $ad-responsive-big) {
           display: block;
         }
       }
+
       .carbon-img {
         @media (max-width: $ad-responsive-small) {
           margin-right: 16px;
           float: left;
         }
+
         .splash-container & {
           @media (max-width: $ad-responsive-big) {
             margin-right: 16px;
@@ -163,46 +161,41 @@
           }
         }
       }
+
       .carbon-text {
         line-height: 1.5em;
+
         @media (max-width: $ad-responsive-big) {
           padding-top: 1em;
           padding-right: 16px;
           display: block;
         }
+
         @media (min-width: $ad-responsive-big) {
           margin: 12px 0 8px;
         }
       }
+
       a {
         &:hover {
           text-decoration: none;
         }
       }
     }
+
     .carbon-poweredby {
       font-size: 11px;
     }
   }
-  .code-sponsor-widget {
-    margin-top: 64px;
-    padding: 10px;
-    display: flex;
-    justify-content: center;
-    @media (max-width: $ad-responsive-small) {
-      align-items: center;
-      flex-direction: column;
-    }
-    .splash-container & {
-      width: calc(100% + 32px);
-      margin: 0 -16px -16px;
-      background: #fff;
-    }
+
+  #ad-manager {
+    width: calc(100% + 32px);
+    margin: 0 -16px -16px;
+
     #carbonads {
-      margin-left: 16px;
-      @media (max-width: $ad-responsive-small) {
-        margin-top: 16px;
-        margin-left: 0;
+      @media (max-width: $ad-responsive-big) {
+        max-width: 530px;
+        margin: 24px auto;
       }
     }
   }
