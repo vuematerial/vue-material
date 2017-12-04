c5cb0652a1ec48c262671edd79c3f851eeeeda13
Samuell1 <samuell.patro@gmail.com>
2017-12-04 10:53:39 +0100

    refactor(MdExtractColor): refactor dominant method

--- src/components/MdExtractColor/MdExtractColor.vue
+++ src/components/MdExtractColor/MdExtractColor.vue
@@ -2,6 +2,9 @@
 export default {
   name: 'MdExtractColor',
   abstract: true,
+  props: {
+    mdMoreColors: Boolean
+  },
   render () {
     try {
       return this.$slots.default[0]
@@ -11,11 +14,11 @@ export default {
     return null
   },
   mounted () {
-
     let image = this.$el
 
     this.getDominantColor(image, (colors) => {
-      this.$emit('md-color', colors)
+      const color = this.mdMoreColors ? colors : colors[0]
+      this.$emit('md-color', color)
     })
   },
   methods: {
@@ -24,7 +27,7 @@ export default {
 
       image.crossOrigin = 'Anonymous'
 
-      image.onload = function () {
+      image.onload = () => {
 
         /*!
         *    Get dominant color from an image version 1.0 BETA
@@ -36,62 +39,62 @@ export default {
 
         let max = -1
         let stats = []
-        let statsResult = []
-        let idata, buffer, len, key, keys, maxKey, prevKey, pprevKey
+        let key, maxKey, prevKey, pprevKey
 
-        canvas.width = this.width
-        canvas.height = this.height
+        canvas.width = image.width
+        canvas.height = image.height
 
         const ctx = canvas.getContext('2d')
-        ctx.drawImage(this, 0, 0)
-
-        idata = ctx.getImageData(0, 0, canvas.width, canvas.height)
-        buffer = new Uint32Array(idata.data.buffer)
-        len = buffer.length - 1
+        ctx.drawImage(image, 0, 0)
+        const idata = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
         // get stats
+        let buffer = new Uint32Array(idata.data.buffer)
+        let len = buffer.length - 1
         while (len--) {
           key = buffer[len] & 0x00ffffff
           stats[key] = (stats[key] || 0) + 1
         }
 
         // determine most dominant color
-        keys = Object.keys(stats)
+        let keys = Object.keys(stats)
         for (len = 0; key = keys[len]; len++) {
-            if (stats[key] > max) {
-                max = stats[key]
-                pprevKey = prevKey
-                prevKey = maxKey
-                maxKey = key
-            }
+          if (stats[key] > max) {
+            max = stats[key]
+            pprevKey = prevKey
+            prevKey = maxKey
+            maxKey = key
+          }
         }
 
-        // convert the key to rgb
-        statsResult.push([(maxKey & 0x000000ff),
-                          (maxKey & 0x0000ff00) >> 8,
-                          (maxKey & 0x00ff0000) >> 16])
-
-        // if (prevKey) {
-        //   statsResult.push([(prevKey & 0x000000ff),
-        //                     (prevKey & 0x0000ff00) >> 8,
-        //                     (prevKey & 0x00ff0000) >> 16])
-
-        //     if (pprevKey) {
-        //       statsResult.push([(pprevKey & 0x000000ff),
-        //                         (pprevKey & 0x0000ff00) >> 8,
-        //                         (pprevKey & 0x00ff0000) >> 16])
-        //     }
-        // }
+        // convert the keys to rgb
+        const colors = this.convertColors(maxKey, prevKey, pprevKey)
 
         // join colors with commas
-        const colors = statsResult.map((color) => {
+        const colorsWithCommas = colors.map((color) => {
           return color.join()
         })
 
-        onLoad(colors)
+        onLoad(colorsWithCommas)
       }
 
       image.onerror = onError
+    },
+    convertToRGB (key) {
+      if (key) {
+        return [(key & 0x000000ff),
+                (key & 0x0000ff00) >> 8,
+                (key & 0x00ff0000) >> 16]
+      }
+      return
+    },
+    convertColors (maxKey, prevKey, pprevKey) {
+      let colors = [
+        this.convertToRGB(maxKey),
+        this.convertToRGB(prevKey),
+        this.convertToRGB(pprevKey)
+      ]
+      return colors
     }
   }
 }
