843056db44959c10211ca96b532256a85f42fede
Samuell <Samuell1@users.noreply.github.com>
2017-12-19 02:10:46 +0100

    fix: z-index issues (#1264)
    * feat(MdCard): remove z-index to prevent card overlap FAB
    
    * fix(MdSelect): fix select content under dialog
    
    * refactor(MdSelect): change order of scss
    
    * docs: remove z-index from main-nav (#1134)
    
    * Revert "feat(MdCard): remove z-index to prevent card overlap FAB"
    
    This reverts commit 9e128066ed814aff520ec6c5723619b147712cad.
    
    * feat(MdButton): add fab z-index to prevent overlap

--- src/components/MdField/MdSelect/MdSelect.vue
+++ src/components/MdField/MdSelect/MdSelect.vue
@@ -311,20 +311,22 @@
       border: 0;
     }
   }
+  .md-menu-content {
+    z-index: 111;
+    &.md-select-menu {
+      width: 100%;
 
-  .md-menu-content.md-select-menu {
-    width: 100%;
+      &.md-menu-content-enter {
+        transform: translate3d(0, -8px, 0) scaleY(.3);
+      }
 
-    &.md-menu-content-enter {
-      transform: translate3d(0, -8px, 0) scaleY(.3);
-    }
+      .md-list {
+        transition: opacity .3s $md-transition-drop-timing;
+      }
 
-    .md-list {
-      transition: opacity .3s $md-transition-drop-timing;
-    }
-
-    .md-dense .md-ripple.md-list-item-content {
-      font-size: 14px;
+      .md-dense .md-ripple.md-list-item-content {
+        font-size: 14px;
+      }
     }
   }
 </style>
