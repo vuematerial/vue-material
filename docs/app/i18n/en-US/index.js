49787d108ae3276565d4c44aef0390ee36e57848
Samuell <Samuell1@users.noreply.github.com>
2017-12-19 02:23:14 +0100

    docs: refactor i18n handling (#1301)

---
+++ docs/app/i18n/en-US/index.js
@@ -0,0 +1,16 @@
+import components from './components'
+import pages from './pages'
+
+export default {
+  aboutMaterial: 'About Vue Material',
+  madeWith: 'Made with',
+  by: 'by',
+  code: 'Code',
+  example: 'Example',
+  default: 'Default',
+  darkTheme: 'Dark Theme',
+  lightTheme: 'Light Theme',
+
+  pages,
+  components
+}
