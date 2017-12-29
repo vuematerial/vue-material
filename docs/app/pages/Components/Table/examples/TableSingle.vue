906a91da065fe56cc253188164b76b21853ea74d
Vi Pro <VdustR@gmail.com>
2017-12-30 01:51:06 +0800

    feat(MdTable): reactive selection (#1358)
    * fix(MdTable): multiple selection with table sorting
    
    an easy way to handle multiple selection
    
    fix #1348
    
    * refactor(MdTable): single selection using instance comparing instead of id comparing
    
    both `MdTable` and `MdTableRow` emit `md-selected` event to avoid breaking change
    
    * feat(MdTable): new props `:md-selected-value.sync` for reactive selection
    
    fix #1292
    
    * fix(MdTable): `syncSelectedValue` without `mdSelectedValue` in multiple selecting mode
    
    * fix(MdTableHeadSelection): fix `allSelected`, now disabled row could be tick from `md-selected-value
    
    * fix(MdTableRow): remove `md-selected` event
    
    too many duplicated events on a selection. It should be emit from `MdTable`
    
    BREAKING CHANGE: no more `md-selected` event from `MdTableRow`
    
    * fix(MdTable): select event should be triggered after select update event
    
    * fix(MdTableHeadSelection): select all only take effect on selectable items

--- docs/app/pages/Components/Table/examples/TableSingle.vue
+++ docs/app/pages/Components/Table/examples/TableSingle.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <md-table v-model="people" md-card>
+    <md-table v-model="people" md-card @md-selected="onSelect">
       <md-table-toolbar>
         <h1 class="md-title">Selection Colors</h1>
       </md-table-toolbar>
 
-      <md-table-row slot="md-table-row" slot-scope="{ item }" :class="getClass(item)" md-selectable="single" @md-selected="onSelect">
+      <md-table-row slot="md-table-row" slot-scope="{ item }" :class="getClass(item)" md-selectable="single">
         <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
         <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
         <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
