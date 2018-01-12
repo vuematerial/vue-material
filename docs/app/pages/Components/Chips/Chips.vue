247a840643e0316e10025a87c12446f4d3ffdaed
Vi Pro <VdustR@gmail.com>
2018-01-13 02:33:18 +0800

    feat(MdChips): add formatter for individual chips (#1339)
    * feat(MdChips): formatter
    
    Formatter before chip insertion. Effects to insertion and duplicated-checking.
    
    fix #1288
    
    * style(MdChips): always return a value
    
    * docs(Chips): fix `md-format` API description
    
    https://github.com/vuematerial/vue-material/pull/1339#discussion_r158723276
    
    * docs(ChipsFormat): fix component name and example message
    
    * docs(ChipsFormat): `formatName` return result directly
    
    https://github.com/vuematerial/vue-material/pull/1339#discussion_r158723492
    
    * style(MdChips): add a space before `if` statement
    
    https://github.com/vuematerial/vue-material/pull/1339#discussion_r158723536
    
    * docs(Chips): better message for formatter
    
    https://github.com/vuematerial/vue-material/pull/1339#discussion_r158723959
    
    * fix: code style

--- docs/app/pages/Components/Chips/Chips.vue
+++ docs/app/pages/Components/Chips/Chips.vue
@@ -3,6 +3,7 @@
 <example src="./examples/Editable.vue" />
 <example src="./examples/ChipCustomTemplate.vue" />
 <example src="./examples/DuplicatedFeedback.vue" />
+<example src="./examples/Format.vue" />
 <example src="./examples/Themed.vue" />
 
 <template>
@@ -51,6 +52,13 @@
 
       <p>Chips would reject insertion while a chip was duplicated. You could customize feedback style of the duplicated chip:</p>
       <code-example title="Duplicated Feedback" :component="examples['duplicated-feedback']" />
+    </div>
+
+    <div class="page-container-section">
+      <h2>Formatter</h2>
+
+      <p>Sometimes you may need to format a chip value before adding it, and for this case you can use a custom formatter function. This function will receive the chip value and must return the formatted value.</p>
+      <code-example title="Formatted chips" :component="examples['format']" />
     </div>
 
     <div class="page-container-section">
@@ -156,6 +164,15 @@ export default {
             type: 'Boolean',
             description: 'Always check if there is a duplicated chip while changing the input value, or check it only on insertion',
             defaults: 'false'
+          },
+          {
+            name: 'md-format',
+            type: 'Function',
+            description: [
+              'Formatter before chip insertion. Effects to insertion and duplicated-checking.',
+              'The Chips will pass the inputted value as a parameter of this function. This function returns the formatted result.'
+            ].join('<br/>'),
+            defaults: 'null'
           }
         ]
       },
