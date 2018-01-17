f0788be339e31491370469ab9c18b61bde2b7dcc
VdustR <VdustR@gmail.com>
2018-01-17 12:15:09 +0800

    feat(MdDatepicker): custom first day of a week
    new props `md-first-day-of-a-week` for custom first day of a week.
    
    fix #1397

--- docs/app/pages/Components/Datepicker/examples/BasicDatepicker.vue
+++ docs/app/pages/Components/Datepicker/examples/BasicDatepicker.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <md-datepicker v-model="selectedDate" />
+    Start from Monday:
+    <md-datepicker v-model="selectedDate" md-first-day-of-a-week="1" />
   </div>
 </template>
 
--- src/components/MdDatepicker/MdDatepicker.vue
+++ src/components/MdDatepicker/MdDatepicker.vue
@@ -6,7 +6,7 @@
     <slot />
 
     <keep-alive>
-      <md-datepicker-dialog :md-date.sync="selectedDate" :md-disabled-dates="mdDisabledDates" v-if="showDialog" @md-closed="toggleDialog" />
+      <md-datepicker-dialog :md-date.sync="selectedDate" :md-disabled-dates="mdDisabledDates" :md-first-day-of-a-week="mdFirstDayOfAWeek" v-if="showDialog" @md-closed="toggleDialog" />
     </keep-alive>
 
     <md-overlay class="md-datepicker-overlay" md-fixed :md-active="showDialog" @click="toggleDialog" />
@@ -44,7 +44,8 @@
       mdOverrideNative: {
         type: Boolean,
         default: true
-      }
+      },
+      mdFirstDayOfAWeek: {}
     },
     data: () => ({
       showDialog: false,
