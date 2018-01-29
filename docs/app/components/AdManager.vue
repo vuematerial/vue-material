78179b8c7d02cdd117257c9a1b5a57c4292cbe46
Marcos Moura <marcos.rufino@hotmart.com.br>
2018-01-29 17:13:01 -0200

    docs: change sponsorship link

--- docs/app/components/AdManager.vue
+++ docs/app/components/AdManager.vue
@@ -1,4 +1,3 @@
-
 <template>
   <md-content class="ad-manager" id="ad-manager" md-theme="default">
     <div id="code-sponsor" class="code-sponsor">
@@ -16,19 +15,24 @@
 
 <script>
   const isProd = process.env.NODE_ENV === 'production'
-  const token = 'bb9dade0-0207-4ab0-8c86-d00502d952f5'
+  const token = '0989e0dbf1919e50f5b34ab00c81fa6f'
+
   export default {
     name: 'AdManager',
     computed: {
       linkHref () {
         if (isProd) {
-          return `https://cs.berry.sh/c/${token}`
+          return `https://codesponsor.io/t/c/${token}`
         }
+
+        return false
       },
       pixelHref () {
         if (isProd) {
-          return `https://cs.berry.sh/l/${token}/pixel.png`
+          return `https://codesponsor.io/t/l/${token}/pixel.png`
         }
+
+        return false
       }
     }
   }
@@ -36,8 +40,10 @@
 
 <style lang="scss" scoped>
   @import "~vue-material/theme/engine";
+
   $ad-responsive-big: 1690px;
   $ad-responsive-small: 768px;
+
   @mixin ad-theme ($hue, $color) {
     .code-sponsor {
       background: md-get-palette-color(grey, $hue);
@@ -45,6 +51,7 @@
         color: rgba($color, .54);
       }
     }
+
     .code-sponsor-content {
       color: rgba($color, .7);
       &:hover {
@@ -52,13 +59,16 @@
       }
     }
   }
+
   .ad-manager {
     width: calc(100% + 32px);
     margin: 0 -16px -16px;
+
     @media (max-width: $ad-responsive-small) {
       display: flex;
     }
   }
+
   .code-sponsor {
     max-width: 175px;
     margin: 7px 16px 24px 16px;
@@ -70,17 +80,20 @@
     z-index: 10;
     font-size: 12px;
     line-height: 1.5em;
+
     @media (min-width: $ad-responsive-big) {
       margin: 0;
       position: fixed;
       right: 16px;
       bottom: 16px;
     }
+
     @media (max-width: $ad-responsive-small) {
       max-width: none;
       margin-bottom: 16px;
       float: none;
     }
+
     .splash-container & {
       @media (max-width: $ad-responsive-big) {
         max-width: 530px;
@@ -91,27 +104,32 @@
       }
     }
   }
+
   .code-sponsor-header {
     font-size: 11px;
     line-height: 1.3em;
   }
+
   .code-sponsor-content {
     margin-top: 1em;
     &:hover {
       text-decoration: none;
     }
   }
+
   .code-sponsor-name,
   .code-sponsor-description {
     &:hover {
       text-decoration: underline;
     }
   }
+
   .code-sponsor-name {
     margin-bottom: .5em;
     display: block;
     font-weight: 600;
   }
+
   .code-sponsor-pixel {
     width: 1px;
     height: 1px;
@@ -122,9 +140,11 @@
     clip: rect(0 0 0 0);
     border: 0;
   }
+
   .main-container {
     @include ad-theme(200, #000);
   }
+
   .splash-container {
     @media (min-width: $ad-responsive-big) {
       @include ad-theme(800, #fff);
