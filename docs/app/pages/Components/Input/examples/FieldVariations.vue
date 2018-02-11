683a18c0384e36cb2c60b47f7ccb566f5cd9efdf
Muhammed Shafi <shafimsp@gmail.com>
2018-02-11 09:59:56 +0530

    docs for field variants

---
+++ docs/app/pages/Components/Input/examples/FieldVariations.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <md-field md-variant="bottom-line">
+      <label>Normal</label>
+      <md-input v-model="normal"></md-input>
+    </md-field>
+
+    <md-field md-variant="box">
+      <label>Box</label>
+      <md-input v-model="box"></md-input>
+    </md-field>
+
+    <md-field md-variant="raised">
+      <label>Raised (Inline Only)</label>
+      <md-input v-model="raised"></md-input>
+    </md-field>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TextFields',
+    data: () => ({
+      normal: null,
+      box: null,
+      raised: null,
+      inline: null,
+      number: null,
+      textarea: null,
+      autogrow: null,
+      disabled: null
+    })
+  }
+</script>
