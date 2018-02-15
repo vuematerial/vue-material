3c9b2c243e625675f283306142d72f00896ef1e4
Muhammed Shafi <shafimsp@gmail.com>
2018-02-15 09:46:29 +0530

    MdDateInput component added in index of MdDatepicker

--- src/components/MdDatepicker/index.js
+++ src/components/MdDatepicker/index.js
@@ -1,7 +1,9 @@
 import material from 'vue-material/material'
 import MdDatepicker from './MdDatepicker'
+import MdDateInput from './MdDateInput'
 
 export default Vue => {
   material(Vue)
   Vue.component(MdDatepicker.name, MdDatepicker)
+  Vue.component(MdDateInput.name, MdDateInput)
 }
