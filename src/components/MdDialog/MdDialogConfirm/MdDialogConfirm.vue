e125c0d536b38ee707275b201bb92b7f389e586d
Matt Grande <matt@weeverapps.com>
2018-03-27 17:44:55 -0400

    fix(MdDialog): remove md-primary from MdDialogConfirm's cancel button (#1633)
    As recommended in #1622

--- src/components/MdDialog/MdDialogConfirm/MdDialogConfirm.vue
+++ src/components/MdDialog/MdDialogConfirm/MdDialogConfirm.vue
@@ -4,7 +4,7 @@
     <md-dialog-content v-if="mdContent" v-html="mdContent" />
 
     <md-dialog-actions>
-      <md-button class="md-primary" @click="onCancel">{{ mdCancelText }}</md-button>
+      <md-button @click="onCancel">{{ mdCancelText }}</md-button>
       <md-button class="md-primary" @click="onConfirm">{{ mdConfirmText }}</md-button>
     </md-dialog-actions>
   </md-dialog>
