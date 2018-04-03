79c3bde96e6f53f993a151822463de9996673fa9
Muhammed Shafi <shafimsp@gmail.com>
2018-04-03 17:27:18 +0530

    docs(MdFile): Support with field variants

--- docs/app/pages/Components/File/examples/FileField.vue
+++ docs/app/pages/Components/File/examples/FileField.vue
@@ -1,31 +1,37 @@
 <template>
   <div>
     <md-field>
+      <md-icon md-file-trigger>attach_file</md-icon>
       <label>Single</label>
       <md-file v-model="single" />
     </md-field>
 
     <md-field>
+      <md-icon md-file-trigger>attach_file</md-icon>
       <label>Upload files</label>
       <md-file v-model="placeholder" placeholder="A nice input placeholder" />
     </md-field>
 
     <md-field>
+      <md-icon md-file-trigger>attach_file</md-icon>
       <label>Disabled</label>
       <md-file v-model="disabled" disabled/>
     </md-field>
 
     <md-field>
+      <md-icon md-file-trigger>attach_file</md-icon>
       <label>Initial value</label>
       <md-file v-model="initial" />
     </md-field>
 
     <md-field>
+      <md-icon md-file-trigger>attach_file</md-icon>
       <label>Multiple</label>
       <md-file v-model="multiple" multiple />
     </md-field>
 
     <md-field>
+      <md-icon md-file-trigger>attach_file</md-icon>
       <label>Only images</label>
       <md-file v-model="single" accept="image/*" />
     </md-field>
