032ae515bac520b1935f82d7a5e316d076c565d1
Samuell1 <samuell.patro@gmail.com>
2018-04-03 14:51:35 +0200

    fix: constructor name support for IE11

--- src/components/MdRipple/MdRipple.vue
+++ src/components/MdRipple/MdRipple.vue
@@ -52,7 +52,7 @@
     watch: {
       mdActive (active) {
         const isBoolean = typeof active === 'boolean'
-        const isEvent = active.constructor.name.toLowerCase() === 'mouseevent'
+        const isEvent = active.constructor.toString().match(/function (\w*)/)[1].toLowerCase() === 'mouseevent'
 
         if (isBoolean && this.mdCentered && active) {
           this.startRipple({
