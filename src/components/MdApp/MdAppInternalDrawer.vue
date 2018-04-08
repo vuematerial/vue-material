131dacbb52cbe63775d3541fec8dfb69c9ca0b91
VdustR <VdustR@gmail.com>
2018-04-08 19:24:06 +0800

    fix(MdDrawer): Temporary style

--- src/components/MdApp/MdAppInternalDrawer.vue
+++ src/components/MdApp/MdAppInternalDrawer.vue
@@ -4,6 +4,7 @@
 
     <main class="md-app-container md-flex md-layout-row" :style="[containerStyles, contentStyles]" :class="[$mdActiveTheme, scrollerClasses]">
       <slot name="md-app-drawer-left"></slot>
+      <slot name="md-app-drawer-right-previous"></slot>
       <div class="md-app-scroller md-layout-column md-flex" :class="[$mdActiveTheme, scrollerClasses]">
         <slot name="md-app-content"></slot>
       </div>
--- src/components/MdDrawer/MdDrawerRightPrevious.vue
+++ src/components/MdDrawer/MdDrawerRightPrevious.vue
@@ -46,6 +46,9 @@
         }
 
         return classes
+      },
+      isTemporary () {
+        return !this.mdPermanent && !this.mdPersistent
       }
     }
   })
