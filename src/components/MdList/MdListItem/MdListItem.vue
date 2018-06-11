714ee9f6b7b4704ae97c4557967a6bcf99e893d0
Samuell <Samuell1@users.noreply.github.com>
2018-06-11 13:22:39 +0200

    fix(MdListItem): allow multiple actions (#1760)
    * fix(MdListItem): allow multiple actions
    
    * refactor(MdListItem): refactor hasChildrenButtons function
    
    * refactor(MdListItem): remove const
    
    * feat(MdListItem): remove prefixed user-select

--- src/components/MdList/MdListItem/MdListItem.vue
+++ src/components/MdList/MdListItem/MdListItem.vue
@@ -2,6 +2,7 @@
   import MdInteractionEvents from 'core/utils/MdInteractionEvents'
   import MdRouterLinkProps from 'core/utils/MdRouterLinkProps'
   import MdListItemDefault from './MdListItemDefault'
+  import MdListItemFakeButton from './MdListItemFakeButton'
   import MdListItemButton from './MdListItemButton'
   import MdListItemLink from './MdListItemLink'
   import MdListItemRouter from './MdListItemRouter'
@@ -22,6 +23,10 @@
     }
   }
 
+  function hasChildrenButtons (childrens) {
+    return childrens.default.some(children => children.componentOptions && children.componentOptions.tag === 'md-button')
+  }
+
   function shouldRenderButtonWithListener (listeners) {
     let listenerNames = Object.keys(listeners)
     let shouldRender = false
@@ -39,7 +44,7 @@
     return parent && parent.$router && props.to
   }
 
-  function createListComponent (props, parent, listeners) {
+  function createListComponent (props, parent, listeners, children) {
     if (hasExpansion(props)) {
       return MdListItemExpand
     }
@@ -62,6 +67,9 @@
     }
 
     if (shouldRenderButtonWithListener(listeners)) {
+      if (hasChildrenButtons(children)) {
+        return MdListItemFakeButton
+      }
       return MdListItemButton
     }
 
@@ -76,7 +84,7 @@
     },
     render (createElement, { parent, props, listeners, data, slots }) {
       let children = slots()
-      let listComponent = createListComponent(props, parent, listeners)
+      let listComponent = createListComponent(props, parent, listeners, children)
       let staticClass = 'md-list-item'
 
       if (data.staticClass) {
