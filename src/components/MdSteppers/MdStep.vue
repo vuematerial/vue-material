2fef269b39bcf87e06317a9a17d5f1d18b9f743c
Samuell1 <samuell.patro@gmail.com>
2018-06-15 14:42:34 +0200

    feat(MdStep): add v-show to hide content

--- src/components/MdSteppers/MdStep.vue
+++ src/components/MdSteppers/MdStep.vue
@@ -2,7 +2,7 @@
   <div class="md-stepper">
     <md-step-header v-if="MdSteppers.isVertical" :index="id" />
 
-    <div :class="['md-stepper-content', { 'md-active': id === MdSteppers.activeStep }]" :tabindex="tabIndex">
+    <div :class="['md-stepper-content', { 'md-active': isActive }]" :tabindex="tabIndex" v-show="isActive">
       <slot />
     </div>
   </div>
@@ -44,10 +44,13 @@
       }
     },
     computed: {
+      isActive () {
+        return this.id === this.MdSteppers.activeStep
+      },
       tabIndex () {
-        return this.id !== this.MdSteppers.activeStep
-        ? -1
-        : false
+        return !this.isActive
+          ? -1
+          : false
       }
     },
     methods: {
