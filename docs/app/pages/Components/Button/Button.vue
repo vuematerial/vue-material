801f9bf3e8cd9d7bee60707d00262529772b67a0
Ray Foss <rayfoss@users.noreply.github.com>
2018-07-23 09:17:12 -0500

    docs: A tag renders out
    escaping

--- docs/app/pages/Components/Button/Button.vue
+++ docs/app/pages/Components/Button/Button.vue
@@ -83,7 +83,7 @@ export default {
             {
               name: 'href',
               type: 'String',
-              description: 'Creates a anchor on the button. In this case the generated tag will be <a>.',
+              description: 'Creates a anchor on the button. In this case the generated tag will be `<a>`.',
               defaults: 'null'
             },
             {
