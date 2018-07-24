5e76f88d15446d1257b9233c072c570cac4ea37e
Ray Foss <ray@vblip.com>
2018-07-24 13:58:53 -0500

    comp(MdTable): merge glitch fix

--- src/components/MdTable/MdTable.vue
+++ src/components/MdTable/MdTable.vue
@@ -335,15 +335,6 @@
         this.$emit('update:mdSelectedValue', val)
         this.$emit('md-selected', val)
       },
-<<<<<<< Updated upstream
-      syncSelectedValue: async function () {
-        await this.$nextTick() // render the table first
-        if (this.MdTable.selectingMode === 'single') {
-          this.MdTable.singleSelection = this.mdSelectedValue
-        } else if (this.MdTable.selectingMode === 'multiple') {
-          this.MdTable.selectedItems = this.mdSelectedValue || []
-        }
-=======
       syncSelectedValue () {
         this.$nextTick().then(() => { // render the table first
           if (this.MdTable.selectingMode === 'single') {
@@ -352,7 +343,6 @@
             this.MdTable.selectedItems = this.mdSelectedValue || []
           }
         })
->>>>>>> Stashed changes
       },
       setWidth () {
         if (this.mdFixedHeader) {
