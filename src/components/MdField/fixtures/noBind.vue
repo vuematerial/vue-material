c0bd8e66cf0670b600ed755ecb067d2e99dc2296
Ray Foss <ray@vblip.com>
2018-08-08 16:26:30 -0500

    comp(MdField): More tests for MdField edge cases

---
+++ src/components/MdField/fixtures/noBind.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Non binded empty</h1>
+    <md-field>
+      <label>Initial Value</label>
+      <md-input ref="nondatabinded" @input="handleInputNoBind"></md-input>
+    </md-field>
+  </div>
+</template>
+
+<script>
+import MdField from '../MdField.vue'
+export default {
+  name: 'TextFields',
+  components: {
+    MdField
+  },
+  data() {
+    return {
+      noBind: ''
+    }
+  },
+  methods: {
+    handleInputNoBind(value) {
+      this.noBind = value
+    }
+  },
+  mounted () {
+    const nonDataBinded = this.$refs.nondatabinded
+    nonDataBinded.model = "Not data binded"
+  }
+}
+</script>
