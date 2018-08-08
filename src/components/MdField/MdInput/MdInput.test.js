b35127ed77738dfc2936c4e014e060303e0d9924
Ray Foss <ray@vblip.com>
2018-08-08 16:01:49 -0500

    MdInput: mimic old

--- src/components/MdField/MdInput/MdInput.test.js
+++ src/components/MdField/MdInput/MdInput.test.js
@@ -3,8 +3,6 @@ import mountTemplate from 'test/utils/mountTemplate'
 import MdFieldModule from 'components/MdField'
 import MdField from '../MdField.vue'
 
-
-
 Vue.use(MdFieldModule)
 
 test('should render the input', async () => {
