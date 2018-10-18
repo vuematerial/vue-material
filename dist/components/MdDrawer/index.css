d33cca82e1d14cf23683721e990df7226783ac24
Joey <joey91133@gmail.com>
2018-10-18 11:52:14 +0800

    build: v1.0.0-beta-10.3[D[4[D[D[D[D[D[D[D[D[D[D[D[D[D[D[

--- dist/components/MdDrawer/index.css
+++ dist/components/MdDrawer/index.css
@@ -1,5 +1,5 @@
 /*!
- * vue-material vv1.0.0-beta-10.4
+ * vue-material vv1.0.0-beta-10.3[D[4[D[D[D[D[D[D[D[D[D[D[D[D[D[D[
  * Made with <3 by marcosmoura 2018
  * Released under the MIT License.
  */
