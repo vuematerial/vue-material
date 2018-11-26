9d7544ae79e58c30b70598b170e88c193771a15e
Andre Bunse <andre.bunse@gmail.com>
2018-11-27 00:07:21 +0100

    support vue-router 3.0.2

--- src/core/utils/MdRouterLinkProps.js
+++ src/core/utils/MdRouterLinkProps.js
@@ -1,6 +1,7 @@
 export default (context, props) => {
+  const RouterLink = context.$options.components['RouterLink'] || context.$options.components['router-link']
   return {
     ...props,
-    ...context.$options.components['router-link'].options.props
+    ...RouterLink.options.props
   }
 }
