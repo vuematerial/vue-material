67abb32482638a11fa88281778e53b7687fb9856
tzahush <t@bena.co.il>
2019-03-27 10:29:17 +0200

    Added in Getting Started include of default.css
    Hello,
    
    I suggest u add the include of default.css so people wont have to fiddle with until getting the components to work.

--- docs/app/pages/GettingStarted.vue
+++ docs/app/pages/GettingStarted.vue
@@ -34,6 +34,7 @@
         import Vue from 'vue'
         import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
         import 'vue-material/dist/vue-material.min.css'
+        import 'vue-material/dist/theme/default.css'
 
         Vue.use(MdButton)
         Vue.use(MdContent)
