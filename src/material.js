2d45d98511447a74773174323cf826c4ab7a7900
hastom <hastomit@gmail.com>
2019-05-12 02:47:25 +0300

    refactor(MdDatepicker): upgrade date-fns (#2037)

--- src/material.js
+++ src/material.js
@@ -9,7 +9,7 @@ const init = () => {
     locale: {
       startYear: 1900,
       endYear: 2099,
-      dateFormat: 'YYYY-MM-DD',
+      dateFormat: 'yyyy-MM-dd',
       days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
       shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
       shorterDays: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
