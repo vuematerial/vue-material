6466937a29597133e595b32db9cf079f211c8122
Toma Rares <tomairares@gmail.com>
2019-11-06 17:33:22 +0200

    Added products to premium themes page

--- docs/app/pages/PremiumThemes/PremiumThemes.vue
+++ docs/app/pages/PremiumThemes/PremiumThemes.vue
@@ -1,8 +1,64 @@
  <template>
-   <splash-container splash class="home-page">
-   </splash-container>
+   <page-container centered>
+     <section class="page-container-section">
+       <h2 class="md-headline">Premium Themes</h2>
+       <h3>Creative Tim</h3>
+       <p>With the examples below built by our partners from
+          <a href="https://www.creative-tim.com/" target="_blank">Creative Tim</a> you can see how a real world application
+          is built, the technology stack behind it and how most
+          of the concepts you've learned so far apply in a real
+          world application.
+       </p>
+
+         <md-card>
+           <md-card-media>
+             <img src="https://s3.amazonaws.com/creativetim_bucket/products/81/original/opt_md_vue_thumbnail.jpg?1534938464">
+           </md-card-media>
+           <md-card-content>
+             Vue Material Dashboard
+           </md-card-content>
+         </md-card>
+
+         <md-card>
+           <md-card-media>
+             <img src="https://s3.amazonaws.com/creativetim_bucket/products/87/original/opt_mdp_vue_thumbnail.jpg?1534938492">
+           </md-card-media>
+           <md-card-content>
+             Vue Material Dashboard PRO
+           </md-card-content>
+         </md-card>
+
+         <md-card>
+           <md-card-media>
+             <img src="https://s3.amazonaws.com/creativetim_bucket/products/97/original/opt_mk_vue_thumbnail.jpg?1538041206">
+           </md-card-media>
+           <md-card-content>
+             Vue Material Kit
+           </md-card-content>
+         </md-card>
+
+         <md-card>
+           <md-card-media>
+             <img src="https://s3.amazonaws.com/creativetim_bucket/products/139/original/opt_mkp_vue_thumbnail.jpg?1547651786">
+           </md-card-media>
+           <md-card-content>
+             Vue Material Kit PRO
+           </md-card-content>
+         </md-card>
+    </section>
+   </page-container>
+
  </template>
  <script>
  </script>
- <style>
+ <style lang="scss" scoped>
+
+ .md-card{
+   width: 500px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+ }
+
+
  </style>
