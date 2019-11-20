890f405a128ffddb3b0aa9df3f57ca810d7d34d3
Toma Rares <tomairares@gmail.com>
2019-11-20 16:48:55 +0200

    Cleaned console.log test texts

--- docs/app/template/MainNavContent.vue
+++ docs/app/template/MainNavContent.vue
@@ -133,10 +133,8 @@
       responsiveLinks(){
         if (window.innerWidth < 601) {
           this.responsive = true;
-          console.log('mai mic ');
         } else {
           this.responsive = false;
-          console.log('mai mare ');
         }
       }
     }
