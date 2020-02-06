05a643266fcd0fc30538fc135c76d09596ecda9a
mhio <37205532+mhio@users.noreply.github.com>
2020-02-06 23:09:14 +1100

    MdCheckbox - `classes`, `attributes`, `find` and async `await`
    from #1679

--- src/components/MdCheckbox/MdCheckbox.test.js
+++ src/components/MdCheckbox/MdCheckbox.test.js
@@ -6,41 +6,37 @@ test('should render the checkbox', async () => {
   const template = '<md-checkbox></md-checkbox>'
   const wrapper = await mountTemplate(MdCheckbox, template)
 
-  expect(wrapper.hasClass('md-checkbox')).toBe(true)
+  expect(wrapper.classes('md-checkbox')).toBe(true)
 })
 
 test('should render the theme class', async () => {
   const template = '<md-checkbox md-theme="alt"></md-checkbox>'
   const wrapper = await mountTemplate(MdCheckbox, template)
 
-  expect(wrapper.hasClass('md-theme-alt')).toBe(true)
+  expect(wrapper.classes('md-theme-alt')).toBe(true)
 })
 
 test('should add id and for on input and label', async () => {
   const myId = 'my-id'
   const template = `<md-checkbox id="${myId}">Label</md-checkbox>`
   const wrapper = await mountTemplate(MdCheckbox, template)
-  const input = wrapper.find('input')[0]
-  const label = wrapper.find('label')[0]
+  const input = wrapper.find('input')
+  const label = wrapper.find('label')
 
-  expect(input.hasAttribute('id')).toBe(true)
-  expect(input.getAttribute('id')).toBe(myId)
+  expect(input.attributes().id).toBe(myId)
 
-  expect(label.hasAttribute('for')).toBe(true)
-  expect(label.getAttribute('for')).toBe(myId)
+  expect(label.attributes().for).toBe(myId)
 })
 
 test('should create a fallback id if not given', async () => {
   const wrapper = await mountStringSlot(MdCheckbox, 'Label')
   const createdId = wrapper.vm.$props.id
-  const input = wrapper.find('input')[0]
-  const label = wrapper.find('label')[0]
+  const input = wrapper.find('input')
+  const label = wrapper.find('label')
 
-  expect(input.hasAttribute('id')).toBe(true)
-  expect(input.getAttribute('id')).toBe(createdId)
+  expect(input.attributes().id).toBe(createdId)
 
-  expect(label.hasAttribute('for')).toBe(true)
-  expect(label.getAttribute('for')).toBe(createdId)
+  expect(label.attributes().for).toBe(createdId)
 })
 
 test('should create disabled and required classes', async () => {
@@ -51,8 +47,8 @@ test('should create disabled and required classes', async () => {
     }
   })
 
-  expect(wrapper.hasClass('md-disabled')).toBe(true)
-  expect(wrapper.hasClass('md-required')).toBe(true)
+  expect(wrapper.classes('md-disabled')).toBe(true)
+  expect(wrapper.classes('md-required')).toBe(true)
 })
 
 test('should bind id, name, disabled and required to the inner input', async () => {
@@ -64,19 +60,15 @@ test('should bind id, name, disabled and required to the inner input', async () 
       required: true
     }
   })
-  const input = wrapper.find('input')[0]
+  const input = wrapper.find('input')
 
-  expect(input.hasAttribute('id')).toBe(true)
-  expect(input.getAttribute('id')).toBe('test')
+  expect(input.attributes().id).toBe('test')
 
-  expect(input.hasAttribute('name')).toBe(true)
-  expect(input.getAttribute('name')).toBe('test')
+  expect(input.attributes().name).toBe('test')
 
-  expect(input.hasAttribute('disabled')).toBe(true)
-  expect(input.getAttribute('disabled')).toBe('disabled')
+  expect(input.attributes().disabled).toBe('disabled')
 
-  expect(input.hasAttribute('required')).toBe(true)
-  expect(input.getAttribute('required')).toBe('required')
+  expect(input.attributes().required).toBe('required')
 })
 
 test('should add and remove a value from model when model is an array by clicking on container', async () => {
@@ -86,20 +78,24 @@ test('should add and remove a value from model when model is an array by clickin
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: ['1']
+    data() {
+      return {
+        model: ['1']
+      }
     }
   })
-  const checkbox = wrapper.find(MdCheckbox)[0]
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const checkbox = wrapper.find(MdCheckbox)
+  const container = wrapper.find('.md-checkbox-container')
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(true)
-  expect(wrapper.data().model).toEqual(['1', '2'])
+  expect(wrapper.vm.model).toEqual(['1', '2'])
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(false)
-  expect(wrapper.data().model).toEqual(['1'])
+  expect(wrapper.vm.model).toEqual(['1'])
 })
 
 test('should add and remove a value from model when model is an array by clicking on label', async () => {
@@ -109,17 +105,19 @@ test('should add and remove a value from model when model is an array by clickin
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: []
+    data() {
+      return {
+        model: []
+      }
     }
   })
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const container = wrapper.find('.md-checkbox-container')
 
   container.trigger('click')
-  expect(wrapper.data().model).toEqual(['1'])
+  expect(wrapper.vm.model).toEqual(['1'])
 
   container.trigger('click')
-  expect(wrapper.data().model).toEqual([])
+  expect(wrapper.vm.model).toEqual([])
 })
 
 test('should toggle a checked class when checked', async () => {
@@ -129,18 +127,20 @@ test('should toggle a checked class when checked', async () => {
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: ['1']
+    data() {
+      return {
+        model: ['1']
+      }
     }
   })
-  const checkbox = wrapper.find(MdCheckbox)[0]
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const checkbox = wrapper.find(MdCheckbox)
+  const container = wrapper.find('.md-checkbox-container')
 
-  expect(checkbox.hasClass('md-checked')).toBe(true)
+  expect(checkbox.classes('md-checked')).toBe(true)
 
   container.trigger('click')
   await checkbox.vm.$nextTick()
-  expect(checkbox.hasClass('md-checked')).toBe(false)
+  expect(checkbox.classes('md-checked')).toBe(false)
 })
 
 test('should bind true / false when no value attribute is given', async () => {
@@ -150,22 +150,26 @@ test('should bind true / false when no value attribute is given', async () => {
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: null
+    data() {
+      return {
+        model: null
+      }
     }
   })
-  const checkbox = wrapper.find(MdCheckbox)[0]
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const checkbox = wrapper.find(MdCheckbox)
+  const container = wrapper.find('.md-checkbox-container')
 
   expect(checkbox.vm.isSelected).toBe(false)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(true)
-  expect(wrapper.data().model).toBe(true)
+  expect(wrapper.vm.model).toBe(true)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(false)
-  expect(wrapper.data().model).toBe(false)
+  expect(wrapper.vm.model).toBe(false)
 })
 
 test('true-value / false-value should works', async () => {
@@ -175,22 +179,26 @@ test('true-value / false-value should works', async () => {
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: null
+    data() {
+      return {
+        model: null
+      }
     }
   })
-  const checkbox = wrapper.find(MdCheckbox)[0]
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const checkbox = wrapper.find(MdCheckbox)
+  const container = wrapper.find('.md-checkbox-container')
 
   expect(checkbox.vm.isSelected).toBe(false)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(true)
-  expect(wrapper.data().model).toBe('true')
+  expect(wrapper.vm.model).toBe('true')
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(false)
-  expect(wrapper.data().model).toBe('false')
+  expect(wrapper.vm.model).toBe('false')
 })
 
 test('should toggle string values on model', async () => {
@@ -200,22 +208,26 @@ test('should toggle string values on model', async () => {
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: '1'
+    data() {
+      return {
+        model: '1'
+      }
     }
   })
-  const checkbox = wrapper.find(MdCheckbox)[0]
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const checkbox = wrapper.find(MdCheckbox)
+  const container = wrapper.find('.md-checkbox-container')
 
   expect(checkbox.vm.isSelected).toBe(true)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(false)
-  expect(wrapper.data().model).toBe(null)
+  expect(wrapper.vm.model).toBe(null)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(true)
-  expect(wrapper.data().model).toBe('1')
+  expect(wrapper.vm.model).toBe('1')
 })
 
 test('should toggle boolean model when checkbox do not have a value', async () => {
@@ -225,24 +237,28 @@ test('should toggle boolean model when checkbox do not have a value', async () =
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: false
+    data() {
+      return {
+        model: false
+      }
     }
   })
-  const checkbox = wrapper.find(MdCheckbox)[0]
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const checkbox = wrapper.find(MdCheckbox)
+  const container = wrapper.find('.md-checkbox-container')
 
   await checkbox.vm.$nextTick()
 
   expect(checkbox.vm.isSelected).toBe(false)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(true)
-  expect(wrapper.data().model).toBe(true)
+  expect(wrapper.vm.model).toBe(true)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(false)
-  expect(wrapper.data().model).toBe(false)
+  expect(wrapper.vm.model).toBe(false)
 })
 
 test('should toggle null / value while checkbox has been set value', async () => {
@@ -252,22 +268,26 @@ test('should toggle null / value while checkbox has been set value', async () =>
     </div>
   `
   const wrapper = await mountTemplate(MdCheckbox, template, {
-    data: {
-      model: false
+    data() {
+      return {
+        model: false
+      }
     }
   })
-  const checkbox = wrapper.find(MdCheckbox)[0]
-  const container = wrapper.find('.md-checkbox-container')[0]
+  const checkbox = wrapper.find(MdCheckbox)
+  const container = wrapper.find('.md-checkbox-container')
 
   await checkbox.vm.$nextTick()
 
   expect(checkbox.vm.isSelected).toBe(false)
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(true)
-  expect(wrapper.data().model).toBe('val')
+  expect(wrapper.vm.model).toBe('val')
 
   container.trigger('click')
+  await wrapper.vm.$nextTick()
   expect(checkbox.vm.isSelected).toBe(false)
-  expect(wrapper.data().model).toBe(null)
+  expect(wrapper.vm.model).toBe(null)
 })
