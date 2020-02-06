04929cf8009a29c2e73f719a1c9872d3f904ac74
mhio <37205532+mhio@users.noreply.github.com>
2020-02-06 23:04:48 +1100

    MdButton `class` and `attributes`
    from #1679

--- src/components/MdButton/MdButton.test.js
+++ src/components/MdButton/MdButton.test.js
@@ -8,11 +8,10 @@ test('should render the theme class', async () => {
   const template = '<md-button md-theme="alt">Button</md-button>'
   const wrapper = await mountTemplate(MdButton, template)
 
-  expect(wrapper.hasClass('md-button')).toBe(true)
-  expect(wrapper.hasClass('md-theme-alt')).toBe(true)
+  expect(wrapper.classes('md-button')).toBe(true)
+  expect(wrapper.classes('md-theme-alt')).toBe(true)
   expect(wrapper.is('button')).toBe(true)
-  expect(wrapper.hasAttribute('type')).toBe(true)
-  expect(wrapper.getAttribute('type')).toBe('button')
+  expect(wrapper.attributes().type).toBe('button')
 })
 
 test('should render tag <button> with type "button"', async () => {
@@ -21,31 +20,28 @@ test('should render tag <button> with type "button"', async () => {
 
   wrapper.trigger('click')
 
-  expect(wrapper.hasClass('md-button')).toBe(true)
+  expect(wrapper.classes('md-button')).toBe(true)
   expect(wrapper.is('button')).toBe(true)
-  expect(wrapper.hasAttribute('type')).toBe(true)
-  expect(wrapper.getAttribute('type')).toBe('button')
+  expect(wrapper.attributes().type).toBe('button')
 })
 
 test('should render tag <button> with type "submit"', async () => {
   const template = '<md-button type="submit">Button</md-button>'
   const wrapper = await mountTemplate(MdButton, template)
 
-  expect(wrapper.hasClass('md-button')).toBe(true)
+  expect(wrapper.classes('md-button')).toBe(true)
   expect(wrapper.is('button')).toBe(true)
-  expect(wrapper.hasAttribute('type')).toBe(true)
-  expect(wrapper.getAttribute('type')).toBe('submit')
+  expect(wrapper.attributes().type).toBe('submit')
 })
 
 test('should render tag <a> when a href is given', async () => {
   const template = '<md-button href="#test">Button</md-button>'
   const wrapper = await mountTemplate(MdButton, template)
 
-  expect(wrapper.hasClass('md-button')).toBe(true)
+  expect(wrapper.classes('md-button')).toBe(true)
   expect(wrapper.is('a')).toBe(true)
-  expect(wrapper.hasAttribute('type')).toBe(false)
-  expect(wrapper.hasAttribute('href')).toBe(true)
-  expect(wrapper.getAttribute('href')).toBe('#test')
+  expect(wrapper.attributes()).not.toContain('type')
+  expect(wrapper.attributes().href).toBe('#test')
 })
 
 test('should render tag <a> when using "to" prop from vue-router', async () => {
@@ -60,50 +56,44 @@ test('should render tag <a> when using "to" prop from vue-router', async () => {
   const rootWrapper = await mountTemplate(MdButton, rootRoute, { router })
   const testWrapper = await mountTemplate(MdButton, testRoute, { router })
 
-  expect(rootWrapper.hasClass('router-link-active')).toBe(true)
-  expect(testWrapper.hasClass('md-button')).toBe(true)
+  expect(rootWrapper.classes('router-link-active')).toBe(true)
+  expect(rootWrapper.classes('md-button')).toBe(true)
   expect(testWrapper.is('a')).toBe(true)
-  expect(testWrapper.hasAttribute('type')).toBe(false)
-  expect(rootWrapper.hasAttribute('href')).toBe(true)
-  expect(rootWrapper.getAttribute('href')).toBe('/')
-  expect(testWrapper.hasAttribute('href')).toBe(true)
-  expect(testWrapper.getAttribute('href')).toBe('/test')
+  expect(testWrapper.attributes()).not.toContain('type')
+  expect(rootWrapper.attributes().href).toBe('/')
+  expect(testWrapper.attributes().href).toBe('/test')
 })
 
 test('should render tag <button> when using "to" prop and vue-router is not configured', async () => {
   const template = '<md-button to="/test">Button</md-button>'
   const wrapper = await mountTemplate(MdButton, template)
 
-  expect(wrapper.hasClass('md-button')).toBe(true)
+  expect(wrapper.classes('md-button')).toBe(true)
   expect(wrapper.is('button')).toBe(true)
-  expect(wrapper.hasAttribute('type')).toBe(true)
-  expect(wrapper.getAttribute('type')).toBe('button')
-  expect(wrapper.hasAttribute('href')).toBe(false)
+  expect(wrapper.attributes().type).toBe('button')
+  expect(wrapper.attributes()).not.toContain('href')
 })
 
 test('should not render a ripple element if the button is disabled', async () => {
   const template = '<md-button disabled>Disabled</md-button>'
   const wrapper = await mountTemplate(MdButton, template)
-  const ripple = wrapper.find(MdRipple)[0]
+  const ripple = wrapper.find(MdRipple)
 
-  expect(wrapper.hasClass('md-button')).toBe(true)
+  expect(wrapper.classes('md-button')).toBe(true)
   expect(wrapper.is('button')).toBe(true)
-  expect(wrapper.hasAttribute('type')).toBe(true)
-  expect(wrapper.getAttribute('type')).toBe('button')
-  expect(wrapper.hasAttribute('disabled')).toBe(true)
-  expect(wrapper.getAttribute('disabled')).toBe('disabled')
-  expect(ripple.hasClass('md-disabled')).toBe(true)
+  expect(wrapper.attributes().type).toBe('button')
+  expect(wrapper.attributes().disabled).toBe('disabled')
+  expect(ripple.classes('md-disabled')).toBe(true)
 })
 
 test('should not render a ripple element when md-ripple is false', async () => {
   const template = '<md-button :md-ripple="false">Disabled</md-button>'
   const wrapper = await mountTemplate(MdButton, template)
-  const ripple = wrapper.find(MdRipple)[0]
+  const ripple = wrapper.find(MdRipple)
 
-  expect(wrapper.hasClass('md-button')).toBe(true)
-  expect(wrapper.hasClass('md-ripple-off')).toBe(true)
+  expect(wrapper.classes('md-button')).toBe(true)
+  expect(wrapper.classes('md-ripple-off')).toBe(true)
   expect(wrapper.is('button')).toBe(true)
-  expect(wrapper.hasAttribute('type')).toBe(true)
-  expect(wrapper.getAttribute('type')).toBe('button')
-  expect(ripple.hasClass('md-disabled')).toBe(true)
+  expect(wrapper.attributes().type).toBe('button')
+  expect(ripple.classes('md-disabled')).toBe(true)
 })
