0930fd4fec1a241c12ae38321847cc54358a7264
Konstantin Mohov <htmlmak@gmail.com>
2020-05-27 15:28:40 +0700

    Auto insert in MdChips after focusout (#2189)
    * Implemented the function of automatically adding a value in the MdChips component, in case of loss of focus
    
    * fixed code format

---
+++ docs/app/pages/Components/Chips/examples/AutoInsert.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <md-chips class="md-primary" v-model="emails" md-placeholder="Enter a email" :md-auto-insert="true">
+      <label>Recipients</label>
+    </md-chips>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AutoInsert',
+  data: () => ({
+    emails: [
+      'John.Smith@example.com',
+    ]
+  })
+}
+</script>
