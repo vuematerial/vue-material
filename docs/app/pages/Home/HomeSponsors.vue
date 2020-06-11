d1d7923f79207f605b1677f9b02bd9f81a6d6048
Dragos <dragos@creative-tim.com>
2020-06-11 15:12:13 +0300

    feat: add contributors & become backer button

--- docs/app/pages/Home/HomeSponsors.vue
+++ docs/app/pages/Home/HomeSponsors.vue
@@ -2,15 +2,36 @@
   <md-content md-tag="section" class="home-sponsors">
     <div class="page-wrapper">
       <h2 class="home-title md-headline">
-        <md-icon>monetization_on</md-icon>
-        {{ $t('pages.home.sponsors') }} &amp; {{ $t('pages.home.backers') }}
+        <md-icon>group</md-icon>
+        {{ $t('pages.home.contributors') }}
       </h2>
 
 
       <div class="home-sponsors-description">
+        <h3><md-icon>person</md-icon> {{ $t('pages.home.contributorsTitle') }}</h3>
+        <a class="contributors-image" href="https://github.com/vuematerial/vue-material/contributors"><img src="https://opencollective.com/vue-material/contributors.svg?width=1500&button=false" /></a>
+        <br><br>
+        <h3><md-icon>monetization_on</md-icon>
+        {{ $t('pages.home.sponsors') }} &amp; {{ $t('pages.home.backers') }}</h3>
+
+        <div class="backers-button">
+          <h3>OpenCollective Sponsors</h3>
+          <object type="image/svg+xml" data="https://opencollective.com/vue-material/tiers/backer.svg?avatarHeight=100&width=600"></object>
+        </div>
+
+
+        <h3><md-icon>favorite</md-icon>
+        {{ $t('pages.home.backersTitle') }}</h3>
+
+
         <i18n path="pages.home.sponsorsDescription" tag="p">
           <a href="https://www.github.com/marcosmoura" target="_blank">Marcos Moura</a>
         </i18n>
+
+        <ul>
+          <li><a href="https://github.com/elviskang" target="_blank">elviskang</a> for donating the npm package name!</li>
+          <li><a href="https://github.com/brunocastro" target="_blank">Bruno Castro</a> for the awesome Vue Material Logo. <logo-vue-material animated /></li>
+        </ul>
 
         <div class="d-flex align-items-center">
           <a href="https://www.codeinwp.com/?ref=vuematerial.io" target="_blank">
@@ -59,6 +80,24 @@
   @import "~vue-material/components/MdAnimation/variables";
   @import "~vue-material/components/MdLayout/mixins";
 
+  .logo-vue-material {
+    width: 24px;
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  object {
+    width: 200px;
+    height: 115px !important;
+  }
+
+  .backers-button {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+
 
   .home-sponsors {
     width: calc(100% + 32px);
@@ -66,9 +105,16 @@
     padding-bottom: 28px;
     a{
       &:hover{
-        color: transparent !important;
         text-decoration: none;
       }
+    }
+
+    p, .contributors-image, .align-items-center {
+      padding-left: 30px;
+    }
+
+    ul {
+      padding-left: 45px;
     }
   }
 
