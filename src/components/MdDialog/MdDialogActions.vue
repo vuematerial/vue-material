ce3bced98e134b6779a5ef659c6455ff6dbaf662
Michele Moresi <m.moresi@arca24.com>
2020-08-18 12:04:32 +0200

    fix: issue #2246 chrome btns visibility problems reformat code indentation

--- src/components/MdDialog/MdDialogActions.vue
+++ src/components/MdDialog/MdDialogActions.vue
@@ -6,38 +6,39 @@
 
 <script>
 export default {
-  name: "MdDialogActions",
-};
+  name: 'MdDialogActions'
+}
 </script>
 
 <style lang="scss">
-@import "~components/MdAnimation/variables";
+  @import "~components/MdAnimation/variables";
 
-.md-dialog-actions {
-  min-height: 52px;
-  padding: 8px 8px 8px 24px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  position: relative;
-  backface-visibility: hidden;
+  .md-dialog-actions {
+    min-height: 52px;
+    padding: 8px 8px 8px 24px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+	position: relative;
+	backface-visibility: hidden;
+	
 
-  &:before {
-    height: 1px;
-    position: absolute;
-    top: -1px;
-    right: 0;
-    left: 0;
-    content: " ";
-  }
+    &:before {
+      height: 1px;
+      position: absolute;
+      top: -1px;
+      right: 0;
+      left: 0;
+      content: " ";
+    }
 
-  .md-button {
-    min-width: 64px;
-    margin: 0;
+    .md-button {
+      min-width: 64px;
+      margin: 0;
 
-    + .md-button {
-      margin-left: 8px;
+      + .md-button {
+        margin-left: 8px;
+      }
     }
   }
-}
 </style>
