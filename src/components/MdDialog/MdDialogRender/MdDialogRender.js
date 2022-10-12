30ae8d5eed0640aa9dee6afd87098823ef44f267
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2022-10-12 17:56:30 -0300

    Refactor: replace display none for v-show directive on MdDialogRender

--- src/components/MdDialog/MdDialogRender/MdDialogRender.js
+++ src/components/MdDialog/MdDialogRender/MdDialogRender.js
@@ -11,14 +11,20 @@ export default {
       default: false
     }
   },
+  methods: {
+    // For elements that are hidden in the dom and have no height value
+    dispatchResizeEvent() {
+      this.$nextTick(() => {
+        window.dispatchEvent(new Event('resize'))
+      })
+    }
+  },
   render () {
     const defaultSlot = this.$slots.default
     if (defaultSlot && defaultSlot[0]) {
       if (this.keepAlive) {
-        defaultSlot[0].data.style = this.value ? {} : { display: 'none' }
-        this.$nextTick(() => {
-          if (this.value) window.dispatchEvent(new Event('resize'))
-        })
+        defaultSlot[0].data.directives = [{ name: 'show', value: this.value }]
+        if (this.value) this.dispatchResizeEvent()
         return defaultSlot[0]
       } else {
         return this.value && defaultSlot[0]
