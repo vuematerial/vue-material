9d0784cfb1a9d2dc885273ae18574fa0a244543d
Nepcha Team <marqbeniamin@yahoo.com>
2023-02-28 17:36:00 +0200

    Update ButtonRouter.vue

--- docs/app/pages/Components/Button/examples/ButtonRouter.vue
+++ docs/app/pages/Components/Button/examples/ButtonRouter.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <md-button to="/components/button">Default</md-button>
-    <md-button to="/components/button" class="md-primary">Primary</md-button>
-    <md-button to="/components/button" class="md-accent">Accent</md-button>
-    <md-button to="/components/button" disabled>Disabled</md-button>
+    <md-button to="/vuematerial/components/button">Default</md-button>
+    <md-button to="/vuematerial/components/button" class="md-primary">Primary</md-button>
+    <md-button to="/vuematerial/components/button" class="md-accent">Accent</md-button>
+    <md-button to="/vuematerial/components/button" disabled>Disabled</md-button>
   </div>
 </template>
 
