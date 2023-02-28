02cbf94547860e755f84362ba26e68d542cfe822
Nepcha Team <marqbeniamin@yahoo.com>
2023-02-28 13:20:30 +0200

    Update actions.js

--- docs/app/store/actions.js
+++ docs/app/store/actions.js
@@ -6,6 +6,7 @@ export default {
     const name = 'Vue Material'
     const metaTitle = document.querySelector('meta[property="og:title"]')
     const metaUrl = document.querySelector('meta[property="og:url"]')
+    const canonicalUrl = document.querySelector('link[rel="canonical"]')
 
     if (title) {
       document.title = `${title} - ${name}`
@@ -19,6 +20,10 @@ export default {
 
     if (metaUrl) {
       metaUrl.setAttribute('content', window.location.href)
+    }
+
+    if(canonicalUrl) {
+      canonicalUrl.setAttribute('href', window.location.href)
     }
 
     commit(types.SET_PAGE_TITLE, title)
