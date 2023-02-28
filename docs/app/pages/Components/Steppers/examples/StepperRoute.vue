26445ddc65ff88892a59e257e63024dc600e9272
Nepcha Team <marqbeniamin@yahoo.com>
2023-02-28 17:36:23 +0200

    Update StepperRoute.vue

--- docs/app/pages/Components/Steppers/examples/StepperRoute.vue
+++ docs/app/pages/Components/Steppers/examples/StepperRoute.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
     <md-steppers md-sync-route md-dynamic-height>
-      <md-step id="first" to="/components/steppers" exact md-label="First Step">
+      <md-step id="first" to="/vuematerial/components/steppers" exact md-label="First Step">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
       </md-step>
 
-      <md-step id="second" to="/components/steppers/second" md-label="Second Step">
+      <md-step id="second" to="/vuematerial/components/steppers/second" md-label="Second Step">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
         <md-list>
-          <md-list-item to="/components/steppers/second/sub1">Subpage 1</md-list-item>
-          <md-list-item to="/components/steppers/second/sub2">Subpage 2</md-list-item>
+          <md-list-item to="/vuematerial/components/steppers/second/sub1">Subpage 1</md-list-item>
+          <md-list-item to="/vuematerial/components/steppers/second/sub2">Subpage 2</md-list-item>
         </md-list>
       </md-step>
 
-      <md-step id="third" to="/components/steppers/third" md-label="Third Step">
+      <md-step id="third" to="/vuematerial/components/steppers/third" md-label="Third Step">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
       </md-step>
     </md-steppers>
     <md-list>
-      <md-list-item to="/components/steppers/second/sub1">Subpage 2-1</md-list-item>
-      <md-list-item to="/components/steppers/second/sub2">Subpage 2-2</md-list-item>
+      <md-list-item to="/vuematerial/components/steppers/second/sub1">Subpage 2-1</md-list-item>
+      <md-list-item to="/vuematerial/components/steppers/second/sub2">Subpage 2-2</md-list-item>
     </md-list>
   </div>
 </template>
