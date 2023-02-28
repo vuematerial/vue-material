911bb0cfa13c904b6d5bf7e74d2040898b7eb4d6
Nepcha Team <marqbeniamin@yahoo.com>
2023-02-28 13:21:00 +0200

    Update routes.js

--- docs/app/routes.js
+++ docs/app/routes.js
@@ -1,274 +1,280 @@
 export const routes = [
   {
-    path: '/components/badge',
+    path: '/vuematerial/components/badge',
     name: 'components/badge',
     page: 'Components/Badge/Badge.vue'
   },
   {
-    path: '/components/autocomplete',
+    path: '/vuematerial/components/autocomplete',
     name: 'components/autocomplete',
     page: 'Components/Autocomplete/Autocomplete.vue'
   },
   {
-    path: '/components/progress',
+    path: '/vuematerial/components/progress',
     name: 'components/progress',
-    redirect: '/components/progress-bar'
+    redirect: '/vuematerial/components/progress-bar'
   },
   {
-    path: '/components/progress-bar',
+    path: '/vuematerial/components/progress-bar',
     name: 'components/progress-bar',
     page: 'Components/ProgressBar/ProgressBar.vue'
   },
   {
-    path: '/components/progress-spinner',
+    path: '/vuematerial/components/progress-spinner',
     name: 'components/progress-spinner',
     page: 'Components/ProgressSpinner/ProgressSpinner.vue'
   },
   {
-    path: '/components/table',
+    path: '/vuematerial/components/table',
     name: 'components/table',
     page: 'Components/Table/Table.vue'
   },
   {
-    path: '/components/empty-state',
+    path: '/vuematerial/components/empty-state',
     name: 'components/empty-state',
     page: 'Components/EmptyState/EmptyState.vue'
   },
   {
-    path: '/components/select',
+    path: '/vuematerial/components/select',
     name: 'components/select',
     page: 'Components/Select/Select.vue'
   },
   {
-    path: '/components/datepicker',
+    path: '/vuematerial/components/datepicker',
     name: 'components/datepicker',
     page: 'Components/Datepicker/Datepicker.vue'
   },
   {
-    path: '/components/steppers/:optional?/:sub?',
+    path: '/vuematerial/components/steppers/:optional?/:sub?',
     name: 'components/steppers',
     page: 'Components/Steppers/Steppers.vue'
   },
   {
-    path: '/components/menu',
+    path: '/vuematerial/components/menu',
     name: 'components/menu',
     page: 'Components/Menu/Menu.vue'
   },
   {
-    path: '/components/tooltip',
+    path: '/vuematerial/components/tooltip',
     name: 'components/tooltip',
     page: 'Components/Tooltip/Tooltip.vue'
   },
   {
-    path: '/components/dialog',
+    path: '/vuematerial/components/dialog',
     name: 'components/dialog',
     page: 'Components/Dialog/Dialog.vue'
   },
   {
-    path: '/components/snackbar',
+    path: '/vuematerial/components/snackbar',
     name: 'components/snackbar',
     page: 'Components/Snackbar/Snackbar.vue'
   },
   {
-    path: '/components/tabs/:optional?/:sub?',
+    path: '/vuematerial/components/tabs/:optional?/:sub?',
     name: 'components/tabs',
     page: 'Components/Tabs/Tabs.vue'
   },
   {
-    path: '/components/bottom-bar/:optional?/:sub?',
+    path: '/vuematerial/components/bottom-bar/:optional?/:sub?',
     name: 'components/bottom-bar',
     page: 'Components/BottomBar/BottomBar.vue'
   },
   {
-    path: '/components/chips',
+    path: '/vuematerial/components/chips',
     name: 'components/chips',
     page: 'Components/Chips/Chips.vue'
   },
   {
-    path: '/components/avatar',
+    path: '/vuematerial/components/avatar',
     name: 'components/avatar',
     page: 'Components/Avatar/Avatar.vue'
   },
   {
-    path: '/components/speed-dial',
+    path: '/vuematerial/components/speed-dial',
     name: 'components/speed-dial',
     page: 'Components/SpeedDial/SpeedDial.vue'
   },
   {
-    path: '/about',
+    path: '/vuematerial/about',
     name: 'about',
     page: 'About.vue'
   },
   {
-    path: '/license',
+    path: '/vuematerial/license',
     name: 'license',
     page: 'License.vue'
   },
   {
-    path: '/',
+    path: '/vuematerial/',
     name: 'home',
     page: 'Home/Home.vue'
   },
   {
-    path: '/premium-themes',
+    path: '/vuematerial/premium-themes',
     name: 'Premium Themes',
     page: 'PremiumThemes/PremiumThemes.vue'
   },
   {
-    path: '/getting-started',
+    path: '/vuematerial/getting-started',
     name: 'getting-started',
     page: 'GettingStarted.vue'
   },
   {
-    path: '/configuration',
+    path: '/vuematerial/configuration',
     name: 'configuration',
     page: 'Configuration.vue'
   },
   {
-    path: '/themes',
+    path: '/vuematerial/themes',
     name: 'themes',
-    redirect: '/themes/concepts'
+    redirect: '/vuematerial/themes/concepts'
     // page: 'Themes/Themes.vue'
   },
   {
-    path: '/themes/concepts',
+    path: '/vuematerial/themes/concepts',
     name: 'themes/concepts',
     page: 'Themes/Concepts.vue'
   },
   {
-    path: '/themes/prebuilt',
+    path: '/vuematerial/themes/prebuilt',
     name: 'themes/prebuilt',
     page: 'Themes/Prebuilt.vue'
   },
   {
-    path: '/themes/configuration',
+    path: '/vuematerial/themes/configuration',
     name: 'themes/configuration',
     page: 'Themes/Configuration.vue'
   },
   {
-    path: '/themes/advanced',
+    path: '/vuematerial/themes/advanced',
     name: 'themes/advanced',
     page: 'Themes/Advanced.vue'
   },
   {
-    path: '/components',
+    path: '/vuematerial/components',
     name: 'components',
-    redirect: '/components/app'
+    redirect: '/vuematerial/components/app'
     // page: 'Components.vue'
   },
   {
-    path: '/components/button',
+    path: '/vuematerial/components/button',
     name: 'components/button',
     page: 'Components/Button/Button.vue'
   },
   {
-    path: '/components/app',
+    path: '/vuematerial/components/app',
     name: 'components/app',
     page: 'Components/App/App.vue'
   },
   {
-    path: '/components/checkbox',
+    path: '/vuematerial/components/checkbox',
     name: 'components/checkbox',
     page: 'Components/Checkbox/Checkbox.vue'
   },
   {
-    path: '/components/content',
+    path: '/vuematerial/components/content',
     name: 'components/content',
     page: 'Components/Content/Content.vue'
   },
   {
-    path: '/components/divider',
+    path: '/vuematerial/components/divider',
     name: 'components/divider',
     page: 'Components/Divider/Divider.vue'
   },
   {
-    path: '/components/drawer',
+    path: '/vuematerial/components/drawer',
     name: 'components/drawer',
     page: 'Components/Drawer/Drawer.vue'
   },
   {
-    path: '/components/card',
+    path: '/vuematerial/components/card',
     name: 'components/card',
     page: 'Components/Card/Card.vue'
   },
   {
-    path: '/components/form',
+    path: '/vuematerial/components/form',
     name: 'form',
     page: 'Components/Form/Form.vue'
   },
   {
-    path: '/components/icon',
+    path: '/vuematerial/components/icon',
     name: 'components/icon',
     page: 'Components/Icon/Icon.vue'
   },
   {
-    path: '/components/input',
+    path: '/vuematerial/components/input',
     name: 'components/input',
     page: 'Components/Input/Input.vue'
   },
   {
-    path: '/components/file',
+    path: '/vuematerial/components/file',
     name: 'components/file',
     page: 'Components/File/File.vue'
   },
   {
-    path: '/components/list/:optional?/:sub?',
+    path: '/vuematerial/components/list/:optional?/:sub?',
     name: 'components/list',
     page: 'Components/List/List.vue'
   },
   {
-    path: '/components/radio',
+    path: '/vuematerial/components/radio',
     name: 'components/radio',
     page: 'Components/Radio/Radio.vue'
   },
   {
-    path: '/components/subheader',
+    path: '/vuematerial/components/subheader',
     name: 'components/subheader',
     page: 'Components/Subheader/Subheader.vue'
   },
   {
-    path: '/components/switch',
+    path: '/vuematerial/components/switch',
     name: 'components/switch',
     page: 'Components/Switch/Switch.vue'
   },
   {
-    path: '/components/toolbar',
+    path: '/vuematerial/components/toolbar',
     name: 'components/toolbar',
     page: 'Components/Toolbar/Toolbar.vue'
   },
   {
-    path: '/ui-elements',
+    path: '/vuematerial/ui-elements',
     name: 'ui-elements',
-    redirect: '/ui-elements/elevation'
+    redirect: '/vuematerial/ui-elements/elevation'
     // page: 'UiElements.vue'
   },
   {
-    path: '/ui-elements/elevation',
+    path: '/',
+    name: 'home-redirect',
+    redirect: '/vuematerial'
+    // page: 'UiElements.vue'
+  },
+  {
+    path: '/vuematerial/ui-elements/elevation',
     name: 'ui-elements/elevation',
     page: 'UiElements/Elevation/Elevation.vue'
   },
   {
-    path: '/ui-elements/layout',
+    path: '/vuematerial/ui-elements/layout',
     name: 'ui-elements/layout',
     page: 'UiElements/Layout/Layout.vue'
   },
   {
-    path: '/ui-elements/scrollbar',
+    path: '/vuematerial/ui-elements/scrollbar',
     name: 'ui-elements/scrollbar',
     page: 'UiElements/Scrollbar/Scrollbar.vue'
   },
   {
-    path: '/ui-elements/text-selection',
+    path: '/vuematerial/ui-elements/text-selection',
     name: 'ui-elements/text-selection',
     page: 'UiElements/TextSelection/TextSelection.vue'
   },
   {
-    path: '/ui-elements/typography',
+    path: '/vuematerial/ui-elements/typography',
     name: 'ui-elements/typography',
     page: 'UiElements/Typography/Typography.vue'
   },
   {
-    path: '/with-router',
+    path: '/vuematerial/with-router',
     name: 'with-router',
     page: 'WithRouter.vue'
   },
